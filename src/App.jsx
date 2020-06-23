b7de322d83225b34e14b5df7e37726ff01cb2098
jiunkoo <jiunkoo0106@gmail.com>
2020-06-23 21:59:59 +0900

    App.jsx 컴포넌트 내부에서 상태와 렌더링 기능 분리

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,55 @@ import React, { useState } from 'react';
 
 import Page from './Page';
 
-export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [
+    { id: 1, title: 'redux 강의 듣기 #1' },
+    { id: 2, title: 'redux 과제 하기 #2' },
+  ],
+};
 
+function updateTaskTitle(state, value) {
+  return{
+    ...state,
+    taskTitle: value,
+  };
+}
+
+function addTask(state) {
   const { newId, taskTitle, tasks } = state;
+  return {
+    ...state,
+    newId: newId + 1,
+    taskTitle: '',
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  };
+}
+
+function deleteTask(state, id) {
+  const { tasks } = state;
+  return {
+    ...state,
+    tasks: tasks.filter((task) => task.id !== id),
+  };
+}
+
+export default function App() {
+  const [state, setState] = useState(initialState);
+
+  const { taskTitle, tasks } = state;
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+    setState(updateTaskTitle(state, event.target.value));
   }
 
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
+    setState(addTask(state));
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
+    setState(deleteTask(state, id));
   }
 
   return (
