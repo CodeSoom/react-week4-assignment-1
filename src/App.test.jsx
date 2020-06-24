5d9bcff4115bb388612526469614fc16987becc6
daadaadaah <daadaadaah@gmail.com>
2020-06-24 23:24:42 +0900

    [Refactor] Refactor App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,32 +4,38 @@ import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
+import tasks from '../__fixture__/data';
+
+function renderApp() {
+  return render(<App />);
+}
+
 describe('<App />', () => {
   context('When a user first launches the to-do app', () => {
     it('shows "To-do" ', () => {
-      const { container } = render(<App />);
+      const { container } = renderApp();
       expect(container).toHaveTextContent('To-do');
     });
 
     it('shows "할 일"', () => {
-      const { container } = render(<App />);
+      const { container } = renderApp();
       expect(container).toHaveTextContent('할 일');
     });
 
     it('shows "추가"', () => {
-      const { container } = render(<App />);
+      const { container } = renderApp();
       expect(container).toHaveTextContent('추가');
     });
 
     it('shows "할 일이 없어요!"', () => {
-      const { container } = render(<App />);
+      const { container } = renderApp();
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
 
   context('When a user enters a task called "바뀐다"', () => {
     it('shows "바뀐다" in the input', () => {
-      const { getByLabelText } = render(<App />);
+      const { getByLabelText } = renderApp();
 
       fireEvent.change(getByLabelText('할 일'), {
         target: {
@@ -48,24 +54,9 @@ describe('<App />', () => {
         getByLabelText,
         getByText,
         getByPlaceholderText,
-      } = render(<App />);
+      } = renderApp();
 
       const inputTodo = getByLabelText('할 일');
-
-      const tasks = [
-        {
-          id: 1,
-          title: '할 일1',
-        },
-        {
-          id: 2,
-          title: '할 일2',
-        },
-        {
-          id: 3,
-          title: '할 일3',
-        },
-      ];
 
       tasks.forEach((task) => {
         fireEvent.change(inputTodo, { target: { value: task.title } });
@@ -89,24 +80,9 @@ describe('<App />', () => {
     'when a user click the "완료" button for a task called "할 일2"',
     () => {
       it('does not show "할 일2"', () => {
-        const { container, getByLabelText, getByText } = render(<App />);
+        const { container, getByLabelText, getByText } = renderApp();
 
         const inputTodo = getByLabelText('할 일');
-
-        const tasks = [
-          {
-            id: 1,
-            title: '할 일1',
-          },
-          {
-            id: 2,
-            title: '할 일2',
-          },
-          {
-            id: 3,
-            title: '할 일3',
-          },
-        ];
 
         tasks.forEach((task) => {
           fireEvent.change(inputTodo, { target: { value: task.title } });
