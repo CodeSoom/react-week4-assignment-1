c3c9eba15591283b295e7dc674433f6beb433d7b
eonsang <2eonhworking@gmail.com>
2020-06-24 19:03:10 +0900

    [ RED ] should create an action to add a todo

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,4 +1,5 @@
 import reducer from './reducer';
+import {} from './actions';
 
 describe('reducer', () => {
   it('should return the initial state', () => {
@@ -6,6 +7,19 @@ describe('reducer', () => {
       newId: 0,
       taskTitle: '',
       tasks: [],
+    });
+  });
+
+  describe('ADD_TASK', () => {
+    it('should create an action to add a todo', () => {
+      const taskTitle = 'New TItle';
+      const expectedAction = {
+        type: 'ADD_TASK',
+        payload: {
+          title: taskTitle,
+        },
+      };
+      expect(addTaskAction(taskTitle)).toEqual(expectedAction);
     });
   });
 });
