3206cd9a956c4ef9eaf2231b11c635038486bacd
jiunkoo <jiunkoo0106@gmail.com>
2020-06-25 00:21:34 +0900

    Add Task 테스트 리팩토링2

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -19,6 +19,7 @@ describe('reducer', () => {
   describe('addTask', () => {
     context('with task title', () => {
       const previousState = {
+        newId: 100,
         taskTitle: 'Add Task',
         tasks: [],
       };
@@ -28,10 +29,13 @@ describe('reducer', () => {
       const state = reducer(previousState, action);
   
       expect(state.tasks).toHaveLength(1);
+      expect(state.tasks[0].id).not.toBeUndefined();
       expect(state.tasks[0].title).toBe('Add Task');
+      expect(state.taskTitle).toBe('');
     });
     context('without task title', () => {
       const previousState = {
+        newId: 100,
         taskTitle: '',
         tasks: [],
       };
