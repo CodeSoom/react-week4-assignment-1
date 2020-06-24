0901c995c055b5440a573264724dad2a954ce64d
jiunkoo <jiunkoo0106@gmail.com>
2020-06-24 22:40:34 +0900

    Add Task 테스트 구현

--- src/reducer.js
+++ src/reducer.js
@@ -2,13 +2,32 @@
 // - type(string)
 // - payload => object => { taskTitle }
 
-const reducer = (state, action) => {
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [
+    { id: 1, title: 'redux 강의 듣기 #1' },
+    { id: 2, title: 'redux 과제 하기 #2' },
+  ],
+};
+
+const reducer = (state = initialState, action) => {
   if(action.type === 'updateTaskTitle'){
     return {
       state,
       taskTitle: action.payload.taskTitle,
     };
   };
+  
+  if(action.type === 'addTask'){
+    const { newId, taskTitle, tasks } = state;
+    return {
+      ...state,
+      newId: newId + 1,
+      taskTitle: '',
+      tasks: [...tasks, { id: newId, title: taskTitle }],
+    }
+  }
 };
 
 export default reducer;
