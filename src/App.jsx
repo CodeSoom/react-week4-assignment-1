53643d5c640ed605c92e6e206487e87918cadc26
daadaadaah <daadaadaah@gmail.com>
2020-06-26 05:21:12 +0900

    [Refactor] Refactor a App.jsx for removing a Page.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,15 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
-import Page from './Page';
-
-import { changeTitle, addTask, deleteTask } from './action';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  };
-}
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const { taskTitle, tasks } = useSelector(selector);
-
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(changeTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
 
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
