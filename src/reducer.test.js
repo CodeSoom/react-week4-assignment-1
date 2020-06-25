5f9161e71a89e36fa7142941fb816c8bf48ee7df
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-25 19:19:49 +0900

    Fix errors in reducer test
    Set test state to avoid crashing with production code

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,40 +1,39 @@
 import reducer from './reducer';
+
 import {
   changeTitle, addTask, deleteTask,
 } from './actions';
 
 describe('reducer', () => {
+  const testState = {
+    newId: 100,
+    taskTitle: '새로운 할 일',
+    tasks: [{ id: 1, title: '기존 할 일' }],
+  };
+
   describe('changeTitle', () => {
     it('changes task title', () => {
-      const state = reducer({
-        taskTitle: '',
-      }, changeTitle('새로운 할 일'));
+      const state = reducer(testState, changeTitle('새로운 할 일'));
 
       expect(state.taskTitle).toBe('새로운 할 일');
     });
   });
 
   describe('addTask', () => {
-    function reduceAddTask(taskTitle) {
-      return reducer({
-        newId: 100,
-        taskTitle,
-        tasks: [],
-      }, addTask());
-    }
-
     context('with task title', () => {
       it('appends a new task into tasks', () => {
-        const state = reduceAddTask('새로운 할 일');
+        const state = reducer(testState, addTask());
 
-        expect(state.tasks).toHaveLength(1);
-        expect(state.tasks[0].id).not.toBeUndefined();
-        expect(state.tasks[0].title).toBe('새로운 할 일');
+        expect(state.tasks[1].id).not.toBeUndefined();
+        expect(state.tasks[1].title).toBe('새로운 할 일');
       });
     });
     context('without task title', () => {
       it('clears task title', () => {
-        const state = reduceAddTask('');
+        const state = reducer({
+          ...testState,
+          taskTitle: '',
+        }, '');
 
         expect(state.taskTitle).toBe('');
       });
@@ -44,22 +43,14 @@ describe('reducer', () => {
   describe('deleteTask', () => {
     context('with task id', () => {
       it('removes the task from tasks', () => {
-        const state = reducer({
-          tasks: [
-            { id: 1, title: '새로운 할 일' },
-          ],
-        }, deleteTask(1));
+        const state = reducer(testState, deleteTask(1));
 
         expect(state.tasks).toHaveLength(0);
       });
     });
     context('without task id', () => {
       it('does not work', () => {
-        const state = reducer({
-          tasks: [
-            { id: 1, title: '새로운 할 일' },
-          ],
-        }, deleteTask(100));
+        const state = reducer(testState, deleteTask(100));
 
         expect(state.tasks).toHaveLength(1);
       });
@@ -69,10 +60,9 @@ describe('reducer', () => {
   context('when action type does not exist', () => {
     it('return previous state', () => {
       const triggerMockAction = () => ({ action: 'mockAction' });
-      const previousState = {};
-      const state = reducer(previousState, triggerMockAction());
+      const state = reducer(testState, triggerMockAction());
 
-      expect(state).toBe(previousState);
+      expect(state).toBe(testState);
     });
   });
 });
