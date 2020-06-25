730634744c5700efa527f09690d339d2e3948b5d
daadaadaah <daadaadaah@gmail.com>
2020-06-26 02:42:10 +0900

    Write ListContainer test scenario

---
+++ src/ListContainer.test.jsx
@@ -0,0 +1,20 @@
+describe('<ListContainer />', () => {
+  describe('Initial rendering', () => {
+    context('When tasks are empty', () => {
+      it('shows "할 일이 없어요!"', () => {});
+    });
+
+    context('When tasks are not empty', () => {
+      it('shows tasks', () => {});
+    });
+  });
+
+  describe('User interaction', () => {
+    context(
+      'when a user click the "완료" button for a task called "할 일2"',
+      () => {
+        it('occurs a deleteTask action', () => {});
+      },
+    );
+  });
+});
