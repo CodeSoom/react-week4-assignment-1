a45612af65e2dcab581446fff87942ea52d1bc15
Kwakcena <rhkrgudwh@gmail.com>
2020-06-26 01:31:25 +0900

    App component의 통합 테스트 작성
    App 컴포넌트는 상태를 변경할 수 있는 기능을 포함하고 있다.
    - 할 일 입력하기
    - 할 일 추가하기
    - 할 일 삭제하기
    
    이 세 가지 구현에 대해 테스트를 작성했다.
    
    어떻게 시작해야 할 지 몰라서 App 테스트를 작성한 뒤 redux를 적용하면서
    테스트를 고치는 방향으로 잡았다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,70 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
+// TODO: 통합 테스트 코드 작성
+describe('App', () => {
+  function renderApp() {
+    const { container, getByText, getByPlaceholderText } = render(<App />);
+    return {
+      container,
+      input: getByPlaceholderText('할 일을 입력해 주세요'),
+      getByText,
+    };
+  }
 
-  expect(getByText(/추가/)).not.toBeNull();
+  function changeValue(input, value) {
+    fireEvent.change(input, {
+      target: {
+        value,
+      },
+    });
+  }
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  it('title과 버튼을 화면에 보여준다.', () => {
+    const { getByText } = renderApp();
+
+    expect(getByText(/To-do/)).not.toBeNull();
+    expect(getByText(/추가/)).not.toBeNull();
+  });
+
+  context('할 일을 입력하면', () => {
+    it('value가 변한다.', () => {
+      const { input } = renderApp();
+
+      changeValue(input, 'New Task');
+
+      expect(input.value).toBe('New Task');
+    });
+  });
+
+  context('할 일을 추가하면', () => {
+    it('목록에 추가되고 value는 비어진다.', () => {
+      const { container, input, getByText } = renderApp();
+
+      changeValue(input, 'New Task');
+
+      fireEvent.click(getByText(/추가/));
+      expect(container).toHaveTextContent(/New Task/);
+      expect(input.value).toBe('');
+    });
+  });
+
+  context('할 일을 삭제하면', () => {
+    it('목록에서 사라진다.', () => {
+      const { getByText, getByPlaceholderText } = render(<App />);
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
+
+      changeValue(input, 'New Task');
+
+      fireEvent.click(getByText(/추가/));
+      const task = getByText(/New Task/);
+      const removeButton = getByText(/완료/);
+
+      fireEvent.click(removeButton);
+      expect(task).not.toBeInTheDocument();
+    });
+  });
 });
