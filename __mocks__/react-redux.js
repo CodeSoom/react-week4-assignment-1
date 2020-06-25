f9ad267bbdd11465fc215e454ab51f8cac8c4fbb
daadaadaah <daadaadaah@gmail.com>
2020-06-25 20:40:52 +0900

    [Red] Add a test for a App with redux

---
+++ __mocks__/react-redux.js
@@ -0,0 +1,3 @@
+export const useSelector = jest.fn((selector) => selector({}));
+
+export const useDispatch = jest.fn();
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
 import tasks from '../__fixture__/data';
+
+import { changeTitle, addTask, deleteTask } from './action';
+
+jest.mock('react-redux');
 
 function renderApp() {
   return render(<App />);
@@ -12,22 +18,44 @@ function renderApp() {
 
 describe('<App />', () => {
   context('When a user first launches the to-do app', () => {
+    useSelector.mockImplementation((selector) => selector({
+      taskTitle: '',
+      tasks: [],
+    }));
+
     it('shows "To-do" ', () => {
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: '',
+        tasks: [],
+      }));
+
       const { container } = renderApp();
       expect(container).toHaveTextContent('To-do');
     });
 
     it('shows "할 일"', () => {
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: '',
+        tasks: [],
+      }));
       const { container } = renderApp();
       expect(container).toHaveTextContent('할 일');
     });
 
     it('shows "추가"', () => {
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: '',
+        tasks: [],
+      }));
       const { container } = renderApp();
       expect(container).toHaveTextContent('추가');
     });
 
     it('shows "할 일이 없어요!"', () => {
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: '',
+        tasks: [],
+      }));
       const { container } = renderApp();
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
@@ -35,6 +63,13 @@ describe('<App />', () => {
 
   context('When a user enters a task called "바뀐다"', () => {
     it('shows "바뀐다" in the input', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [],
+      }));
+
+      const changeTitleDispatch = jest.fn();
+      useDispatch.mockImplementation(() => changeTitleDispatch);
+
       const { getByLabelText } = renderApp();
 
       fireEvent.change(getByLabelText('할 일'), {
@@ -44,55 +79,73 @@ describe('<App />', () => {
       });
 
       expect(getByLabelText('할 일').value).toBe('바뀐다');
+
+      expect(changeTitleDispatch).toHaveBeenCalledWith(changeTitle('바뀐다'));
+      expect(changeTitleDispatch).toHaveBeenCalledTimes(1);
     });
   });
 
   context('when a user add a task called "할 일4"', () => {
-    it('appends a new task into tasks', () => {
-      const {
-        container,
-        getByLabelText,
-        getByText,
-        getByPlaceholderText,
-      } = renderApp();
+    it('clear task title', () => {
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: '할 일4',
+        tasks,
+      }));
+
+      const addTaskDispatch = jest.fn();
+      useDispatch.mockImplementation(() => addTaskDispatch);
+
+      const { getByLabelText, getByText, getByPlaceholderText } = renderApp();
 
       const inputTodo = getByLabelText('할 일');
-
-      tasks.forEach((task) => {
-        fireEvent.change(inputTodo, { target: { value: task.title } });
-
-        fireEvent.click(getByText('추가'));
-      });
 
       fireEvent.change(inputTodo, { target: { value: '할 일4' } });
 
       fireEvent.click(getByText('추가'));
 
       expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
+    });
 
-      expect(container).toHaveTextContent('할 일4');
+    it('occurs a addTask action', () => {
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: '할 일4',
+        tasks,
+      }));
 
-      expect(container).toHaveTextContent('완료');
+      const addTaskDispatch = jest.fn();
+      useDispatch.mockImplementation(() => addTaskDispatch);
+
+      const { getByLabelText, getByText } = renderApp();
+
+      const inputTodo = getByLabelText('할 일');
+
+      fireEvent.change(inputTodo, { target: { value: '할 일4' } });
+
+      fireEvent.click(getByText('추가'));
+
+      expect(addTaskDispatch).toHaveBeenCalledWith(addTask());
+      expect(addTaskDispatch).toHaveBeenCalledTimes(1);
     });
   });
 
   context(
     'when a user click the "완료" button for a task called "할 일2"',
     () => {
-      it('does not show "할 일2"', () => {
-        const { container, getByLabelText, getByText } = renderApp();
+      it('occurs a deleteTask action', () => {
+        useSelector.mockImplementation((selector) => selector({
+          taskTitle: '',
+          tasks,
+        }));
 
-        const inputTodo = getByLabelText('할 일');
+        const deleteTaskDispatch = jest.fn();
+        useDispatch.mockImplementation(() => deleteTaskDispatch);
 
-        tasks.forEach((task) => {
-          fireEvent.change(inputTodo, { target: { value: task.title } });
-
-          fireEvent.click(getByText('추가'));
-        });
+        const { getByText } = renderApp();
 
         fireEvent.click(getByText('할 일2').lastChild);
 
-        expect(container).not.toHaveTextContent('할 일2');
+        expect(deleteTaskDispatch).toHaveBeenCalledWith(deleteTask(2));
+        expect(deleteTaskDispatch).toHaveBeenCalledTimes(1);
       });
     },
   );
