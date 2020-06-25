8f1016299f8fb7758f53afa31ed5b910fc0a8e92
Kwakcena <rhkrgudwh@gmail.com>
2020-06-26 03:32:33 +0900

    [RED] addTask test
    할 일을 추가하면 새로운 상태의 newId는 +1이 되어야 한다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,6 +2,7 @@ import reducer from './reducer';
 
 import {
   updateTaskTitle,
+  addTask,
 } from './actions';
 
 describe('reducer', () => {
@@ -12,6 +13,20 @@ describe('reducer', () => {
       }, updateTaskTitle('New Task'));
 
       expect(newState.taskTitle).toBe('New Task');
+    });
+  });
+
+  describe('addTask', () => {
+    it('새로운 할 일을 추가한 상태를 반환한다.', () => {
+      const previousState = {
+        newId: 1,
+        taskTitle: 'New Task',
+        tasks: [],
+      };
+
+      const newState = reducer(previousState, addTask());
+
+      expect(newState.newId).toBe(2);
     });
   });
 });
