cf1814d48b72dc52eebe02a3c6b2525b9792748a
Kwakcena <rhkrgudwh@gmail.com>
2020-06-26 03:10:15 +0900

    [Refactor] actions의 사용, Inline Variable
    - actions 에 정의된 updateTaskTitle action creater를 사용.
    - 이전 상태값인 previousState 변수에 대해 Inline variable refactoring
    
    see also : https://refactoring.com/catalog/inlineVariable.html

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,20 +1,16 @@
 import reducer from './reducer';
 
+import {
+  updateTaskTitle,
+} from './actions';
+
 describe('reducer', () => {
   context('updateTaskTitle', () => {
-    it('taskTitle을 바꾼다.', () => {
-      const previousState = {
+    it('taskTitle을 바꿔 새로운 상태를 반환한다.', () => {
+      const newState = reducer({
         taskTitle: '',
-      };
+      }, updateTaskTitle('New Task'));
 
-      const action = {
-        type: 'updateTaskTitle',
-        payload: {
-          taskTitle: 'New Task',
-        },
-      };
-
-      const newState = reducer(previousState, action);
       expect(newState.taskTitle).toBe('New Task');
     });
   });
