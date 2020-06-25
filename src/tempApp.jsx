5661ee31c505c8b6fdbae5d8be2646fba9879d13
daadaadaah <daadaadaah@gmail.com>
2020-06-25 19:17:40 +0900

    Backup App before using redux

---
+++ src/tempApp.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+import Page from './Page';
+
+const initState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+function changeTaskTitle(state, taskTitle) {
+  return {
+    ...state,
+    taskTitle,
+  };
+}
+
+function addTask(state) {
+  const { newId, taskTitle, tasks } = state;
+
+  return {
+    ...state,
+    newId: newId + 1,
+    taskTitle: '',
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  };
+}
+
+function deleteTask(state, id) {
+  const { tasks } = state;
+
+  return {
+    ...state,
+    tasks: tasks.filter((task) => task.id !== id),
+  };
+}
+
+export default function App() {
+  const [state, setState] = useState(initState);
+
+  const { taskTitle, tasks } = state;
+
+  function handleChangeTitle(event) {
+    setState(changeTaskTitle(state, event.target.value));
+  }
+
+  function handleClickAddTask() {
+    setState(addTask(state));
+  }
+
+  function handleClickDeleteTask(id) {
+    setState(deleteTask(state, id));
+  }
+
+  return (
+    <Page
+      taskTitle={taskTitle}
+      onChangeTitle={handleChangeTitle}
+      onClickAddTask={handleClickAddTask}
+      tasks={tasks}
+      onClickDeleteTask={handleClickDeleteTask}
+    />
+  );
+}
