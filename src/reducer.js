4b7dc573647e3f60b3b321090ee850d74b5cdd22
KiChang Shin <kc7851@gmail.com>
2020-06-26 00:46:43 +0900

    reducer addTask 구현

--- src/reducer.js
+++ src/reducer.js
@@ -9,7 +9,9 @@ export default function reducer(state, action) {
   if (action.type === 'addTask') {
     return {
       ...state,
+      newId: state.newId + 1,
       tasks: [...state.tasks, {
+        id: state.newId,
         title: state.taskTitle,
       }],
     };
