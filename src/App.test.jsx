e13e8a7e2f83fa55cf7ea4c5c634b0cb461ac9c3
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-25 20:33:14 +0900

    Update test for App to use redux
    Add useSelector and useDispatch in App test
    y

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,22 +1,54 @@
 import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
+import {
+  changeTitle, addTask, deleteTask,
+} from './actions';
 
-test('App', () => {
-  const { getByText, getByLabelText } = render((
-    <App />
-  ));
+jest.mock('react-redux');
 
-  expect(getByText(/추가/)).not.toBeNull();
-  const value = '새로운 할 일';
+describe('App', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({
+    taskTitle: '',
+    tasks: [{ id: 1, title: '저녁먹기' }],
+  }));
 
-  fireEvent.change(getByLabelText(/할 일/), { target: { value } });
-  fireEvent.click(getByText(/추가/));
+  context('when value is changed', () => {
+    it('occurs change title action', () => {
+      const value = '새로운 할 일';
+      const { getByLabelText } = render((
+        <App />
+      ));
 
-  expect(getByText(value)).not.toBeNull();
+      fireEvent.change(getByLabelText(/할 일/), { target: { value } });
+      expect(dispatch).toBeCalledWith(changeTitle(value));
+    });
+  });
 
-  expect(getByText(/완료/)).not.toBeNull();
-  fireEvent.click(getByText(/완료/));
+  context('when add button is clicked', () => {
+    it('occurs add task action', () => {
+      const { getByText } = render((
+        <App />
+      ));
+
+      fireEvent.click(getByText(/추가/));
+      expect(dispatch).toBeCalledWith(addTask());
+    });
+  });
+
+  context('when delete button is clicked', () => {
+    it('occurs delete task action', () => {
+      const { getByText } = render((
+        <App />
+      ));
+
+      fireEvent.click(getByText(/완료/));
+      expect(dispatch).toBeCalledWith(deleteTask(1));
+    });
+  });
 });
