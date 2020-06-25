8a84696ec3d5e0613609d5b12c2697b893963abe
daadaadaah <daadaadaah@gmail.com>
2020-06-26 04:18:03 +0900

    Write InputContainer test scenario

---
+++ src/InputContainer.test.jsx
@@ -0,0 +1,17 @@
+describe('<InputContainer />', () => {
+  context('When the user does nothing', () => {
+    it('shows "할 일을 입력해 주세요"', () => {});
+
+    it('shows a "추가" button', () => {});
+  });
+
+  context('When a user enters a task called "바뀐다"', () => {
+    it('shows "바뀐다" in the input', () => {});
+  });
+
+  context('when a user add a task called "할 일4"', () => {
+    it('clear task title', () => {});
+
+    it('occurs a addTask action', () => {});
+  });
+});
