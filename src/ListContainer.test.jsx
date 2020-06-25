a53252306e6393901f0ef9260f7527229ded6cd9
daadaadaah <daadaadaah@gmail.com>
2020-06-26 03:49:55 +0900

    [Red] Add a test for ListContainer for initial rendering without tasks

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,7 +1,15 @@
 describe('<ListContainer />', () => {
   describe('Initial rendering', () => {
     context('When tasks are empty', () => {
-      it('shows "할 일이 없어요!"', () => {});
+      it('shows "할 일이 없어요!"', () => {
+        useSelector.mockImplementation((selector) => selector({
+          tasks: [],
+        }));
+        
+        const { container } = render(<ListContainer />);
+
+        expect(container).toHaveTextContent('할 일이 없어요!');
+      });
     });
 
     context('When tasks are not empty', () => {
