dce208dc732db7e73a61179503963ccee170c612
Kwakcena <rhkrgudwh@gmail.com>
2020-06-26 20:31:09 +0900

    [Refactor] page 컴포넌트 삭제, InputContainer 사용
    Input 컨테이너를 사용하고 Page 컴포넌트를 삭제함.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
-import Page from './Page';
-
-import {
-  updateTaskTitle,
-  addTask,
-} from './actions';
+import ListContainer from './ListContainer';
+import InputContainer from './InputContainer';
 
 export default function App() {
-  const { taskTitle } = useSelector((state) => ({
-    taskTitle: state.taskTitle,
-  }));
-
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
