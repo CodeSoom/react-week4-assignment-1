371f17934639eeaffb3c970173ac366a4723bceb
Minwook Park <mwpark0975@gmail.com>
2020-06-26 19:18:41 +0900

    5.1 App - test - need to refactor for containers

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,23 +2,37 @@ import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const tasks = [
+    // { id: 1, title: '아무 것도 하지 않기 #1' },
+    // { id: 2, title: '아무 것도 하지 않기 #2' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
+  // const dispatch = jest.fn();
+  // useDispatch.mockImplementation(() => dispatch);
+
   const { getByText, getByLabelText } = render((
     <App />
   ));
 
-  const TASK = '테스트';
-
   expect(getByText(/추가/)).not.toBeNull();
 
-  fireEvent.change(getByLabelText(/할 일/), { target: { value: TASK } });
-  fireEvent.click(getByText(/추가/));
+  // fireEvent.change(getByLabelText(/할 일/), { target: { value: /테스트/ } });
+  // fireEvent.click(getByText(/추가/));
 
-  expect(getByText(TASK)).not.toBeNull();
+  // expect(getByText(/테스트/)).not.toBeNull();
 
-  fireEvent.click(getByText(/완료/));
+  // fireEvent.click(getByText(/완료/));
 
-  expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+  // expect(getByText(/할 일이 없어요!/)).not.toBeNull();
 });
