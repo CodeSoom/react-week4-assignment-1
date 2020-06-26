ee9a0979d9518d75d8df0b3be9fd5ba68328b689
newoo <newoo4297@naver.com>
2020-06-27 00:40:57 +0900

    Update index, App, App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const testTasks = [
+    { id: 1, title: 'Distribute new version' },
+    { id: 2, title: 'Fix errors' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    taskTitle: '',
+    tasks: testTasks,
+  }));
+
   const { getByText } = render((
     <App />
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  expect(getByText(testTasks[0].title)).not.toBeNull();
 });
