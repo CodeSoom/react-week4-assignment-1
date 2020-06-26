c146181ee2914e3239df7eafba710770a7254319
KiChang Shin <kc7851@gmail.com>
2020-06-26 21:41:07 +0900

    App, Page 리팩토링
    ListContainer를 만들어서 App, Page에서 List와 관련된 부분을 변경하였습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
 import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
 import Page from './Page';
 
-import {
-  deleteTask,
-} from './action';
-
 export default function App() {
-  const dispatch = useDispatch();
-
-  const { tasks } = useSelector((state) => ({
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  }));
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <Page />
   );
 }
--- src/Page.jsx
+++ src/Page.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
 import InputContainer from './InputContainer';
-import List from './List';
+import ListContainer from './ListContainer';
 
-export default function Page({
-  tasks, onClickDeleteTask,
-}) {
+export default function Page() {
   return (
     <div>
       <h1>To-do</h1>
       <InputContainer />
-      <List
-        tasks={tasks}
-        onClickDelete={onClickDeleteTask}
-      />
+      <ListContainer />
     </div>
   );
 }
