c4611639b62505c48b1eacc48ba2e53ffd720fc4
newoo <newoo4297@naver.com>
2020-06-26 23:41:57 +0900

    Add ListContainer and Update ListContainer test

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
   context('without any tasks', () => {
     it("doesn't anything", () => {
+      const emptyTasks = [];
 
+      useSelector.mockImplementation((selector) => selector({
+        tasks: emptyTasks,
+      }));
+
+      const { getByText } = render((
+        <ListContainer />
+      ));
+
+      expect(getByText('할 일이 없어요!')).not.toBeNull();
     });
   });
 
   context('with tasks', () => {
-    it('give tasks List component', () => {
-      const testTasks = [
-        { id: 1, title: 'Distribute new version' },
-        { id: 2, title: 'Fix errors' },
-      ];
+    const testTasks = [
+      { id: 1, title: 'Distribute new version' },
+      { id: 2, title: 'Fix errors' },
+    ];
 
+    it('select tasks to List component', () => {
       useSelector.mockImplementation((selector) => selector({
         tasks: testTasks,
       }));
@@ -28,7 +40,30 @@ describe('ListContainer', () => {
         <ListContainer />
       ));
 
-      expect(getByText(testTasks[0])).not.toBeNull();
+      expect(getByText(testTasks[0].title)).not.toBeNull();
+    });
+
+    it('dispatch id of task to be deleted', () => {
+      const dispatch = jest.fn();
+
+      useDispatch.mockImplementation(() => dispatch);
+
+      useSelector.mockImplementation((selector) => selector({
+        tasks: testTasks,
+      }));
+
+      const { getAllByText } = render((
+        <ListContainer />
+      ));
+
+      fireEvent.click(getAllByText(/완료/)[0]);
+
+      expect(dispatch).toBeCalledWith({
+        type: 'deleteTask',
+        payload: {
+          id: 1,
+        },
+      });
     });
   });
 });
