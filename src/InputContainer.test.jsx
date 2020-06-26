a26b3085224a13a7138a60be138d69c9cd8c7dc5
Kwakcena <rhkrgudwh@gmail.com>
2020-06-27 01:29:39 +0900

    [Refactor] InputContainer test coverage 100%
    InputContainer에서 test 커버리지의 100% 달성.
    
    기존의 테스트에서는 updateTaskTitle에 대한 테스트가 없었기 때문에
    테스트로 인해 실행되는 handleChangeTitle 함수가 없었다.
    
    따라서 이에 대한 테스트를 작성하여 커버리지 100퍼센트를 달성한다.

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -7,26 +7,59 @@ import { useDispatch, useSelector } from 'react-redux';
 import InputContainer from './InputContainer';
 
 import {
-  addTask,
+  addTask, updateTaskTitle,
 } from './actions';
 
 jest.mock('react-redux');
 
 describe('InputContainer', () => {
-  it('할 일을 추가한다', () => {
-    useSelector.mockImplementation((selector) => selector({
-      taskTitle: 'New Task',
-    }));
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
 
-    const dispatch = jest.fn();
-    useDispatch.mockImplementation(() => dispatch);
+  function renderInputContainer(taskTitle = '') {
+    const { getByText, getByDisplayValue, getByPlaceholderText } = render(<InputContainer />);
+    return {
+      addButton: getByText(/추가/),
+      taskTitle: getByDisplayValue(taskTitle),
+      input: getByPlaceholderText(/할 일을 입력해 주세요/),
+    };
+  }
 
-    const { getByText, getByDisplayValue } = render(<InputContainer />);
+  context('updateTaskTitle', () => {
+    it('새로운 상태로 변경시킨다.', () => {
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: '',
+      }));
 
-    expect(getByText(/추가/)).not.toBeNull();
-    expect(getByDisplayValue(/New Task/)).not.toBeNull();
+      const { input } = renderInputContainer();
+      fireEvent.change(
+        input,
+        {
+          target: {
+            value: 'New Task',
+          },
+        },
+      );
 
-    fireEvent.click(getByText(/추가/));
-    expect(dispatch).toBeCalledWith(addTask());
+      expect(dispatch).toBeCalledWith(updateTaskTitle(
+        'New Task',
+      ));
+    });
+  });
+
+  context('addTask', () => {
+    it('할 일을 추가한다', () => {
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: 'New Task',
+      }));
+
+      const { addButton, taskTitle } = renderInputContainer('New Task');
+
+      expect(addButton).not.toBeNull();
+      expect(taskTitle).not.toBeNull();
+
+      fireEvent.click(addButton);
+      expect(dispatch).toBeCalledWith(addTask());
+    });
   });
 });
