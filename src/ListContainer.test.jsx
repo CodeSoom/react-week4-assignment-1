5089bb9ace6dce4805b0256b1eec6fda0791bb3a
eonsang <2eonhworking@gmail.com>
2020-06-26 19:23:59 +0900

    Refactor: ListContainer test에서 tasks 여부에 따른 context 분리

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -7,57 +7,49 @@ import ListContainer from './ListContainer';
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
-  it("tasks가 없을 경우, '할 일이 없어요!' 텍스트가 보인다.", () => {
-    useSelector.mockImplementation((selector) =>
-      selector({
+
+  context('tasks가 있을 경우,', () => {
+    useSelector.mockImplementation((selector) => selector({
+      tasks: [
+        { id: 1, title: 'Task-1' },
+        { id: 2, title: 'Task-2' },
+      ],
+    }));
+
+    it('기존에 등록되어있는 task가 있다.', () => {
+      const { getByText } = render(<ListContainer />);
+
+      expect(getByText('Task-1')).not.toBeNull();
+      expect(getByText('Task-2')).not.toBeNull();
+    });
+
+    it("'완료'버튼 2개가 있다.", () => {
+      const { getAllByText } = render(<ListContainer />);
+
+      expect(getAllByText('완료')).toHaveLength(2);
+    });
+
+    it("'완료' 버튼을 클릭하면 task가 사라진다.", () => {
+      const dispatch = jest.fn();
+      useDispatch.mockImplementation(() => dispatch);
+
+      const { getAllByText } = render(<ListContainer />);
+
+      fireEvent.click(getAllByText('완료')[0]);
+
+      expect(dispatch).toBeCalledTimes(1);
+    });
+  });
+
+  context('tasks가 없을 경우, ', () => {
+    it("'할 일이 없어요!' 텍스트가 보인다.", () => {
+      useSelector.mockImplementation((selector) => selector({
         tasks: [],
-      }),
-    );
-    const { getByText } = render(<ListContainer />);
-    expect(getByText('할 일이 없어요!')).not.toBeNull();
-  });
+      }));
 
-  it('기존에 등록되어있는 task가 있다.', () => {
-    useSelector.mockImplementation((selector) =>
-      selector({
-        tasks: [
-          { id: 1, title: 'Task-1' },
-          { id: 2, title: 'Task-2' },
-        ],
-      }),
-    );
-    const { getByText } = render(<ListContainer />);
-    expect(getByText('Task-1')).not.toBeNull();
-    expect(getByText('Task-2')).not.toBeNull();
-  });
-
-  it("'완료'버튼 2개가 있다.", () => {
-    useSelector.mockImplementation((selector) =>
-      selector({
-        tasks: [
-          { id: 1, title: 'Task-1' },
-          { id: 2, title: 'Task-2' },
-        ],
-      }),
-    );
-    const { getAllByText } = render(<ListContainer />);
-    expect(getAllByText('완료')).toHaveLength(2);
-  });
-
-  it("'완료' 버튼을 클릭하면 task가 사라진다.", () => {
-    const dispatch = jest.fn();
-    useDispatch.mockImplementation(() => dispatch);
-    useSelector.mockImplementation((selector) =>
-      selector({
-        tasks: [
-          { id: 1, title: 'Task-1' },
-          { id: 2, title: 'Task-2' },
-        ],
-      }),
-    );
-    const { getAllByText } = render(<ListContainer />);
-
-    fireEvent.click(getAllByText('완료')[0]);
-    expect(dispatch).toBeCalledTimes(1);
+      const { getByText } = render(<ListContainer />);
+      
+      expect(getByText('할 일이 없어요!')).not.toBeNull();
+    });
   });
 });
