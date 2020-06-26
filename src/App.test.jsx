8e1d775f6b2978f24559299e10ce7a9ca9d66eeb
eonsang <2eonhworking@gmail.com>
2020-06-26 20:19:49 +0900

    Test: 추가 버튼 클릭 시 task 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
+import reducer from './reducer';
 
 jest.mock('react-redux');
 
 describe('App', () => {
-  useSelector.mockImplementation((selector) =>
-    selector({
-      tasks: [
-        { id: 1, title: 'Task-1' },
-        { id: 2, title: 'Task-2' },
-      ],
-    }),
-  );
   it('task를 추가할 수 있는 버튼이 있다.', () => {
     const { getByText } = render(<App />);
 
@@ -22,13 +15,19 @@ describe('App', () => {
   });
 
   it('기존에 등록되어있는 task가 있다.', () => {
+    useSelector.mockImplementation((selector) =>
+      selector({
+        tasks: [
+          { id: 1, title: 'Task-1' },
+          { id: 2, title: 'Task-2' },
+        ],
+      }),
+    );
     const { getByText } = render(<App />);
 
     expect(getByText('Task-1')).not.toBeNull();
     expect(getByText('Task-2')).not.toBeNull();
   });
-
-  // TODO: 통합 테스트 코드 작성
 
   it("task가 없을 경우 '할 일이 없어요' 텍스트가 노출된다.", () => {
     useSelector.mockImplementation((selector) =>
@@ -38,5 +37,24 @@ describe('App', () => {
     );
     const { getByText } = render(<App />);
     expect(getByText('할 일이 없어요!')).not.toBeNull();
+  });
+
+  it('추가 버튼을 클릭하면 task를 추가 한다.', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) =>
+      selector({
+        taskTitle: 'new Title',
+        tasks: [],
+      }),
+    );
+    const { getByText } = render(<App />);
+
+    fireEvent.click(getByText('추가'));
+
+    expect(dispatch).toBeCalled();
+    expect(dispatch).toBeCalledWith({
+      type: 'ADD_TASK',
+    });
   });
 });
