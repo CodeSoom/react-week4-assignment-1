b402cf99186067e6a7ba05107dd3c5cac187d7ca
Kwakcena <rhkrgudwh@gmail.com>
2020-06-26 18:12:51 +0900

    [Refactor] describe-context-it 구조 사용
    context를 추가함

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -10,27 +10,31 @@ jest.mock('react-redux');
 
 // TODO: 통합 테스트 코드 작성
 describe('App', () => {
-  it('할 일이 있으면 할 일 목록을 보여준다.', () => {
-    useSelector.mockImplementation((selector) => selector({
-      tasks: [
-        { id: 1, title: 'New Task #1' },
-        { id: 2, title: 'New Task #2' },
-      ],
-    }));
+  context('할 일이 있으면', () => {
+    it('목록을 보여준다.', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [
+          { id: 1, title: 'New Task #1' },
+          { id: 2, title: 'New Task #2' },
+        ],
+      }));
 
-    const { getByText } = render(<App />);
+      const { getByText } = render(<App />);
 
-    expect(getByText(/New Task #1/)).not.toBeNull();
-    expect(getByText(/New Task #2/)).not.toBeNull();
+      expect(getByText(/New Task #1/)).not.toBeNull();
+      expect(getByText(/New Task #2/)).not.toBeNull();
+    });
   });
 
-  it('할 일이 없으면 빈 메세지를 출력한다', () => {
-    useSelector.mockImplementation((selector) => selector({
-      tasks: [],
-    }));
+  context('할 일이 없으면', () => {
+    it('빈 메세지를 출력한다', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [],
+      }));
 
-    const { getByText } = render(<App />);
+      const { getByText } = render(<App />);
 
-    expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+      expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+    });
   });
 });
