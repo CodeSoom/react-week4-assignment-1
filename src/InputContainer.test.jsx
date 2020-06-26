b75158ce1f3c90b35505bc96634a020a6107bebe
SeaFalcon <seafalcon323@gmail.com>
2020-06-26 23:59:38 +0900

    InputContainer 테스트_2 추가

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
 
 import InputContainer from './InputContainer';
+
+import {
+  updateTaskTitle,
+} from './actions';
+
+jest.mock('react-redux');
 
 describe('InputContainer', () => {
   it('초기화면 로딩', () => {
@@ -11,5 +19,24 @@ describe('InputContainer', () => {
     ));
 
     expect(container).toHaveTextContent(/할 일/);
+    expect(container).toHaveTextContent(/추가/);
+  });
+
+  context('할 일을 작성하면', () => {
+    it('updateTaskTitle 액션이 dispatch 된다.', () => {
+      const dispatch = jest.fn();
+
+      useDispatch.mockImplementation(() => dispatch);
+
+      const { getByLabelText } = render((
+        <InputContainer />
+      ));
+
+      const input = getByLabelText('할 일');
+
+      fireEvent.change(input, { target: { value: 'do something' } });
+
+      expect(dispatch).toBeCalledWith(updateTaskTitle('do something'));
+    });
   });
 });
