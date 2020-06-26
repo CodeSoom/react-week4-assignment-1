9bcdc97ddf89e216afd68e61d804c5ce4edcccfd
eonsang <2eonhworking@gmail.com>
2020-06-26 19:38:54 +0900

    Test: taskTitle 변경 dispatch 발생

---
+++ src/InputContainer.test.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+
+import InputContainer from './InputContainer';
+
+jest.mock('react-redux');
+
+describe('InputContainer', () => {
+  it('task를 추가할 수 있는 버튼이 있다.', () => {
+    const { getByText } = render(<InputContainer />);
+
+    expect(getByText(/추가/)).not.toBeNull();
+  });
+
+  it('taskTitle 입력창이 있다.', () => {
+    const { getByPlaceholderText } = render(<InputContainer />);
+
+    expect(getByPlaceholderText('할 일을 입력해 주세요')).not.toBeNull();
+  });
+
+  it('taskTitle을 변경한다.', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) =>
+      selector({
+        taskTitle: '',
+      }),
+    );
+
+    const { getByPlaceholderText } = render(<InputContainer />);
+
+    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+      target: { value: 'new Task' },
+    });
+
+    expect(dispatch).toBeCalled();
+  });
+});
