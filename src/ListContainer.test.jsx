85698008001e90bf857ee2a2e84cadd64a4297b2
Kwakcena <rhkrgudwh@gmail.com>
2020-06-26 19:28:23 +0900

    [Refactor] ListContainer 할 일 없을 경우
    할 일 없을 경우 테스트 추가

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -9,7 +9,7 @@ import ListContainer from './ListContainer';
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
-  it('할 일 목록을 보여준다.', () => {
+  context('할 일이 있으면', () => {
     useSelector.mockImplementation((selector) => selector({
       tasks: [
         { id: 1, title: 'New Task #1' },
@@ -17,9 +17,23 @@ describe('ListContainer', () => {
       ],
     }));
 
-    const { getByText } = render(<ListContainer />);
+    it('할 일 목록을 보여준다.', () => {
+      const { getByText } = render(<ListContainer />);
 
-    expect(getByText(/New Task #1/)).not.toBeNull();
-    expect(getByText(/New Task #2/)).not.toBeNull();
+      expect(getByText(/New Task #1/)).not.toBeNull();
+      expect(getByText(/New Task #2/)).not.toBeNull();
+    });
+  });
+
+  context('할 일이 없으면', () => {
+    it('빈 메세지를 출력한다.', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [],
+      }));
+
+      const { getByText } = render(<ListContainer />);
+
+      expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+    });
   });
 });
