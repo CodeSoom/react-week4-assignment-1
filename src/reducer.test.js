01410e0c6b247a9252bb161b283a4ba3aa7a3c24
SeaFalcon <seafalcon323@gmail.com>
2020-06-26 19:36:31 +0900

    addTask 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,6 +2,7 @@ import reducer from './reducer';
 
 import {
   updateTaskTitle,
+  addTask,
 } from './actions';
 
 describe('reducer', () => {
@@ -12,6 +13,18 @@ describe('reducer', () => {
       const state = reducer(previousState, updateTaskTitle('new title'));
 
       expect(state.taskTitle).toBe('new title');
+    });
+  });
+
+  describe('addTask', () => {
+    it('task가 추가된다.', () => {
+      const previousState = { tasks: [] };
+
+      const state = reducer(previousState, addTask());
+
+      expect(state.tasks).toHaveLength(1);
+      expect(state.tasks[0].id).not.toBeUndefined();
+      expect(state.tasks[0].title).toHaveLength(1);
     });
   });
 });
