3dd0005644d7a95c23fbee751034e8aff44728c0
Kwakcena <rhkrgudwh@gmail.com>
2020-06-27 02:05:19 +0900

    [Refactor] props 이름 수정, deleteTask test
    ListContainer의 onClickDelete props가 이름이 잘못 되어 전달이 제대로
    되지 않았다. 그래서 테스트가 계속 실패했다.
    
    이를 고치고 ListContainer에 추가하여 테스트를 통과하였다.

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
+
+import {
+  deleteTask,
+} from './actions';
 
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
   context('할 일이 있으면', () => {
+    const tasks = [
+      { id: 1, title: 'New Task #1' },
+      { id: 2, title: 'New Task #2' },
+    ];
+
     useSelector.mockImplementation((selector) => selector({
-      tasks: [
-        { id: 1, title: 'New Task #1' },
-        { id: 2, title: 'New Task #2' },
-      ],
+      tasks,
     }));
+
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
 
     it('할 일 목록을 보여준다.', () => {
       const { getByText } = render(<ListContainer />);
 
-      expect(getByText(/New Task #1/)).not.toBeNull();
-      expect(getByText(/New Task #2/)).not.toBeNull();
+      tasks.forEach(({ title }) => {
+        expect(getByText(title)).not.toBeNull();
+      });
+    });
+
+    it('할 일 목록을 삭제한다', () => {
+      const { getAllByText } = render(<ListContainer />);
+      const removeButtons = getAllByText(/완료/);
+
+      tasks.forEach(({ id }, index) => {
+        fireEvent.click(removeButtons[index]);
+        expect(dispatch).toBeCalledWith(deleteTask(id));
+      });
     });
   });
 
