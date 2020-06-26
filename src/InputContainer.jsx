20080a8cb8f0e0cc41e68d92d0fd38262c929c6d
SeaFalcon <seafalcon323@gmail.com>
2020-06-27 00:04:05 +0900

    InputContainer 테스트_2 구현

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { useDispatch } from 'react-redux';
+
 import Input from './Input';
 
+import {
+  updateTaskTitle,
+} from './actions';
+
 export default function InputContainer() {
+  const dispatch = useDispatch();
+
+  function handleChangeTitle(event) {
+    dispatch(updateTaskTitle(event.target.value));
+  }
+
   return (
-    <Input />
+    <Input
+      onChange={handleChangeTitle}
+    />
   );
 }
