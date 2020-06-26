b24493df01698b569c5368c59d5b802d502c152b
morykwon <kwonmory@gmail.com>
2020-06-26 17:44:59 +0900

    ListContainer 리팩토링
    - 이게 리팩토링인가봉가?

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 
 import { render, fireEvent } from "@testing-library/react";
-
 import { useSelector, useDispatch } from "react-redux";
 
 import ListContainer from "./ListContainer";
 
 jest.mock("react-redux");
 
+function mockSelector({ tasks }) {
+  useSelector.mockImplementation((selector) => selector({
+    newId: 1,
+    tasks
+  }));
+}
+
 describe("ListContainer", () => {
 
   context("with tasks", () => {
     it("exist task in tasks", () => {
-
-      useSelector.mockImplementation((selector) => selector({
-        newId: 1,
+      mockSelector({
         tasks: [
           {
             id: 1,
@@ -24,7 +28,7 @@ describe("ListContainer", () => {
             title: "너의 첫번째 임무다! #2"
           }
         ]
-      }));
+      });
 
       const { getByText, getAllByText } = render((
         <ListContainer/>
@@ -40,14 +44,14 @@ describe("ListContainer", () => {
 
       useDispatch.mockImplementation(() => dispatch);
 
-      useSelector.mockImplementation((selector) => selector({
+      mockSelector({
         tasks: [
           {
             id: 1,
             title: "너의 첫번째 임무다! #1"
           }
         ]
-      }));
+      });
 
       const { getByText } = render((
         <ListContainer/>
@@ -64,9 +68,9 @@ describe("ListContainer", () => {
   context("without tasks", () => {
     it("not exist task", () => {
 
-      useSelector.mockImplementation((selector) => selector({
+      mockSelector({
         tasks: []
-      }));
+      });
 
       const { getByText } = render((
         <ListContainer/>
