c6f0b4ef385e9e2c83b454c4f45dc74860bf0da0
eonsang <2eonhworking@gmail.com>
2020-06-26 19:41:50 +0900

    Feat: InputContainer에 taskTitle 변경 dispatch 함수 추가

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Input from './Input';
+import { changeTitleAction } from './actions';
 
-export default function InputContainer({ value, onChange, onClick }) {
-  return <Input value={value} onChange={onChange} onClick={onClick} />;
+export default function InputContainer({ value, onClick }) {
+  const dispatch = useDispatch();
+  const handleChangeTaskTitle = (e) => {
+    dispatch(changeTitleAction(e.target.value));
+  };
+
+  return (
+    <Input value={value} onChange={handleChangeTaskTitle} onClick={onClick} />
+  );
 }
