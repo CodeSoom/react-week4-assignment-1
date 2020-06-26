2372e36b9be165d46e68fef6dc6710ead8fd5e9c
Kwakcena <rhkrgudwh@gmail.com>
2020-06-26 17:51:08 +0900

    [RED] App component test
    App 컴포넌트는 리덕스를 사용하여 구현에 대한 관심사는 갖고 있지 않다.
    따라서 그에 맞게 테스트의 내용도 수정되어야 한다.
    
    useSelector로 tasks 목록을 가져와서 해당 목록의 title 값이 화면에
    보이는지 테스트를 수행한다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,70 +1,26 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 // TODO: 통합 테스트 코드 작성
 describe('App', () => {
-  function renderApp() {
-    const { container, getByText, getByPlaceholderText } = render(<App />);
-    return {
-      container,
-      input: getByPlaceholderText('할 일을 입력해 주세요'),
-      getByText,
-    };
-  }
+  it('할 일이 있으면 할 일 목록을 보여준다.', () => {
+    useSelector.mockImplementation((selector) => selector({
+      tasks: [
+        { id: 1, title: 'New Task #1' },
+        { id: 2, title: 'New Task #2' },
+      ],
+    }));
 
-  function changeValue(input, value) {
-    fireEvent.change(input, {
-      target: {
-        value,
-      },
-    });
-  }
+    const { getByText } = render(<App />);
 
-  it('title과 버튼을 화면에 보여준다.', () => {
-    const { getByText } = renderApp();
-
-    expect(getByText(/To-do/)).not.toBeNull();
-    expect(getByText(/추가/)).not.toBeNull();
-  });
-
-  context('할 일을 입력하면', () => {
-    it('value가 변한다.', () => {
-      const { input } = renderApp();
-
-      changeValue(input, 'New Task');
-
-      expect(input.value).toBe('New Task');
-    });
-  });
-
-  context('할 일을 추가하면', () => {
-    it('목록에 추가되고 value는 비어진다.', () => {
-      const { container, input, getByText } = renderApp();
-
-      changeValue(input, 'New Task');
-
-      fireEvent.click(getByText(/추가/));
-      expect(container).toHaveTextContent(/New Task/);
-      expect(input.value).toBe('');
-    });
-  });
-
-  context('할 일을 삭제하면', () => {
-    it('목록에서 사라진다.', () => {
-      const { getByText, getByPlaceholderText } = render(<App />);
-      const input = getByPlaceholderText('할 일을 입력해 주세요');
-
-      changeValue(input, 'New Task');
-
-      fireEvent.click(getByText(/추가/));
-      const task = getByText(/New Task/);
-      const removeButton = getByText(/완료/);
-
-      fireEvent.click(removeButton);
-      expect(task).not.toBeInTheDocument();
-    });
+    expect(getByText(/New Task #1/)).not.toBeNull();
+    expect(getByText(/New Task #2/)).not.toBeNull();
   });
 });
