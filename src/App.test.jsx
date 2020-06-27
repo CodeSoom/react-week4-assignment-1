e24228d34e2a4f5ba7df07318f0bd78439affc28
jiunkoo <jiunkoo0106@gmail.com>
2020-06-27 18:14:40 +0900

    App - handleClickAddTask, handleClickDeleteTask 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
+const renderApp = () => {
+  const { container, getByPlaceholderText, getByText } = render(
+    <App />,
+  );
 
-  expect(getByText(/추가/)).not.toBeNull();
+  return {
+    container,
+    getByPlaceholderText,
+    getByText,
+  };
+};
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+describe('App', () => {
+  test('handleClickAddTask', () => {
+    const todo = 'Handle Click Add Task!';
+    const { container, getByPlaceholderText, getByText } = renderApp();
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    const button = getByText('추가');
+
+    fireEvent.change(input, { target: { value: todo } });
+
+    expect(input).toHaveAttribute('value', todo);
+
+    fireEvent.click(button);
+
+    expect(input).toHaveAttribute('value', '');
+    expect(container).toHaveTextContent(todo);
+  });
+
+  test('handleClickDeleteTask', () => {
+    const todo = 'Handle Click Delete Task';
+    const { container, getByPlaceholderText, getByText } = renderApp();
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    const addButton = getByText('추가');
+
+    fireEvent.change(input, { target: { value: todo } });
+    fireEvent.click(addButton);
+
+    const completeButton = getByText('완료');
+
+    expect(container).toHaveTextContent(todo);
+
+    fireEvent.click(completeButton);
+
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 });
