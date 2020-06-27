f7f5ee94148b04b74812e4b3e07d0c54a4d9e04d
jiunkoo <jiunkoo0106@gmail.com>
2020-06-27 19:38:37 +0900

    ListContainer - handleClickDeleteTask 테스트 구현

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,5 +1,27 @@
-const ListContainer = () => {
+import React from 'react';
 
-};
+import { useSelector, useDispatch } from 'react-redux';
 
-export default ListContainer;
+import List from './List';
+
+import {
+  deleteTask,
+} from './actions';
+
+export default function ListContainer() {
+  const { tasks } = useSelector((state) => ({
+    tasks: state.tasks,
+  }));
+  const dispatch = useDispatch();
+
+  function handleClickDeleteTask(id) {
+    dispatch(deleteTask(id));
+  }
+
+  return (
+    <List 
+      tasks={tasks}
+      onClickDelete={handleClickDeleteTask}
+    />
+  );
+}
