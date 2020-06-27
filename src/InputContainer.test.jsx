a17d0181d0661dd0057f191869875c1697e8475b
jiunkoo <jiunkoo0106@gmail.com>
2020-06-27 20:15:57 +0900

    InputContainer - handleChangeTitle 테스트 추가

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,4 +1,36 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
 import InputContainer from './InputContainer';
 
+jest.mock('react-redux');
+
 describe('InputContainer', () => {
+  test('handleChangeTitle', () => {
+    const dispatch = jest.fn();
+  
+    useSelector.mockImplementation((selector) => selector({
+      taskTitle: '',
+    }));
+    useDispatch.mockImplementation(() => dispatch);
+  
+    const { getByPlaceholderText } = render((
+      <InputContainer />
+    ));
+
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+
+    fireEvent.change(input, { target: { value: 'Handle Change Title' } });
+
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toBeCalledWith({
+      type: 'updateTaskTitle',
+      payload: {
+        taskTitle: 'Handle Change Title',
+      },
+    });
+  });
 });
