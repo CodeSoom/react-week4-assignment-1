bacd14499e6147a90ac4528dbd504e87dcdf1275
jiunkoo <jiunkoo0106@gmail.com>
2020-06-27 18:23:47 +0900

    actions - addTask Action 테스트 추가

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,4 +1,4 @@
-import { updateTaskTitle } from './actions';
+import { updateTaskTitle, addTask } from './actions';
 
 describe('actions', () => {
   test('updateTaskTitle Action', () => {
@@ -8,5 +8,12 @@ describe('actions', () => {
 
     expect(action.type).toBe('updateTaskTitle');
     expect(action.payload.title).toBe(taskTitle);
+  });
+
+  test('addTask Action', () => {
+    const action = addTask();
+
+    expect(action.type).toBe('addTask');
+    expect(action.payload).toBeNull();
   });
 });
