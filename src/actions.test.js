3ae5255f77bb109d6c8e3e0ab5aa7e4067e15e15
jiunkoo <jiunkoo0106@gmail.com>
2020-06-27 18:19:54 +0900

    actions - updateTaskTitle Action 테스트 추가

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,4 +1,12 @@
-import actions from './actions';
+import { updateTaskTitle } from './actions';
 
 describe('actions', () => {
+  test('updateTaskTitle Action', () => {
+    const taskTitle = 'Update Task Title Action!';
+
+    const action = updateTaskTitle(taskTitle);
+
+    expect(action.type).toBe('updateTaskTitle');
+    expect(action.payload.title).toBe(taskTitle);
+  });
 });
