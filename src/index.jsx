9d9328b6f711dcaa857e72ba1d6e2d9cceb6993d
Paul An <anpaul0615@gmail.com>
2020-06-28 22:31:00 +0900

    store 명칭 변경

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import RootStore from './stores';
+import TodoStore from './stores';
 
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={RootStore}>
+  <Provider store={TodoStore}>
     <App />
   </Provider>,
   document.getElementById('app'),
