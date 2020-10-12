4758d2f45419ec4aab05bf0a78b7bb6eb4dbe319
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-12 21:28:54 +0900

    reducer.jsx에 테스트를 추가한다.
    reducer.jsx에 addTask 테스트를 추가한다.

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -18,4 +18,38 @@ describe('reducer', () => {
       expect(state.taskTitle).toBe('New title');
     });
   });
+
+  describe('addTask', () => {
+    it('appends task into tasks', () => {
+      const previousState = {
+        taskTitle: '첫번째 할 일',
+        tasks: [],
+      };
+
+      const action = {
+        type: 'addTask',
+      };
+
+      const state = reducer(previousState, action);
+
+      expect(state.tasks).toHaveLength(1);
+      expect(state.tasks[0].title).toBe('첫번째 할 일');
+      expect(state.tasks[0].id).not.toBeUndefined();
+    });
+
+    it('clear task title', () => {
+      const previousState = {
+        taskTitle: '첫번째 할 일',
+        tasks: [],
+      };
+
+      const action = {
+        type: 'addTask',
+      };
+
+      const state = reducer(previousState, action);
+
+      expect(state.taskTitle).toBe('');
+    });
+  });
 });
