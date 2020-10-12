8c929329f7400424c4b0f8484c8055c13bddd1a5
Dong A Kim <dongakim90@gmail.com>
2020-10-12 19:36:03 +0900

    App의 Task 입력시 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
+describe('App', () => {
   const { getByText } = render((
     <App />
   ));
@@ -13,4 +13,14 @@ test('App', () => {
 
   // TODO: 통합 테스트 코드 작성
   // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+
+  it('Task 타이핑시 입력된 값이 출련된다.', () => {
+    // When
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: '청소하기' },
+    });
+
+    // Then
+    expect(screen.getByRole('textbox')).toHaveValue('청소하기');
+  });
 });
