1e72c004591a29559502f1ae992db28db9c69de4
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-12 20:21:45 +0900

    reducer를 구현한다.
    reducer를 빠르게 구현한다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app'),
 );
---
+++ src/reducer.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+function reducer(state, action) {
+  return { taskTitle: 'New title' };
+}
+
+export default reducer;
