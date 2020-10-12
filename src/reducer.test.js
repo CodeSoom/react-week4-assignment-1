db5eb6fc0fd4f3cfb309d98adb246adaaf0822d7
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-12 20:06:52 +0900

    Add updateTaskTitle test to reducer

---
+++ src/reducer.test.js
@@ -0,0 +1,18 @@
+describe('reducer', () => {
+  describe('updateTaskTitle', () => {
+    it('returns new state with new task title', () => {
+      const previousState = {
+        taskTitle: '',
+      };
+
+      const action = {
+        type: 'updateTaskTitle',
+        payload: 'New Task',
+      };
+
+      const newState = reducer(previousState, action);
+
+      expect(newState.taskTitle).toBe('New Task');
+    });
+  });
+});
