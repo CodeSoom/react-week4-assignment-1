57e2c151b423c2976cd9af05549a706f97281e11
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-10-13 21:50:26 +0900

    Fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
 import Page from './Page';
 
-import {
-  updateTaskTitle,
-  addTask,
-} from './actions';
-
 export default function App() {
-  const { taskTitle } = useSelector((state) => ({
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  }));
-
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
 
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <Page />
   );
 }
