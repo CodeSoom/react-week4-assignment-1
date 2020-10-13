6a834d1b6119162b08d706880cad18f975b1ca0f
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-13 14:48:44 +0900

    App에서 필요없는 부분을 모두 제거해준다.
    필요한 부분만 남기고 모두 제거해준다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
 import Page from './Page';
 
-import {
-  addTask,
-  updateTaskTitle,
-} from './actions';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-  };
-}
-
 export default function App() {
-  const dispatch = useDispatch();
-
-  const { taskTitle } = useSelector(selector);
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <Page />
   );
 }
