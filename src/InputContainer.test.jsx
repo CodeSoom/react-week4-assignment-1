f27e7cbb7885d3a5782c351005f0a93dad9642d2
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-10-13 21:43:42 +0900

    Add InputContainer.test.jsx

---
+++ src/InputContainer.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import InputContainer from './InputContainer';
+
+jest.mock('react-redux');
+
+test('InputContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    taskTitle: 'New Title',
+  }));
+
+  const { getByText } = render((
+    <InputContainer />
+  ));
+
+  expect(getByText(/New Title/)).not.toBeNull();
+  expect(getByText(/추가/)).not.toBeNull();
+});
