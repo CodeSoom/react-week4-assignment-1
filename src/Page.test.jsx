a725905f3984419f26872523569aa7a34400bf1d
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-13 14:46:37 +0900

    Page와 Page의 테스트를 리팩토링한다.
    필요하지 않은 부분을 제거해준다.

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,38 +1,29 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
 import Page from './Page';
 
 test('Page', () => {
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTask = jest.fn();
-
-  jest.mock('react-redux');
-
+  const taskTitle = '';
   const tasks = [
     { id: 1, title: 'Task-1' },
     { id: 2, title: 'Task-2' },
   ];
 
+  jest.mock('react-redux');
+
   useSelector.mockImplementation((selector) => selector({
+    taskTitle,
     tasks,
   }));
 
   const { getByText } = render((
-    <Page
-      taskTitle=""
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <Page />
   ));
 
   expect(getByText(/Task-1/)).not.toBeNull();
   expect(getByText(/Task-2/)).not.toBeNull();
-
-  fireEvent.click(getByText('추가'));
-
-  expect(handleClickAddTask).toBeCalled();
 });
