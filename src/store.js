76cc2c3d48c18b936c64a4198e709e29f659ef4b
juunini <juuni.ni.i@gmail.com>
2020-10-14 07:36:30 +0900

    Write after watching video lecture

---
+++ src/store.js
@@ -0,0 +1,11 @@
+﻿import { createStore } from 'redux';
+
+import reducer from './reducer';
+
+// Redux action
+// - type (string)
+// - payload => object => { taskTitle }
+
+const store = createStore(reducer);
+
+export default store;
