ed83c90dd4f4a9deaacab5816eff8b8342b5856a
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-13 15:55:06 +0900

    Add addTask test

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,6 +2,7 @@ import reducer from './reducer';
 
 import {
   updateTaskTitle,
+  addTask,
 } from './actions';
 
 describe('reducer', () => {
@@ -12,6 +13,39 @@ describe('reducer', () => {
       }, updateTaskTitle('입력 값'));
 
       expect(state.taskTitle).toBe('입력 값');
+    });
+  });
+  
+  describe('addTask', () => {
+    function reduceAddTask(taskTitle) {
+      return reducer({
+        newId: 100,
+        taskTitle,
+        tasks: [],
+      }, addTask());
+    }
+    context('with task title', () => {
+      it('appends a new task into tasks', () => {
+        const state = reduceAddTask('New Task');
+
+        expect(state.tasks).toHaveLength(1);
+        expect(state.tasks[0].id).not.toBeUndefined();
+        expect(state.tasks[0].title).toBe('New Task');
+      });
+
+      it('clears task title', () => {
+        const state = reduceAddTask('New Task');
+
+        expect(state.taskTitle).toBe('');
+      });
+    });
+
+    context('without task title', () => {
+      it("doesn't work", () => {
+        const state = reduceAddTask('');
+
+        expect(state.tasks).toHaveLength(0);
+      });
     });
   });
 });
