2627b0fe7845e2f6236321ba6a0cae2343c0aa0e
oceanide <oceanide@gmail.com>
2020-10-14 08:55:52 +0900

    Define useDispatch and useSelector as mocks

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
+
+jest.mock('react-redux');
 
 test('App', () => {
   const { getByText } = render((
