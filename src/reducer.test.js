d752fba9255bdda565c4d9875a974e7457bbe79e
davidyang2149 <davidyang2149@gmail.com>
2020-10-14 00:11:32 +0900

    Refactor reducer.js, reducer.test.js Update func
    4강 리덕스 - 4주 차 과제 1
    - reducer.text.js 테스트 과정에서 기능 오류 발견에 따른 원본 수정.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -21,21 +21,36 @@ describe('reducer', () => {
     context('with task title', () => {
       it('appends a new task into tasks', () => {
         const state = reducer({
+          newId: 100,
           taskTitle: 'New Task',
           tasks: [],
         }, addTask());
 
         expect(state.tasks).toHaveLength(1);
+        expect(state.tasks[0].id).not.toBeUndefined();
         expect(state.tasks[0].title).toBe('New Task');
       });
 
       it('clears task title', () => {
         const state = reducer({
+          newId: 100,
           taskTitle: 'New Task',
           tasks: [],
         }, addTask());
 
         expect(state.taskTitle).toBe('');
+      });
+    });
+
+    context('without task title', () => {
+      it('doesn\'t work', () => {
+        const state = reducer({
+          newId: 100,
+          taskTitle: '',
+          tasks: [],
+        }, addTask());
+
+        expect(state.tasks).toHaveLength(0);
       });
     });
   });
