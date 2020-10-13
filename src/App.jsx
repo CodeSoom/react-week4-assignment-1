980590707b304e87dbb922a34db2634b0792ce3f
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-13 22:32:14 +0900

    Refactor App Comonent
    to use useSelector, useDispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import reducer from './reducer';
 import { updateTaskTitle, addTask, deleteTask } from './actions';
 
 import Page from './Page';
 
 export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
+  const { tasks, taskTitle } = useSelector((state) => ({
+    taskTitle: state.taskTitle,
+    tasks: state.tasks,
+  }));
+  const dispatch = useDispatch();
 
   function handleChangeTitle(event) {
-    setState(reducer(state, updateTaskTitle(event.target.value)));
+    dispatch(updateTaskTitle(event.target.value));
   }
 
   function handleClickAddTask() {
-    setState(reducer(state, addTask()));
+    dispatch(addTask());
   }
 
   function handleClickDeleteTask(id) {
-    setState(reducer(state, deleteTask(id)));
+    dispatch(deleteTask(id));
   }
 
   return (
     <Page
-      taskTitle={state?.taskTitle}
+      taskTitle={taskTitle}
       onChangeTitle={handleChangeTitle}
       onClickAddTask={handleClickAddTask}
-      tasks={state?.tasks}
+      tasks={tasks}
       onClickDeleteTask={handleClickDeleteTask}
     />
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './App';
 
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app'),
 );
