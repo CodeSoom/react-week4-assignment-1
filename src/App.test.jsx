c3ce16f83edaaf55fa29e260c4b12abf46e93b00
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-13 21:56:00 +0900

    Add test to App Component
    to use redux

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
+import { mockComponent } from 'react-dom/test-utils';
 
 describe('App', () => {
-  it('renders elements correctly', () => {
-    const { getByText, getByLabelText } = render((
-      <App />
-    ));
+  context('with initial state', () => {
+    const initialState = ({
+      tasks: [
+        { id: 101, title: 'TASK-01' },
+      ],
+    });
 
-    expect(getByText(/추가/)).not.toBeNull();
-    expect(getByLabelText(/할 일/)).not.toBeNull();
+    useSelector.mockImplementation((selector) => selector(initialState));
+
+    it('renders elements correctly', () => {
+      const { getByText, getByLabelText } = render((
+        <App />
+      ));
+
+      expect(getByText(/TASK-01/)).not.toBeNull();
+      expect(getByText(/추가/)).not.toBeNull();
+      expect(getByLabelText(/할 일/)).not.toBeNull();
+    });
   });
 
   context('when user change task title', () => {
