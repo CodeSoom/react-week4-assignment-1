0ae577fdb7b854c1833777e7aeedc24c925b2837
wjy5446 <wjy5446@gmail.com>
2020-10-13 22:14:00 +0900

    feat) redux를 이용한 App Component 개발

---
+++ src/actions.js
@@ -0,0 +1,28 @@
+// action creator
+export function updateTaskTitle(taskTitle) {
+  return {
+    type: 'updateTaskTitle',
+    payload: { taskTitle },
+  };
+}
+
+export function addTask(state) {
+  const { newId, taskTitle, tasks } = state;
+
+  return {
+    ...state,
+    newId: newId + 1,
+    taskTitle: '',
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  };
+}
+
+export function deleteTask(state, id) {
+
+  const { tasks } = state;
+
+  return {
+    ...state,
+    tasks: tasks.filter((task) => task.id !== id),
+  };
+}
