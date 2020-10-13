cca6aa938517b4a441ab3cb0bd3d77b82de83e9c
Noah <mike1124@naver.com>
2020-10-13 21:34:11 +0900

    🔥 Remove all fixed code / initialize code
    App.jsx 관심사 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import InputContainer from './InputContainer';
-import ListContainer from './ListContainer';
+import Page from './Page';
+
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+function updateTaskTitle(state, taskTitle) {
+  return {
+    ...state,
+    taskTitle,
+  };
+}
+
+function addTask(state) {
+  const { newId, tasks, taskTitle } = state;
+  return {
+    ...state,
+    newId: newId + 1,
+    taskTitle: '',
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  };
+}
+
+function deleteTask(state, id) {
+  const { tasks } = state;
+  return {
+    ...state,
+    tasks: tasks.filter((task) => task.id !== id),
+  };
+}
 
 export default function App() {
+  const [state, setState] = useState(initialState);
+
+  const { taskTitle, tasks } = state;
+
+  function handleChangeTitle(event) {
+    setState(updateTaskTitle(state, event.target.value));
+  }
+
+  function handleClickAddTask() {
+    setState(addTask(state));
+  }
+
+  function handleClickDeleteTask(id) {
+    setState(deleteTask(state, id));
+  }
+
   return (
-    <div>
-      <h1>To-do</h1>
-      <InputContainer />
-      <ListContainer />
-    </div>
+    <Page
+      taskTitle={taskTitle}
+      onChangeTitle={handleChangeTitle}
+      onClickAddTask={handleClickAddTask}
+      tasks={tasks}
+      onClickDeleteTask={handleClickDeleteTask}
+    />
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,33 +2,15 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
-
 import App from './App';
 
-jest.mock('react-redux');
-
 test('App', () => {
-  const tasks = [
-    {
-      id: 1,
-      title: '아무것도 하지않기1',
-    },
-    {
-      id: 2,
-      title: '아무것도 하지않기2',
-    },
-  ];
-
-  useSelector.mockImplementation((selector) => selector({
-    taskTitle: '',
-    tasks,
-  }));
-
   const { getByText } = render((
     <App />
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
-  expect(getByText(/아무것도 하지않기1/)).not.toBeNull();
+
+  // TODO: 통합 테스트 코드 작성
+  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
