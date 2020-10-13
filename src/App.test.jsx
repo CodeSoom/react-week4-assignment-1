ab9e712546ca6431c7794c2aa7ea1888c0d8acfd
saseungmin <dbd02169@naver.com>
2020-10-13 22:48:55 +0900

    App.jsx 에 대한 리팩토링 후 테스트 추가
    - App.jsx의 관심사 분리
    - tasks의 값 추가 후 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,12 +5,13 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('App', () => {
-  const { getByText } = render((
+  const { getByText, getAllByText } = render((
     <App />
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  getAllByText(/아무 것도 하지 않기/).forEach((task) => {
+    expect(task).not.toBeNull();
+  });
 });
