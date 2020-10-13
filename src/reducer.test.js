365799f9f99a3320bd257ae0e85bdea9edcda775
Dong A Kim <dongakim90@gmail.com>
2020-10-13 19:14:38 +0900

    App Task 추가 reducer 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,6 @@
 import reducer from './reducer';
 
-import actions, { CHANGE_TITLE } from './actions';
+import actions, { CHANGE_TITLE, ADD_TASK } from './actions';
 
 describe('reducer', () => {
   describe(CHANGE_TITLE, () => {
@@ -12,6 +12,20 @@ describe('reducer', () => {
       const state = reducer(previousState, action);
 
       expect(state.taskTitle).toBe('밥먹기');
+    });
+  });
+
+  describe(ADD_TASK, () => {
+    it('add Task to the list', () => {
+      const previousState = {
+        newId: 100,
+        taskTitle: '독서하기',
+        tasks: [],
+      };
+      const action = actions.addTask();
+      const state = reducer(previousState, action);
+
+      expect(state.tasks).toMatchObject([{ id: 100, title: '독서하기' }]);
     });
   });
 });
