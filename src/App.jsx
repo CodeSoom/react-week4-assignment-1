7f7d3fec495793246f8213a11921fb7f561428f6
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-14 20:19:30 +0900

    Fix App test
    couldnt use integration test, because
    I needed to configure redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 
-import { updateTaskTitle, addTask, deleteTask } from './actions';
-
-import Page from './Page';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const { tasks, taskTitle } = useSelector((state) => ({
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  }));
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,79 +1,21 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
 
-describe('App', () => {
-  context('with initial state', () => {
-    const initialState = ({
-      tasks: [
-        { id: 101, title: 'TASK-01' },
-      ],
-    });
+test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [
+      { id: 101, title: 'TASK-01' },
+      { id: 102, title: 'TASK-02' },
+    ],
+  }));
 
-    useSelector.mockImplementation((selector) => selector(initialState));
+  const { getByText, getByLabelText } = render((<App />));
 
-    it('renders elements correctly', () => {
-      const { getByText, getByLabelText } = render((
-        <App />
-      ));
-
-      expect(getByText(/TASK-01/)).not.toBeNull();
-      expect(getByText(/추가/)).not.toBeNull();
-      expect(getByLabelText(/할 일/)).not.toBeNull();
-    });
-  });
-
-  context('when user change task title', () => {
-    const newTitle = 'New Task';
-    it('input has the title', () => {
-      const { getByLabelText } = render(<App />);
-      const input = getByLabelText(/할 일/);
-
-      expect(input).not.toHaveValue(newTitle);
-
-      fireEvent.change(input, { target: { value: newTitle } });
-
-      expect(input).toHaveValue(newTitle);
-    });
-  });
-
-  context('when user click addButton', () => {
-    it('added task appears on the screen', () => {
-      const newTitle = 'New Task';
-      const { getByText, getByLabelText, queryByText } = render(<App />);
-      const input = getByLabelText(/할 일/);
-      const addButton = getByText(/추가/);
-
-      fireEvent.change(input, { target: { value: newTitle } });
-
-      expect(queryByText(newTitle)).toBeNull();
-      expect(queryByText(/완료/)).toBeNull();
-
-      fireEvent.click(addButton);
-
-      expect(queryByText(newTitle)).not.toBeNull();
-      expect(queryByText(/완료/)).not.toBeNull();
-    });
-  });
-
-  context('when user click deleteButton', () => {
-    it('deleted task disappears out of screen', () => {
-      const taskTitle = 'TASK-01';
-      const { queryByText, getByText, getByLabelText } = render(<App />);
-      const input = getByLabelText(/할 일/);
-      const addButton = getByText(/추가/);
-
-      fireEvent.change(input, { target: { value: taskTitle } });
-      fireEvent.click(addButton);
-
-      expect(queryByText(taskTitle)).not.toBeNull();
-
-      fireEvent.click(getByText(/완료/));
-
-      expect(queryByText(taskTitle)).toBeNull();
-    });
-  });
+  expect(getByText(/TASK-01/)).not.toBeNull();
+  expect(getByText(/추가/)).not.toBeNull();
+  expect(getByLabelText(/할 일/)).not.toBeNull();
 });
