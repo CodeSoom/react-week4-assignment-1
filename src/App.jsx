506d2e5ae565b8967a4dbecfdf9872e7a42c8dc0
Seungmin Lee <seungmin@uxplus.dev>
2020-10-14 13:41:48 +0900

    Refactor code
    Refactor code to separate concerns.

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,52 @@ import React, { useState } from 'react';
 
 import Page from './Page';
 
-export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+function changeTaskTitle(state, value) {
+  return {
+    ...state,
+    taskTitle: value,
+  };
+}
+
+function addTask(state) {
+  const { newId, taskTitle, tasks } = state;
+
+  return {
+    ...state,
+    newId: newId + 1,
     taskTitle: '',
-    tasks: [],
-  });
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  };
+}
+
+function deleteTask(state, id) {
+  return {
+    ...state,
+    tasks: tasks.filter((task) => task.id !== id),
+  };
+}
+
+export default function App() {
+  const [state, setState] = useState(initialState);
 
   const { newId, taskTitle, tasks } = state;
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+    setState(changeTaskTitle(event.target.value));
   }
 
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
+    setState(addTask());
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
+    setState(deleteTask(id));
   }
 
   return (
