db0c1be6056f785ab8cc2705a212f23a7c60ce7f
saseungmin <dbd02169@naver.com>
2020-10-14 23:34:50 +0900

    Reducer test refactoring

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,92 +1,66 @@
 import reducer from './reducer';
 
+import { updateTaskTitle, deleteTask, addTask } from './actions';
+
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
     it('할 일을 입력한다.', () => {
-      const initialState = {
+      const taskValue = '아무것도 하지 않기';
+
+      const state = reducer({
         taskTitle: '',
-      };
+      }, updateTaskTitle(taskValue));
 
-      const action = {
-        type: 'updateTaskTitle',
-        payload: {
-          taskTitle: '아무것도 하지 않기',
-        },
-      };
-
-      const state = reducer(initialState, action);
-
-      expect(state.taskTitle).toBe('아무것도 하지 않기');
+      expect(state.taskTitle).toBe(taskValue);
     });
   });
 
   describe('addTask', () => {
-    context('with taskTitle', () => {
-      const initialState = {
-        newId: 3,
-        taskTitle: '아무것도 하지 않기',
-        tasks: [
-          { id: 1, title: '아무 것도 하지 않기 1' },
-          { id: 2, title: '아무 것도 하지 않기 2' },
-        ],
-      };
-      it('할 일을 추가한다.', () => {
-        const action = {
-          type: 'addTask',
-        };
+    const addReducer = (taskTitle) => reducer({
+      newId: 3,
+      taskTitle,
+      tasks: [
+        { id: 1, title: '아무 것도 하지 않기 1' },
+        { id: 2, title: '아무 것도 하지 않기 2' },
+      ],
+    }, addTask());
 
-        const state = reducer(initialState, action);
+    context('with taskTitle', () => {
+      it('할 일을 추가한다.', () => {
+        const state = addReducer('아무 것도 하지 않기');
 
         expect(state.tasks).toHaveLength(3);
         expect(state.tasks[0].id).not.toBeUndefined();
       });
       it('할 일을 추가 후 "taskTitle" 비워지는지 확인한다.', () => {
-        const action = {
-          type: 'addTask',
-        };
-
-        const state = reducer(initialState, action);
+        const state = addReducer('아무 것도 하지 않기');
 
         expect(state.taskTitle).toBe('');
       });
     });
 
     context('without taskTitle', () => {
-      const initialState = {
-        newId: 1,
-        taskTitle: '',
-        tasks: [],
-      };
-
       it('할 일이 추가되지 않는다.', () => {
-        const action = {
-          type: 'addTask',
-        };
+        const state = addReducer('');
 
-        const state = reducer(initialState, action);
-
-        expect(state.tasks).toHaveLength(0);
+        expect(state.tasks).toHaveLength(2);
       });
     });
   });
 
   describe('deleteTask', () => {
+    const deleteReducer = (id) => reducer({
+      tasks: [
+        {
+          id,
+          title: '아무 것도 하지 않기 1',
+        },
+      ],
+    }, deleteTask(1));
+
     context('with task id', () => {
       it('할 일을 삭제한다.', () => {
-        const initialState = {
-          tasks: [
-            { id: 1, title: '아무 것도 하지 않기 1' },
-          ],
-        };
-
-        const action = {
-          type: 'deleteTask',
-          payload: {
-            id: 1,
-          },
-        };
-
-        const state = reducer(initialState, action);
+        const state = deleteReducer(1);
 
         expect(state.tasks).toHaveLength(0);
       });
@@ -94,20 +68,7 @@ describe('reducer', () => {
 
     context('without task id', () => {
       it('할 일이 삭제되지 않는다.', () => {
-        const initialState = {
-          tasks: [
-            { id: 1, title: '아무 것도 하지 않기 1' },
-          ],
-        };
-
-        const action = {
-          type: 'deleteTask',
-          payload: {
-            id: 2,
-          },
-        };
-
-        const state = reducer(initialState, action);
+        const state = deleteReducer(2);
 
         expect(state.tasks).toHaveLength(1);
       });
