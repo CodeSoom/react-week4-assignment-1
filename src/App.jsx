b7c7cbc1e062ba7c2ff20da01d0895d8c26feba1
oceanide <oceanide@gmail.com>
2020-10-14 16:55:17 +0900

    Add ListContainer and ListContainer.test.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ import Page from './Page';
 import {
   updateTaskTitle,
   addTask,
-  deleteTask,
 } from './actions';
 
 export default function App() {
   const dispatch = useDispatch();
 
-  const { taskTitle, tasks } = useSelector((state) => ({
+  const { taskTitle } = useSelector((state) => ({
     taskTitle: state.taskTitle,
     tasks: state.tasks,
   }));
@@ -26,17 +25,11 @@ export default function App() {
     dispatch(addTask());
   }
 
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
     <Page
       taskTitle={taskTitle}
       onChangeTitle={handleChangeTitle}
       onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
     />
   );
 }
---
+++ src/ListContainer.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import ListContainer from './ListContainer';
+
+jest.mock('react-redux');
+
+test('ListContainer', () => {
+  const tasks = [
+    { id: 1, title: '아무 것도 하지 않기 #1' },
+    { id: 2, title: '아무 것도 하지 않기 #2' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
+  const { getByText } = render((
+    <ListContainer />
+  ));
+
+  expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+
+  // TODO: 통합 테스트 코드 작성
+  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+});
