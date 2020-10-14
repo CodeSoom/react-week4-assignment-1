ba1d1a3cd38248ac272ddb32d99696e30d20d807
saseungmin <dbd02169@naver.com>
2020-10-14 22:59:27 +0900

    reducer addTask에 대한 테스트 구현

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,20 +1,43 @@
 import reducer from './reducer';
 
 describe('reducer', () => {
-  describe('update task title', () => {
+  describe('updateTaskTitle', () => {
     it('할 일을 입력한다.', () => {
       const initialState = {
         taskTitle: '',
       };
+
       const action = {
         type: 'updateTaskTitle',
         payload: {
           taskTitle: '아무것도 하지 않기',
         },
       };
+
       const state = reducer(initialState, action);
 
       expect(state.taskTitle).toBe('아무것도 하지 않기');
+    });
+  });
+
+  describe('addTask', () => {
+    it('할 일을 추가한다.', () => {
+      const initialState = {
+        newId: 3,
+        taskTitle: '아무것도 하지 않기',
+        tasks: [
+          { id: 1, title: '아무 것도 하지 않기 1' },
+          { id: 2, title: '아무 것도 하지 않기 2' },
+        ],
+      };
+
+      const action = {
+        type: 'addTask',
+      };
+
+      const state = reducer(initialState, action);
+
+      expect(state.tasks).toHaveLength(3);
     });
   });
 });
