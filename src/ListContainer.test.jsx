bee8db91c243fbcfe2ea1a6682855c3a7a99ae19
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-14 20:43:27 +0900

    Add test to ListContainer
    to check if deleteTask event happens correctly

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import ListContainer from './ListContainer';
 
-describe('ListContainer', () => {
-  test('renders elements correctly', () => {
-    const initialState = ({
-      tasks: [
-        { id: 101, title: 'TASK-01' },
-        { id: 101, title: 'TASK-02' },
-      ],
-    });
+test('ListContainer', () => {
+  const dispatch = jest.fn();
 
-    useSelector.mockImplementation((selector) => selector(initialState));
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [
+      { id: 101, title: 'TASK-01' },
+      { id: 102, title: 'TASK-02' },
+    ],
+  }));
 
-    const { getByText } = render((
-      <ListContainer />
-    ));
+  useDispatch.mockImplementation(() => dispatch);
 
-    expect(getByText(/TASK-01/)).not.toBeNull();
-    expect(getByText(/TASK-02/)).not.toBeNull();
+  const { getByText, getAllByText } = render((
+    <ListContainer />
+  ));
+
+  expect(getByText(/TASK-01/)).not.toBeNull();
+  expect(getByText(/TASK-02/)).not.toBeNull();
+
+  fireEvent.click(getAllByText(/완료/)[0]);
+
+  expect(dispatch).toBeCalledWith({
+    type: 'deleteTask',
+    payload: {
+      id: 101,
+    },
   });
 });
