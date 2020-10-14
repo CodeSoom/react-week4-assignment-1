4067aacf56e139fecb955d0dc1ab0bc11a8e0bf1
Noah <mike1124@naver.com>
2020-10-14 19:22:20 +0900

    ♻️ Add test code about reducer.js and Refactor

--- src/reducer.js
+++ src/reducer.js
@@ -4,32 +4,35 @@ const initialState = {
   tasks: [],
 };
 
+const actionType = {
+  updateTaskTitle: (state, action) => ({
+    ...state,
+    taskTitle: action.payload.taskTitle,
+  }),
+
+  addTask: (state) => ({
+    ...state,
+    newId: state.newId + 1,
+    taskTitle: '',
+    tasks: [...state.tasks, { id: state.newId, title: state.taskTitle }],
+  }),
+
+  deleteTask: (state, action) => ({
+    ...state,
+    tasks: state.tasks.filter((task) => task.id !== action.payload.id),
+  }),
+};
+
 export default function reducer(state = initialState, action) {
-  if (action.type === 'updateTaskTitle') {
-    return {
-      ...state,
-      taskTitle: action.payload.taskTitle,
-    };
-  }
+  const { taskTitle } = state;
 
-  if (action.type === 'addTask') {
-    const { newId, tasks, taskTitle } = state;
+  const { type } = action;
 
-    return {
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    };
-  }
+  if (action.type === 'updateTaskTitle') return actionType[type](state, action);
 
-  if (action.type === 'deleteTask') {
-    const { tasks } = state;
+  if (action.type === 'addTask') return taskTitle ? actionType[type](state) : state;
 
-    return {
-      ...state,
-      tasks: tasks.filter((task) => task.id !== action.payload.id),
-    };
-  }
+  if (action.type === 'deleteTask') return actionType[type](state, action);
+
   return state;
 }
---
+++ src/reducer.test.js
@@ -0,0 +1,76 @@
+import reducer from './reducer';
+
+import { updateTaskTitle, addTask, deleteTask } from './actions';
+
+describe('reducer', () => {
+  describe('updateTaskTitle', () => {
+    it('changes task title', () => {
+      const state = reducer({
+        taskTitle: '',
+      }, updateTaskTitle('New title'));
+
+      expect(state.taskTitle).toBe('New title');
+    });
+  });
+
+  describe('addTask', () => {
+    const addReducer = (taskTitle) => reducer({
+      newId: 100,
+      taskTitle,
+      tasks: [],
+    }, addTask());
+
+    context('with exist a new task title', () => {
+      it('appends new task into tasks', () => {
+        const state = addReducer('New task');
+
+        expect(state.tasks).toHaveLength(1);
+
+        expect(state.tasks[0].title).toBe('New task');
+        expect(state.tasks[0].id).not.toBeUndefined();
+      });
+
+      it('clears task after appending', () => {
+        const state = addReducer('New task');
+
+        expect(state.tasks).toHaveLength(1);
+
+        expect(state.taskTitle).toBe('');
+      });
+    });
+
+    context('with not exist a new task title', () => {
+      it("doesn't work", () => {
+        const state = addReducer('');
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+  });
+
+  describe('deleteTask', () => {
+    function deleteReducer(id) {
+      return reducer({
+        tasks: [
+          { id: 1, title: '할일 #1' },
+        ],
+      }, deleteTask(id));
+    }
+
+    context('with existed task ID', () => {
+      it('remove the task in tasks', () => {
+        const state = deleteReducer(1);
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+
+    context('with not existed task ID', () => {
+      it("doesn't work", () => {
+        const state = deleteReducer(100);
+
+        expect(state.tasks).toHaveLength(1);
+      });
+    });
+  });
+});
