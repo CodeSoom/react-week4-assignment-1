4335091fc30c87b00a2b91e76dd15635ac1b2a85
comejtome508 <youngnoh.goh@gmail.com>
2020-10-15 02:31:35 +0900

    state를 정의해주고 expect를 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,6 +2,8 @@ import reducer from './reducer';
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
+    const state = reducer(previdousState, action);
 
+    expect(state.taskTitle).toBe('New Title');
   });
 });
