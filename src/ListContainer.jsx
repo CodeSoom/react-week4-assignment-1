9069fcfa39cdc9f8cad765cb352740e5f2201fe3
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-14 19:25:24 +0900

    Implement ListContainer

---
+++ src/ListContainer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { deleteTask } from './actions';
+
+import List from './List';
+
+export default function ListContainer() {
+  const { tasks } = useSelector((state) => ({
+    tasks: state.tasks,
+  }));
+  const dispatch = useDispatch();
+
+  function handleClickDeleteTask(id) {
+    dispatch(deleteTask(id));
+  }
+
+  return (
+    <List
+      tasks={tasks}
+      onClickDeleteTask={handleClickDeleteTask}
+    />
+  );
+}
