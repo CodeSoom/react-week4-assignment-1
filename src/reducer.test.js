f3bc0f872b95289c38aeb5511d244733dc077b85
oceanide <oceanide@gmail.com>
2020-10-14 14:26:24 +0900

    Add reducer.test.js

---
+++ src/reducer.test.js
@@ -0,0 +1,56 @@
+import reducer from './reducer';
+
+import {
+  updateTaskTitle,
+  addTask,
+} from './actions';
+
+describe('reducer', () => {
+  describe('updateTaskTitle', () => {
+    it('changes task title', () => {
+      const state = reducer({
+        taskTitle: '',
+      }, updateTaskTitle('New Title'));
+
+      expect(state.taskTitle).toBe('New Title');
+    });
+  });
+
+  describe('addTask', () => {
+    context('with task title', () => {
+      it('appends a new task into tasks', () => {
+        const state = reducer({
+          newId: 100,
+          taskTitle: 'New Task',
+          tasks: [],
+        }, addTask());
+
+        expect(state.tasks).toHaveLength(1);
+        expect(state.tasks[0].id).not.toBeUndefined();
+        expect(state.tasks[0].title).toBe('New Task');
+      });
+
+      it('clears task title', () => {
+        const state = reducer({
+          newId: 100,
+          taskTitle: 'New Task',
+          tasks: [],
+        }, addTask());
+
+        expect(state.taskTitle).toBe('');
+      });
+    });
+
+    context('without task title', () => {
+      it('does not work', () => {
+        const state = reducer({
+          newId: 100,
+          taskTitle: '',
+          tasks: [],
+        }, addTask());
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+  });
+});
