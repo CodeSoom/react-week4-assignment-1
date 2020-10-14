9725729e929a43a16ff5959fafbd87810ad7d995
goodzzong <goodzzong@gmail.com>
2020-10-15 00:13:02 +0900

    reducer 를 구현한다

--- src/store.js
+++ src/store.js
@@ -1,14 +1,5 @@
 import { createStore } from 'redux';
-
-const initialState = {
-  newId: 100,
-  taskTitle: '',
-  tasks: [],
-};
-
-function reducer(state = initialState, action) {
-  return state;
-}
+import reducer from './reducer';
 
 const store = createStore(reducer);
 
