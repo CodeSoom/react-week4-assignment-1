873cf9a373744f943b52d4efbbde8ee00f31ffa8
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-14 13:54:09 +0900

    Add test in ListContainer.test.jsx

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
 test('ListContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     tasks: [
       { id: 1, title: '할일1' },
@@ -16,9 +20,15 @@ test('ListContainer', () => {
     ],
   }));
 
-  const { getByText } = render((
+  const { getByText, getAllByText } = render((
     <ListContainer />
   ));
 
   expect(getByText(/할일1/)).not.toBeNull();
+
+  const buttons = getAllByText('완료');
+
+  fireEvent.click(buttons[0]);
+
+  expect(dispatch).toBeCalledWith({ type: 'deleteTask', payload: { id: 1 } });
 });
