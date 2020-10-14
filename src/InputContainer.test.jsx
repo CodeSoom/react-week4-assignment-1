12bcc7d71a25e7f49ed82179ef0099812b9e1c97
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-14 19:35:35 +0900

    Add test of InputContainer

---
+++ src/InputContainer.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import InputContainer from './InputContainer';
+
+test('Page', () => {
+  const dispatch = jest.fn();
+
+  useSelector.mockImplementation((selector) => selector({
+    taskTitle: '',
+  }));
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  const { getByText } = render((
+    <InputContainer />
+  ));
+
+  fireEvent.click(getByText('추가'));
+
+  expect(dispatch).toBeCalled();
+});
