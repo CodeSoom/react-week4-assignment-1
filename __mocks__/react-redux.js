9e67aaf1243daeea6c2e511cf557f25e650de704
oceanide <oceanide@gmail.com>
2020-10-14 09:20:05 +0900

    Refactor useSelector with mockImplementation

--- __mocks__/react-redux.js
+++ __mocks__/react-redux.js
@@ -1,8 +1,3 @@
 export const useDispatch = jest.fn();
 
-export const useSelector = jest.fn((selector) => selector({
-  tasks: [
-    { id: 1, title: '아무 것도 하지 않기 #1' },
-    { id: 2, title: '아무 것도 하지 않기 #1' },
-  ],
-}));
+export const useSelector = jest.fn();
