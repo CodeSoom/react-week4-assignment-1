3344b4fb1d29a3ad926b856fd80dfdfb0f1aa4c1
Seungmin Lee <seungmin@uxplus.dev>
2020-10-14 21:54:09 +0900

    Write test code
    Write test code for reducer.

---
+++ src/reducer.test.js
@@ -0,0 +1,112 @@
+import reducer from './reducer';
+
+import {
+  changeTaskTitle,
+  addTask,
+  deleteTask,
+} from './actions';
+
+describe('reducer', () => {
+  describe('changeTaskTitle', () => {
+    it('changes task title', () => {
+      const previousState = {
+        taskTitle: '놀기',
+        tasks: [],
+      };
+
+      const state = reducer(
+        previousState,
+        changeTaskTitle('New title'),
+      );
+
+      expect(state.taskTitle).toBe('New title');
+    });
+  });
+
+  describe('addTask', () => {
+    function reduceAddTask(taskTitle) {
+      return reducer(
+        {
+          newId: 1,
+          taskTitle,
+          tasks: [],
+        },
+        addTask(),
+      );
+    }
+
+    context('with task title', () => {
+      it('adds a new task', () => {
+        const state = reduceAddTask('쉬기');
+
+        expect(state.tasks).toHaveLength(1);
+        expect(state.tasks[0].id).not.toBeUndefined();
+        expect(state.tasks[0].title).toBe('쉬기');
+      });
+
+      it('resets taskTitle', () => {
+        const state = reduceAddTask('쉬기');
+
+        expect(state.taskTitle).toBe('');
+      });
+    });
+
+    context('without task title', () => {
+      it('does not add a new task', () => {
+        const state = reduceAddTask('');
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+  });
+
+  describe('deleteTask', () => {
+    context('with a valid task id', () => {
+      it('deletes a completed task', () => {
+        const previousState = {
+          tasks: [
+            {
+              id: 1,
+              title: '쉬기',
+            },
+            {
+              id: 2,
+              title: '놀기',
+            },
+          ],
+        };
+
+        const state = reducer(
+          previousState,
+          deleteTask(1),
+        );
+
+        expect(state.tasks).toHaveLength(1);
+      });
+    });
+
+    context('without a valid task id', () => {
+      it('nothing happens', () => {
+        const previousState = {
+          tasks: [
+            {
+              id: 1,
+              title: '쉬기',
+            },
+            {
+              id: 2,
+              title: '놀기',
+            },
+          ],
+        };
+
+        const state = reducer(
+          previousState,
+          deleteTask(3),
+        );
+
+        expect(state.tasks).toHaveLength(2);
+      });
+    });
+  });
+});
