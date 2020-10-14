469d8c761a24eaf82d7aca9b8ec83903a949155d
goodzzong <goodzzong@gmail.com>
2020-10-15 00:03:32 +0900

    reducer 에 대한 테스트를 추가한다

---
+++ src/reducer.test.js
@@ -0,0 +1,65 @@
+import reducer from './reducer';
+import { updateTaskTitle, addTask, deleteTask } from './actions';
+
+describe('reducer', () => {
+  describe('updateTaskTitle', () => {
+    it('change task title', () => {
+      const { taskTitle } = reducer({
+        taskTitle: 'New Title',
+      }, updateTaskTitle('New Title'));
+
+      expect(taskTitle).toBe('New Title');
+    });
+  });
+
+  describe('addTask', () => {
+    context('when there is a task title', () => {
+      it('add new task', () => {
+        const state = reducer({
+          taskTitle: 'New Title',
+          tasks: [],
+        }, addTask());
+
+        expect(state.taskTitle).toBe('');
+        expect(state.tasks).toHaveLength(1);
+      });
+    });
+
+    context('when there is a no task title', () => {
+      it('add new task', () => {
+        const state = reducer({
+          taskTitle: '',
+          tasks: [],
+        }, addTask());
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+  });
+
+  describe('deleteTask', () => {
+    context('with existed task ID', () => {
+      it('remove the task from tasks', () => {
+        const state = reducer({
+          tasks: [
+            { id: 1, title: 'Todo 할일하기' },
+          ],
+        }, deleteTask(1));
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+
+    context('without existed task ID', () => {
+      it('remove the task from tasks', () => {
+        const state = reducer({
+          tasks: [
+            { id: 1, title: 'Todo 할일하기' },
+          ],
+        }, deleteTask(2));
+
+        expect(state.tasks).toHaveLength(1);
+      });
+    });
+  });
+});
