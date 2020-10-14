9ebc182a3716b7bdf45101d9f568227d36b82150
comejtome508 <youngnoh.goh@gmail.com>
2020-10-15 02:00:53 +0900

    action을 넘겨줌

--- src/store.js
+++ src/store.js
@@ -1,6 +1,31 @@
 import { createStore } from 'redux';
 
-const reducer = () => ({});
+function reducer(state, action){
+  if (action.type === 'updateTaskTitle') {
+    return {
+      ...state,
+      taskTitle: action.payload.taskTitle,
+    };
+  }
+
+  if (action.type === 'addTask') {
+    const { newId, taskTitle, tasks } = state;
+
+  return {
+    ...state,
+    newId: newId + 1,
+    taskTitle: '',
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  };
+}
+
+if (action.type === 'deleteTask') {
+  const { tasks } = state;
+  return {
+    ...state,
+    tasks: tasks.filter((task) => task.id !== action.payload.id),
+  };
+}
 
 const store = createStore(reducer);
 
