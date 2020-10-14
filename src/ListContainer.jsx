e1ead296cbb978d7a67eb51b636367e665cf065e
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-14 10:55:12 +0900

    List와 ListContainer에서 빠르게 구현한다.

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -22,7 +22,7 @@ export default function ListContainer() {
   return (
     <List
       tasks={tasks}
-      onClickDelete={handleClickDeleteTask}
+      onClickComplete={handleClickDeleteTask}
     />
   );
 }
