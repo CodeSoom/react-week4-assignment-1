8c39dd20ef594e0df3578be9958f386f41c586c2
Noah <mike1124@naver.com>
2020-10-14 21:13:57 +0900

    ♻️ Modify test code Page.test.jsx

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render, fireEvent } from '@testing-library/react';
 
 import Page from './Page';
 
+jest.mock('react-redux');
+
 test('Page', () => {
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ],
+  }));
   const handleChangeTitle = jest.fn();
   const handleClickAddTask = jest.fn();
-  const handleClickDeleteTask = jest.fn();
-
-  const tasks = [
-    { id: 1, title: 'Task-1' },
-    { id: 2, title: 'Task-2' },
-  ];
 
   const { getByText } = render((
     <Page
       taskTitle=""
       onChangeTitle={handleChangeTitle}
       onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
     />
   ));
 
