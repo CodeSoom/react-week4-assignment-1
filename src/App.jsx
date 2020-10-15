d283c5d93ca2d250245e8eb648cde0a0a8b26983
saseungmin <dbd02169@naver.com>
2020-10-15 21:02:00 +0900

    Page.jsx 삭제
    - App.jsx 로 합친다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 
-import Page from './Page';
-import { addTask, updateTaskTitle } from './actions';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-  const { taskTitle } = useSelector((state) => ({
-    taskTitle: state.taskTitle,
-  }));
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
