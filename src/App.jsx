54fb51007b2c65a5420ec9fa58569d0c3a905c5c
Dong A Kim <dongakim90@gmail.com>
2020-10-15 14:19:04 +0900

    App 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,14 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
-import { getState } from './selectors';
-import actions from './actions';
-
-import Page from './Page';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
-  const dispatch = useDispatch();
-  const { taskTitle, tasks } = useSelector(getState);
-
-  function handleChangeTitle(event) {
-    dispatch(actions.changeTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(actions.addTask());
-  }
-
-  function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </>
   );
 }
