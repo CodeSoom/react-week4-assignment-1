b00e600877c2fd5160101c451e2054841f81f7e8
Dong A Kim <dongakim90@gmail.com>
2020-10-15 14:12:13 +0900

    ListContainer 적용
    Page 컴포넌트에 적용 및 실패되는 테스트 수정

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -13,20 +13,13 @@ test('Page', () => {
   useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
     taskTitle: '',
+    tasks: [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ],
   }));
-  const handleClickDeleteTask = jest.fn();
 
-  const tasks = [
-    { id: 1, title: 'Task-1' },
-    { id: 2, title: 'Task-2' },
-  ];
-
-  const { getByText } = render((
-    <Page
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
-  ));
+  const { getByText } = render(<Page />);
 
   expect(getByText(/Task-1/)).not.toBeNull();
   expect(getByText(/Task-2/)).not.toBeNull();
