b49788e90791ee1778d419f0efada36865b528d5
davidyang2149 <davidyang2149@gmail.com>
2020-10-15 09:19:53 +0900

    Add ListContainer.test.jsx for divide Page
    4강 리덕스 - 4주 차 과제 1
    - Page 리팩토링을 위한 ListContainer.test.jsx 추가.

---
+++ src/ListContainer.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import ListContainer from './ListContainer';
+
+jest.mock('react-redux');
+
+test('ListContainer', () => {
+  const tasks = [
+    { id: 1, title: '테스트로 진행할 일1' },
+    { id: 2, title: '테스트로 진행할 일2' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
+  const { getByText } = render((
+    <ListContainer />
+  ));
+
+  expect(getByText(/테스트로 진행할 일1/)).not.toBeNull();
+  expect(getByText(/테스트로 진행할 일2/)).not.toBeNull();
+
+  // TODO: 통합 테스트 코드 작성
+  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+});
--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import Page from './Page';
 
 test('Page', () => {
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ],
+  }));
+
   const handleChangeTitle = jest.fn();
   const handleClickAddTask = jest.fn();
-  const handleClickDeleteTask = jest.fn();
-
-  const tasks = [
-    { id: 1, title: 'Task-1' },
-    { id: 2, title: 'Task-2' },
-  ];
 
   const { getByText } = render((
     <Page
       taskTitle=""
       onChangeTitle={handleChangeTitle}
       onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
     />
   ));
 
