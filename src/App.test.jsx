7152f9b9f25e1e9aeca44643d0b2164ef3c08794
davidyang2149 <davidyang2149@gmail.com>
2020-10-15 22:18:03 +0900

    Refactor App.jsx and Page.jsx
    4강 리덕스 - 4주 차 과제 1
    - App.jsx 리팩토링 - App과 Page의 역할 중복에 따른 컨테이너 이전.
    - Page.jsx 리팩토링 - App과 Page의 역할 중복에 따른 삭제.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,13 +9,12 @@ import App from './App';
 jest.mock('react-redux');
 
 test('App', () => {
-  const tasks = [
-    { id: 1, title: '테스트로 진행할 일1' },
-    { id: 2, title: '테스트로 진행할 일2' },
-  ];
-
   useSelector.mockImplementation((selector) => selector({
-    tasks,
+    taskTitle: '',
+    tasks: [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ],
   }));
 
   const { getByText } = render((
@@ -23,9 +22,7 @@ test('App', () => {
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
-  expect(getByText(/테스트로 진행할 일1/)).not.toBeNull();
-  expect(getByText(/테스트로 진행할 일2/)).not.toBeNull();
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  expect(getByText(/Task-1/)).not.toBeNull();
+  expect(getByText(/Task-2/)).not.toBeNull();
 });
