8cc75d6a992d3994657abfb1d2d177c223ec28b0
oceanide <oceanide@gmail.com>
2020-10-15 09:55:45 +0900

    Refactor Page.jsx and Page.test.jsx

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import Input from './Input';
+import InputContainer from './InputContainer';
 import ListContainer from './ListContainer';
 
-export default function Page({
-  taskTitle, onChangeTitle, onClickAddTask,
-}) {
+export default function Page() {
   return (
     <div>
       <h1>To-do</h1>
-      <Input
-        value={taskTitle}
-        onChange={onChangeTitle}
-        onClick={onClickAddTask}
-      />
+      <InputContainer />
       <ListContainer />
     </div>
   );
