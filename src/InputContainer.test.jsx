e3e1a53b37660b865e7ee745b881f2e06894102b
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-15 11:52:54 +0900

    InputContainer에서 태스트 대상을 구체화한다.

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -27,25 +27,29 @@ describe('InputContainer', () => {
     taskTitle,
   }));
 
-  it('dispatch updateTaskTitle action', () => {
-    const newTaskTitle = 'next task';
+  describe('change taskTitle', () => {
+    it('dispatches updateTaskTitle action', () => {
+      const newTaskTitle = 'next task';
 
-    const { getByLabelText } = renderInputContainer();
+      const { getByLabelText } = renderInputContainer();
 
-    expect(getByLabelText('할 일')).not.toBeNull();
+      expect(getByLabelText('할 일')).not.toBeNull();
 
-    fireEvent.change(getByLabelText('할 일'), { target: { value: newTaskTitle } });
+      fireEvent.change(getByLabelText('할 일'), { target: { value: newTaskTitle } });
 
-    expect(dispatch).toBeCalledWith(updateTaskTitle(newTaskTitle));
+      expect(dispatch).toBeCalledWith(updateTaskTitle(newTaskTitle));
+    });
   });
 
-  it('dispatch addTask action', () => {
-    const { getByText } = renderInputContainer();
+  describe('click add task button', () => {
+    it('dispatches addTask action', () => {
+      const { getByText } = renderInputContainer();
 
-    expect(getByText(/추가/)).not.toBeNull();
+      expect(getByText(/추가/)).not.toBeNull();
 
-    fireEvent.click(getByText(/추가/));
+      fireEvent.click(getByText(/추가/));
 
-    expect(dispatch).toBeCalledWith(addTask());
+      expect(dispatch).toBeCalledWith(addTask());
+    });
   });
 });
