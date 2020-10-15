7058ebb95794a5b077ddf9e8df80dcb5cc4d8116
oceanide <oceanide@gmail.com>
2020-10-15 15:27:42 +0900

    Remove parenthesis surrounding <Provider>

--- src/index.jsx
+++ src/index.jsx
@@ -8,10 +8,8 @@ import App from './App';
 import store from './store';
 
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  ),
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app'),
 );
