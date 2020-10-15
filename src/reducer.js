903de37e692df5d45a038c2350ef1fd7fa959b8d
oceanide <oceanide@gmail.com>
2020-10-15 19:56:09 +0900

    Fix error in reducers.js

--- src/reducer.js
+++ src/reducer.js
@@ -4,43 +4,48 @@ const initialState = {
   tasks: [],
 };
 
-export default function reducer(state = initialState, action) {
-  if (!action) {
+const reducers = {
+  updateTaskTitle(state, action) {
+    const { taskTitle } = action.payload;
+
+    return {
+      ...state,
+      taskTitle,
+    };
+  },
+
+  addTask(state) {
+    const { newId, taskTitle, tasks } = state;
+
+    if (!taskTitle) {
+      return state;
+    }
+
+    return {
+      ...state,
+      newId: newId + 1,
+      taskTitle: '',
+      tasks: [...tasks, { id: newId, title: taskTitle }],
+    };
+  },
+
+  deleteTask(state, action) {
+    const { tasks } = state;
+    const { id } = action.payload;
+
+    return {
+      ...state,
+      tasks: tasks.filter((task) => task.id !== id),
+    };
+  },
+};
+
+export default function reducer(state = initialState, action = {}) {
+  const reduce = reducers[action.type];
+
+  if (!reduce) {
     return state;
   }
 
-  const reduceAction = {
-    updateTaskTitle() {
-      return {
-        ...state,
-        taskTitle: action.payload.taskTitle,
-      };
-    },
-
-    addTask() {
-      const { newId, taskTitle, tasks } = state;
-
-      if (!taskTitle) {
-        return state;
-      }
-
-      return {
-        ...state,
-        newId: newId + 1,
-        taskTitle: '',
-        tasks: [...tasks, { id: newId, title: taskTitle }],
-      };
-    },
-
-    deleteTask() {
-      const { tasks } = state;
-
-      return {
-        ...state,
-        tasks: tasks.filter((task) => task.id !== action.payload.id),
-      };
-    },
-  };
-
-  return reduceAction[action.type]();
+  return reduce(state, action);
 }
