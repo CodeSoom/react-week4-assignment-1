7e1f92ed2fc83a8f5e2f0e79f42f7852a9f2e29a
Dong A Kim <dongakim90@gmail.com>
2020-10-15 09:36:06 +0900

    App.test.jsx 초기화
    현재는 redux store의 mocking이 어려움으로
    강의대로 Input 컨테이너, List 컨테이너로 나눠서
    테스트를 진행하고자 기존의 테스트 초기화함.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,44 +1,16 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
-import { render, screen, fireEvent } from '@testing-library/react';
-
-import store from './store';
+import { render } from '@testing-library/react';
 
 import App from './App';
 
-describe('App', () => {
+test('App', () => {
   const { getByText } = render((
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
 
   // TODO: 통합 테스트 코드 작성
   // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
-
-  it('Task 타이핑시 입력된 값이 출련된다.', () => {
-    // When
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: '청소하기' },
-    });
-
-    // Then
-    expect(screen.getByRole('textbox')).toHaveValue('청소하기');
-  });
-
-  it('Task 추가시 목록에 Task가 출력된다.', () => {
-    // Given
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: '선물사기' },
-    });
-
-    // When
-    fireEvent.click(screen.getByText('추가'));
-
-    // Then
-    expect(screen.getByRole('list')).toHaveTextContent('선물사기');
-  });
 });
