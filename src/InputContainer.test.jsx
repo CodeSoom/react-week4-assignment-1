981ccbac83d2bb5a7da9837838734a18534b27bc
Dong A Kim <dongakim90@gmail.com>
2020-10-15 11:42:39 +0900

    InputContainer 테스트 추가
    태스크 입력과 추가에 대한 테스트

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputContainer from './InputContainer';
 
 jest.mock('react-redux');
 
 describe('<InputContainer />', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
     taskTitle: '',
   }));
@@ -32,6 +34,32 @@ describe('<InputContainer />', () => {
 
       // Then
       expect(getByText('추가')).toBeInTheDocument();
+    });
+  });
+
+  describe('event', () => {
+    it('calls dispatch on typing in the input', () => {
+      // Given
+      const { getByRole } = renderInputContainer();
+
+      // When
+      fireEvent.change(getByRole('textbox'), {
+        target: { value: '글자입력' },
+      });
+
+      // Then
+      expect(dispatch).toBeCalled();
+    });
+
+    it('calls dispatch on click the add button', () => {
+      // Given
+      const { getByText } = renderInputContainer();
+
+      // When
+      fireEvent.click(getByText('추가'));
+
+      // Then
+      expect(dispatch).toBeCalled();
     });
   });
 });
