74f717336413115652ff69b81da40f7bd9df57b8
saseungmin <dbd02169@naver.com>
2020-10-15 21:31:19 +0900

    InputContainer 테스트 구현

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { fireEvent, render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputContainer from './InputContainer';
 
 jest.mock('react-redux');
 
-test('List', () => {
+describe('InputContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     taskTitle: '아무것도 하지 않기 3',
   }));
 
-  const { getByText } = render((
+  const renderInputContainer = () => render((
     <InputContainer />
   ));
 
-  expect(getByText(/아무것도 하지 않기 3/)).not.toBeNull();
-  expect(getByText(/추가/)).not.toBeNull();
+  it('할 일을 추가한다.', () => {
+    const { getByDisplayValue, getByText } = renderInputContainer();
+    expect(getByDisplayValue(/아무것도 하지 않기 3/)).not.toBeNull();
+    expect(getByText(/추가/)).not.toBeNull();
+
+    fireEvent.click(getByText(/추가/));
+
+    expect(dispatch).toBeCalledWith({ type: 'addTask' });
+  });
+
+  it('할 일을 입력한다.', () => {
+    const { getByLabelText } = renderInputContainer();
+
+    const taskTitle = '아무것도 하지 않기 4';
+
+    fireEvent.change(getByLabelText('할 일'), { target: { value: taskTitle } });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'updateTaskTitle',
+      payload: {
+        taskTitle,
+      },
+    });
+  });
 });
