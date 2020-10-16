6ba3b232f1e48ff42631dc001e12e31561a0085d
Seungmin Lee <seungmin@uxplus.dev>
2020-10-16 16:42:50 +0900

    Add test code for `App`
    Add test code for `App` to increase test coverage.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,29 +1,88 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from '../__mocks__/react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import App from './App';
 
 jest.mock('react-redux');
 
-test('App', () => {
+const { click } = fireEvent;
+
+function change(target, { value }) {
+  fireEvent.change(target, {
+    target: { value },
+  });
+}
+
+describe('App', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  function renderApp() {
+    const {
+      container, getByLabelText, getByText, getAllByText,
+    } = render((
+      <App />
+    ));
+
+    return {
+      container,
+      input: getByLabelText(/할 일/),
+      buttonAdd: getByText(/추가/),
+      findButtonComplete: () => getAllByText(/완료/)[0],
+    };
+  }
+
   const tasks = [
     { id: 1, title: '쉬기' },
     { id: 2, title: '놀기' },
   ];
 
+  const mockDispatch = jest.fn();
+
   useSelector.mockImplementation((selector) => selector({
     tasks,
   }));
+  useDispatch.mockReturnValue(mockDispatch);
 
-  const { getByText } = render((
-    <App />
-  ));
+  describe('handleChangeTitle', () => {
+    it('changes task title', () => {
+      const { input } = renderApp();
 
-  expect(getByText(/추가/)).not.toBeNull();
+      const value = '커버리지 높이기';
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+      change(input, { value });
+      expect(mockDispatch).toHaveBeenCalledWith({
+        payload: { value },
+        type: 'changeTaskTitle',
+      });
+    });
+  });
+
+  describe('handleClickAddTask', () => {
+    it('adds a new task', () => {
+      const { buttonAdd } = renderApp();
+
+      expect(buttonAdd).not.toBeNull();
+      click(buttonAdd);
+      expect(mockDispatch).toHaveBeenCalledWith({
+        type: 'addTask',
+      });
+    });
+  });
+
+  describe('handleClickDeleteTask', () => {
+    it('deletes completed task', () => {
+      const { findButtonComplete } = renderApp();
+
+      click(findButtonComplete());
+      expect(mockDispatch).toHaveBeenCalledWith({
+        payload: { id: 1 },
+        type: 'deleteTask',
+      });
+    });
+  });
 });
