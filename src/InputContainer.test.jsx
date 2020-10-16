53dc4526ccfe10cf5325b9fdbea9d7e5ce09fff4
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-16 13:39:16 +0900

    Add the container component and test

---
+++ src/InputContainer.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import InputContainer from './InputContainer';
+
+jest.mock('react-redux');
+
+test('InputContainer', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({
+    taskTitle: 'new  title',
+  }));
+
+  const { getByText, getByDisplayValue } = render((
+    <InputContainer />
+  ));
+
+  expect(getByText(/추가/)).not.toBeNull();
+  expect(getByDisplayValue(/new title/)).not.toBeNull();
+  fireEvent.click(getByText(/추가/));
+
+  expect(dispatch).toBeCalledWith({
+    type: 'addTask',
+  });
+
+  // expect(getByText(/new title/)).not.toBeNull();
+
+  // TODO: 통합 테스트 코드 작성
+  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+});
--- src/LIstContainer.test.jsx
+++ src/LIstContainer.test.jsx
@@ -4,13 +4,12 @@ import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
-import App from './App';
+import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('App', () => {
+test('ListContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    taskTitle: '',
     tasks: [
       { id: 1, title: 'Task-1' },
       { id: 2, title: 'Task-2' },
@@ -18,9 +17,11 @@ test('App', () => {
   }));
 
   const { getByText } = render((
-    <App />
+    <ListContainer />
   ));
 
   expect(getByText(/Task-1/)).not.toBeNull();
-  expect(getByText(/Task-2/)).not.toBeNull();
+
+  // TODO: 통합 테스트 코드 작성
+  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
