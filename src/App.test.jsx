92f3409ad180843da98e58b1a721e089104253bf
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-16 14:37:17 +0900

    Revert "Add the Redux package"
    This reverts commit 35a7ba8523f36487f26277c4632b5db7a94fa489.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,23 +2,15 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
-
 import App from './App';
 
-jest.mock('react-redux');
-
 test('App', () => {
-  useSelector.mockImplementation((selector) => selector({
-    taskTitle: '',
-    tasks: [
-      { id: 1, title: 'Task-1' },
-    ],
-  }));
-
   const { getByText } = render((
     <App />
   ));
 
-  expect(getByText(/Task-1/)).not.toBeNull();
+  expect(getByText(/추가/)).not.toBeNull();
+
+  // TODO: 통합 테스트 코드 작성
+  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
