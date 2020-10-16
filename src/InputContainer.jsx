85d84a1be28e9da06fa9491d4f5ec5456b218f84
wjy5446 <wjy5446@gmail.com>
2020-10-16 22:48:08 +0900

    refac) container component 생성

---
+++ src/InputContainer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateTaskTitle, addTask } from './actions';
+
+import Input from './Input';
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  const { taskTitle } = useSelector((state) => {
+    taskTitle: state.taskTitle,
+  });
+
+  function handleChangeTitle(event) {
+    dispatch(updateTaskTitle(event.target.value));
+  }
+
+  function handleClickAddTask() {
+    dispatch(addTask());
+  }
+
+  return (
+    <Input
+      taskTitle={taskTitle}
+      onChangeTitle={handleChangeTitle}
+      onClickAddTask={handleClickAddTask}
+    />
+  );
+}
