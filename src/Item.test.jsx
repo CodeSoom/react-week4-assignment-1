f1f2cb709b48fe3adcfcbc0f033540838bb3dc85
wjy5446 <wjy5446@gmail.com>
2020-10-16 23:20:31 +0900

    feat) test 테스트코드 추가

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -4,27 +4,45 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Item from './Item';
 
-test('Item', () => {
-  const task = {
-    id: 1,
-    title: '뭐라도 하기',
-  };
+describe('Item', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   const handleClick = jest.fn();
 
-  const { container, getByText } = render((
+  const renderItem = (task = { id: '', title: '' }) => render((
     <Item
       task={task}
       onClickDelete={handleClick}
     />
   ));
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('완료');
+  it('완료 버튼이 있는지 확인합니다.', () => {
+    const { container } = renderItem();
 
-  expect(handleClick).not.toBeCalled();
+    expect(container).toHaveTextContent('완료');
+  });
 
-  fireEvent.click(getByText('완료'));
+  it('완료 버튼을 클릭하면 클릭 함수가 호출됩니다.', () => {
+    const { getByText } = renderItem();
+    const buttonDone = getByText('완료');
 
-  expect(handleClick).toBeCalledWith(1);
+    expect(handleClick).not.toBeCalled();
+    fireEvent.click(buttonDone);
+    expect(handleClick).toBeCalled();
+  })
+
+  context('task가 주어졌을 때,', () => {
+    const task = {
+      id: 1,
+      title: '뭐라도 하기',
+    };
+
+    it('주어진 task가 출력됩니다.', () => {
+      const { container } = renderItem(task);
+
+      expect(container).toHaveTextContent(task.title);
+    });
+  });
 });
