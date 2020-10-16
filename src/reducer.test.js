8f0c5500d57295eb3fd0059625cb0ddcfceac955
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-16 16:04:23 +0900

    Refactor the reducer.test.js

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,8 +4,21 @@ import { updateTaskTitle, updateTaskTitleAction } from './actions/updateTaskTitl
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
-    it('changes new task title at first time', () => {
-      const state = reducer(undefined, updateTaskTitleAction('new task'))(updateTaskTitle);
+    context('changes new task title', () => {
+      it('from initial state', () => {
+        const state = reducer(undefined, updateTaskTitleAction('new task'))(updateTaskTitle);
+
+        expect(state.taskTitle).toBe('new task');
+      });
+    });
+
+    context('from previous state', () => {
+      const state = reducer(
+        {
+          taskTitle: 'task-1',
+        },
+        updateTaskTitleAction('new task'),
+      )(updateTaskTitle);
 
       expect(state.taskTitle).toBe('new task');
     });
