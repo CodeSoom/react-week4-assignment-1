0b89ff52da1663efdfe3f062f5e70a3563b70e78
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-17 03:30:01 +0900

    [Refactor] InputContainer에 대한 테스트 리팩토링
    handleChangeTitle() 에 대한 action creator 테스트 추가.

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -6,28 +6,53 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import InputContainer from './InputContainer';
 
+import { updateTaskTitle, addTask } from './actions';
+
 jest.mock('react-redux');
 
 describe('InputContainer', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+  beforeEach(() => {
+    jest.clearAllMocks();
+    useDispatch.mockImplementation(() => dispatch);
+  });
 
-  it('show tasks', () => {
+  it('render a task title', () => {
     useSelector.mockImplementation((selector) => selector({
       taskTitle: '아무것도 안 하기',
     }));
 
-    const { getByText, getByDisplayValue } = render((
-      <InputContainer />
-    ));
+    const { getByDisplayValue } = render(<InputContainer />);
 
     expect(getByDisplayValue(/아무것도 안 하기/)).not.toBeNull();
+  });
+
+  it('changes a task title', () => {
+    useSelector.mockImplementation((selector) => selector({
+      taskTitle: '',
+    }));
+
+    const { getByLabelText } = render(<InputContainer />);
+
+    expect(getByLabelText('할 일')).not.toBeNull();
+
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '무언가 하기' } });
+
+    expect(dispatch).toBeCalledWith(updateTaskTitle('무언가 하기'));
+  });
+
+  it('click add button', () => {
+    useSelector.mockImplementation((selector) => selector({
+      taskTitle: '무언가 하기',
+    }));
+
+    const { getByText } = render(<InputContainer />);
 
     expect(getByText(/추가/)).not.toBeNull();
 
     fireEvent.click(getByText(/추가/));
 
-    expect(dispatch).toBeCalledWith({ type: 'addTask' });
+    expect(dispatch).toBeCalledWith(addTask());
   });
 });
