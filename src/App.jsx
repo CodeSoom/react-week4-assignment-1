4f9691cfab55541650f9db6278ab3b3118578a27
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-16 23:45:41 +0900

    App 컴포넌트 리팩토링
    리덕스를 적용하기 위해 To-do App 상태에 대한 관심사 분리.

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,54 @@ import React, { useState } from 'react';
 
 import Page from './Page';
 
-export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
+// 상태에 대한 관심사 분리
+// 새로운 상태를 리턴하는 reducer() 생성
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
 
+function updateTaskTitle(state, newTaskTitle) {
+  return {
+    ...state,
+    taskTitle: newTaskTitle,
+  };
+}
+
+function addTask(state) {
   const { newId, taskTitle, tasks } = state;
+  return {
+    ...state,
+    newId: newId + 1,
+    taskTitle: '',
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  };
+}
+
+function deleteTask(state, id) {
+  const { tasks } = state;
+  return {
+    ...state,
+    tasks: tasks.filter((task) => task.id !== id),
+  };
+}
+
+export default function App() {
+  const [state, setState] = useState(initialState);
+
+  const { taskTitle, tasks } = state;
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+    setState(updateTaskTitle(state, event.target.value));
   }
 
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
+    setState(addTask(state));
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
+    setState(deleteTask(state, id));
   }
 
   return (
