6eb5de92e2345b4b1db1b1153448d5876830f6af
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-17 02:56:03 +0900

    [Refactor] Page 컴포넌트 단순화
    1st, Page 컴포넌트 InputContainer and ListContainer로 변경.
    2nd, App 컴포넌트 단순화.

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputContainer from './InputContainer';
 
 jest.mock('react-redux');
 
 describe('InputContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   it('show tasks', () => {
     useSelector.mockImplementation((selector) => selector({
       taskTitle: '아무것도 안 하기',
     }));
 
-    const { getByDisplayValue } = render((
+    const { getByText, getByDisplayValue } = render((
       <InputContainer />
     ));
 
     expect(getByDisplayValue(/아무것도 안 하기/)).not.toBeNull();
+
+    expect(getByText(/추가/)).not.toBeNull();
+
+    fireEvent.click(getByText(/추가/));
+
+    expect(dispatch).toBeCalledWith({ type: 'addTask' });
   });
 });
