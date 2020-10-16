050786cba31f04d5158496163758d5c50c781f79
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-17 01:15:27 +0900

    [Refactor] store에서 reducer를 분리
    Make a js file name reduce for divide store.

---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+// Redux action
+// - type   : String)
+// - payload: Object
+
+export const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+// 상태에 대한 관심사 분리
+// 새로운 상태를 리턴하는 reducer() 생성
+export default function reducer(state = initialState, action) {
+  if (action === 'updateTaskTitle') {
+    return {
+      ...state,
+      taskTitle: action.payload.taskTitle,
+    };
+  }
+  if (action === 'addTask') {
+    const { newId, taskTitle, tasks } = state;
+    return {
+      ...state,
+      newId: newId + 1,
+      taskTitle: '',
+      tasks: [...tasks, { id: newId, title: taskTitle }],
+    };
+  }
+  if (action === 'deleteTask') {
+    const { tasks } = state;
+    return {
+      ...state,
+      tasks: tasks.filter((task) => task.id !== action.payload.id),
+    };
+  }
+  return state;
+}
--- src/store.js
+++ src/store.js
@@ -1,42 +1,6 @@
 import { createStore } from 'redux';
 
-// Redux action
-// - type   : String)
-// - payload: Object
-
-const initialState = {
-  newId: 100,
-  taskTitle: '',
-  tasks: [],
-};
-
-// 상태에 대한 관심사 분리
-// 새로운 상태를 리턴하는 reducer() 생성
-function reducer(state = initialState, action) {
-  if (action === 'updateTaskTitle') {
-    return {
-      ...state,
-      taskTitle: action.payload.taskTitle,
-    };
-  }
-  if (action === 'addTask') {
-    const { newId, taskTitle, tasks } = state;
-    return {
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    };
-  }
-  if (action === 'deleteTask') {
-    const { tasks } = state;
-    return {
-      ...state,
-      tasks: tasks.filter((task) => task.id !== action.payload.id),
-    };
-  }
-  return state;
-}
+import { reducer } from './reducer';
 
 const store = createStore(reducer);
 
