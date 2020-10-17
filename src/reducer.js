00bd1a5e708b750e581f31f7c1caf485509e59ff
juunini <juuni.ni.i@gmail.com>
2020-10-17 23:39:18 +0900

    Refactoring reducer. change if to object

--- src/reducer.js
+++ src/reducer.js
@@ -5,36 +5,38 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
-  if (action.type === 'updateTaskTitle') {
-    return {
+  const actions = {
+    updateTaskTitle: () => ({
       ...state,
       taskTitle: action.payload.taskTitle,
-    };
-  }
+    }),
 
-  if (action.type === 'addTask') {
-    const { newId, tasks, taskTitle } = state;
+    addTask: () => {
+      const { newId, tasks, taskTitle } = state;
 
-    if (!taskTitle) {
-      return state;
-    }
+      if (!taskTitle) {
+        return state;
+      }
 
-    return ({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
-  }
+      return ({
+        ...state,
+        newId: newId + 1,
+        taskTitle: '',
+        tasks: [...tasks, { id: newId, title: taskTitle }],
+      });
+    },
 
-  if (action.type === 'deleteTask') {
-    const { tasks } = state;
+    deleteTask: () => {
+      const { tasks } = state;
 
-    return ({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== action.payload.id),
-    });
-  }
+      return ({
+        ...state,
+        tasks: tasks.filter((task) => task.id !== action.payload.id),
+      });
+    },
+  };
 
-  return state;
+  return actions[action.type]
+    ? actions[action.type]()
+    : state;
 }
