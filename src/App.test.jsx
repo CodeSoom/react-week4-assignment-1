d55398ae508887dcadfc104667bd3f82aca4931b
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-16 06:23:24 +0900

    App.test.jsx : 테스트 케이스 추가 및 테스트 실패 확인

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,15 +8,31 @@ import App from './App';
 
 jest.mock('react-redux');
 
-test('App', () => {
-  useSelector.mockImplementation((selector) => selector({
-    tasks: [],
-  }));
+describe('App', () => {
+  context('with no tasks', () => {
+    it('show "할 일이 없어요".', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [],
+      }));
+      const { getByText } = render((
+        <App />
+      ));
+      expect(getByText(/추가/)).not.toBeNull();
+      expect(getByText(/할 일이 없어요/)).not.toBeNull();
+    });
+  });
 
-  const { getByText } = render((
-    <App />
-  ));
-
-  expect(getByText(/추가/)).not.toBeNull();
-  expect(getByText(/할 일이 없어요/)).not.toBeNull();
+  context('with tasks', () => {
+    it('show tasks', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [
+          { id: 1, title: '기모찌' },
+        ],
+      }));
+      const { getByText } = render((
+        <App />
+      ));
+      expect(getByText(/기모찌/)).not.toBeNull();
+    });
+  });
 });
