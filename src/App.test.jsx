1e4196e8ef8c2af63221c306404a912699019b07
younghoon <secret9179@gmail.com>
2021-02-16 00:46:21 +0900

    Redux 1 수강 후 첫 PR

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  // TODO: useSelector 조작
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [
+      { id: 1, title: '아무 것도 하지 않기 #1' },
+      { id: 2, title: '아무 것도 하지 않기 #2' },
+    ],
+  }));
+
   const { getByText } = render((
     <App />
   ));
