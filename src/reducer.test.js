c8b3007a4886dc3d65fc7b38d13f48e87ba4f041
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-15 15:51:40 +0900

    reducer에 대한 test를 추가한다.

---
+++ src/reducer.test.js
@@ -0,0 +1,43 @@
+import reducer from './reducer';
+
+describe('reducer는', () => {
+  it('새로운 할 일을 추가한다.', () => {
+    const preveState = {
+      taskTitle: 'New Task',
+      tasks: [],
+    };
+
+    const state = reducer(preveState, { type: 'addNewTask' });
+
+    expect(state.taskTitle).toBe('');
+    expect(state.tasks).toHaveLength(1);
+  });
+
+  it('입력된 인풋을 반영한다.', () => {
+    const preveState = {
+      taskTitle: '',
+    };
+
+    const state = reducer(preveState, {
+      type: 'updateInput',
+      payload: { taskTitle: '누워있기' },
+    });
+
+    expect(state.taskTitle).toBe('누워있기');
+  });
+
+  it('완료된 일을 삭제한다.', () => {
+    const preveState = {
+      tasks: [
+        { id: 100, title: '밥먹기' },
+        { id: 101, title: '누워있기' },
+      ],
+    };
+
+    const state = reducer(preveState, {
+      type: 'deleteCompleteTask',
+      payload: { id: '100' },
+    });
+    expect(state.tasks).toHaveLength(1);
+  });
+});
