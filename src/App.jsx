843d870bcd732b79228481ceac7d1ffc92edc8b3
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-16 06:42:52 +0900

    App.jsx: 리팩토링 - useSelector 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import Page from './Page';
 
 export default function App() {
+  const { tasks } = useSelector((state) => ({
+    tasks: state.tasks,
+  }));
+
   return (
-    <Page tasks={[]} />
+    <Page tasks={tasks} />
   );
 }
