f66070f0754d2e12cd011c90543208e5acafa833
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-16 03:10:07 +0900

    App.test.jsx: 테스트 케이스 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from '../__mocks__/react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [],
+  }));
+
   const { getByText } = render((
     <App />
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  expect(getByText(/할 일이 없어요/)).not.toBeNull();
 });
