89129c2e1a01b05925c6fb59281077410b0ca89a
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 11:12:48 +0900

    :white_check_mark: Red-Test: make reducer test
    - 할일을 추가했을 때 test

---
+++ src/reducer.test.js
@@ -0,0 +1,24 @@
+import { addTask } from './actions';
+import reducer from './reducer';
+
+describe('reducer', () => {
+  describe('addtask', () => {
+    it('new task should be added to tasks ', () => {
+      const changedState = reducer(
+        {
+          newId: 100,
+          taskTitle: '첫번째 할일',
+          tasks: [],
+        },
+        addTask()
+      );
+
+      expect(changedState.tasks).toHaveLength(1);
+      expect(changedState.tasks[0].title).toBe('첫번째 할일');
+    });
+  });
+
+  describe('updateTask', () => {});
+
+  describe('deleteTask', () => {});
+});
