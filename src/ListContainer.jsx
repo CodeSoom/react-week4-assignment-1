771c0c067baeddeea31ab5fef2a941d8e6c9cd33
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 21:45:56 +0900

    :fire: Remove: code without test code

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-import { deleteTask } from './actions';
 import List from './List';
 
 function ListContainer() {
-  const dispatch = useDispatch();
   const tasks = useSelector((state) => state.tasks);
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
 
   return (
     <List
       tasks={tasks}
-      onClickDelete={handleClickDeleteTask}
+      onClickDelete={() => {}}
     />
   );
 }
