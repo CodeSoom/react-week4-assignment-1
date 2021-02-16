6edde9701694c6aa58b58041d5d4e409cdd97fd1
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 21:50:47 +0900

    :construction: Green-Working: Make ListContainer pass the delete test

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import List from './List';
 
 function ListContainer() {
   const tasks = useSelector((state) => state.tasks);
-
+  const dispatch = useDispatch();
+  function handleClickDelete() {
+    dispatch({ payload: 1, type: 'DELETE_TASK' });
+  }
   return (
     <List
       tasks={tasks}
-      onClickDelete={() => {}}
+      onClickDelete={handleClickDelete}
     />
   );
 }
