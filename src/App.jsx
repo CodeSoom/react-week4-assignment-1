8056ad4fbcdfe0ad5bafec05ef81489187c488af
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 17:32:15 +0900

    :recycle: Refactor: Add InputContainer and ListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { addTask, deleteTask, updateTask } from './actions';
-import Page from './Page';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-  const { taskTitle, tasks } = useSelector((state) => state);
-
-  function handleChangeTitle(e) {
-    dispatch(updateTask(e.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
