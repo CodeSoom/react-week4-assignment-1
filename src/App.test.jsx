59635eb7ef3e553783cd29b75612c7772452b017
datalater <the7mincheol@gmail.com>
2021-02-16 18:48:29 +0900

    App에 대한 add task 테스트를 추가하라

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,6 +8,7 @@ import App from './App';
 TODO
 - App shows "추가"
 - App updates task title
+- App adds task
 */
 
 describe('App', () => {
@@ -29,5 +30,19 @@ describe('App', () => {
     });
 
     expect(getByDisplayValue('task-1')).not.toBeNull();
+  });
+
+  it('adds task', () => {
+    const { getByLabelText, getByText } = render((
+      <App />
+    ));
+
+    fireEvent.change(getByLabelText('할 일'), {
+      target: { value: 'task-1' },
+    });
+
+    fireEvent.click(getByText(/추가/));
+
+    expect(getByText('task-1')).not.toBeNull();
   });
 });
