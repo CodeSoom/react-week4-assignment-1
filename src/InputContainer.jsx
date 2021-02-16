7cb2baf5a8fffdfaf882df08e8edb32130dea609
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 21:57:02 +0900

    :fire: Remove: code without test code

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { addTask, updateTask } from './actions';
+import { useSelector } from 'react-redux';
 
 import Input from './Input';
 
 function InputContainer() {
-  const dispatch = useDispatch();
   const taskTitle = useSelector((state) => (state.taskTitle));
-
-  function handleChangeTitle(event) {
-    dispatch(updateTask(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
 
   return (
     <Input
       value={taskTitle}
-      onChange={handleChangeTitle}
-      onClick={handleClickAddTask}
+      onChange={() => {}}
+      onClick={() => {}}
     />
   );
 }
