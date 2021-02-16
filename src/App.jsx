99af91cb8b479742ef36ac9b7d6d9ea7a8ea7946
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-16 18:55:22 +0900

    ListContainer.jsx, ListContainer.test.jsx, App.jsx: 추가 및 변경(App.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
+
 export default function App() {
   const { tasks } = useSelector((state) => ({
     tasks: state.tasks,
