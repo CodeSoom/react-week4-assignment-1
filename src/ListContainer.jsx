de4071aa529c80e3783855ce39737a60ecbc902f
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 21:52:09 +0900

    :recycle: Refactor: ListContainer

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { DELETE_TASK } from './actions';
 
 import List from './List';
 
 function ListContainer() {
   const tasks = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
-  function handleClickDelete() {
-    dispatch({ payload: 1, type: 'DELETE_TASK' });
+  function handleClickDelete(id) {
+    dispatch({ type: DELETE_TASK, payload: id });
   }
   return (
     <List
