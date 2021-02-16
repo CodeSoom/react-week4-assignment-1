8b9b4d439ab3ce9371353e49c8ca75036c18af49
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 17:17:59 +0900

    :recycle: Refactor: InputContainer.jsx

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTask, updateTask } from './actions';
 
 import Input from './Input';
 
 function InputContainer() {
+  const dispatch = useDispatch();
+  const taskTitle = useSelector((state) => (state.taskTitle));
+
+  function handleChangeTitle(e) {
+    dispatch(updateTask(e.target.value));
+  }
+
+  function handleClickAddTask() {
+    dispatch(addTask());
+  }
+
   return (
-    <Input value="손씻기" onChange={() => {}} />
+    <Input
+      value={taskTitle}
+      onChange={handleChangeTitle}
+      onClick={handleClickAddTask}
+    />
   );
 }
 
