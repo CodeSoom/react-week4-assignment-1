2f070ef3637060c241c1dbd200572e2e775b6270
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 17:09:24 +0900

    :construction: Green-Working: Make InputContainer pass the test

---
+++ src/InputContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Input from './Input';
+
+function InputContainer() {
+  return (
+    <Input value="손씻기" onChange={() => {}} />
+  );
+}
+
+export default InputContainer;
