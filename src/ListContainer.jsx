092d803479a39d7491f0d2db16b8132752756177
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 16:51:56 +0900

    :construction: Green-Working: pass ListContainer test

---
+++ src/ListContainer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { deleteTask } from './actions';
+import List from './List';
+
+function ListContainer() {
+  const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.tasks);
+
+  function handleClickDeleteTask(id) {
+    dispatch(deleteTask(id));
+  }
+
+  return (
+    <List
+      tasks={[{ id: 1, title: '할일1' }]}
+      onClickDelete={handleClickDeleteTask}
+    />
+  );
+}
+
+export default ListContainer;
