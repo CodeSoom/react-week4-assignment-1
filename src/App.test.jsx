d1a7ccfe170934a49c9d639e30ae041efd0a2762
dodoongtak <themighty.tak@gmail.com>
2021-02-16 17:33:08 +0900

    Add Test cases for App
    Add mockImplementation for useSelector

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [
+      { id: 1, tasks: 'TDD 졸잼...' },
+      { id: 2, tasks: 'TDD 진짜 재밌다...' },
+    ],
+  }));
+
   const { getByText } = render((
     <App />
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
