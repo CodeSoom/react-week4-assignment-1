f12518163ac366a6bf0bc368a9c56e05e5b722d8
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 21:50:28 +0900

    :white_check_mark: Red-Test: Add delete test

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ListContainer from './ListContainer';
+import { DELETE_TASK } from './actions';
 
 jest.mock('react-redux');
 
@@ -17,5 +18,21 @@ describe('ListContainer', () => {
     ));
 
     expect(queryByText('할일1')).toBeInTheDocument();
+  });
+
+  it('should delete task when click button', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementationOnce(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      tasks: [{ id: 1, title: '할일1' }],
+    }));
+
+    const { queryByText } = render((
+      <ListContainer />
+    ));
+
+    fireEvent.click(queryByText('완료'));
+
+    expect(dispatch).toHaveBeenCalledWith({ type: DELETE_TASK, payload: 1 });
   });
 });
