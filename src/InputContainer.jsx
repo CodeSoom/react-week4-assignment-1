0464c0be5acb800b74b861731e68a58fb3fbaa52
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 21:59:38 +0900

    :construction: Green-Working: Make pass the appending task test

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Input from './Input';
 
 function InputContainer() {
   const taskTitle = useSelector((state) => (state.taskTitle));
+  const dispatch = useDispatch();
+
+  function handleClickAddTask() {
+    dispatch({ type: 'ADD_TASK' });
+  }
 
   return (
     <Input
       value={taskTitle}
       onChange={() => {}}
-      onClick={() => {}}
+      onClick={handleClickAddTask}
     />
   );
 }
