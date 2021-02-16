8a0327a4f9cdb201c8ebd679c7a6860b505d608c
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 11:28:05 +0900

    :recycle: Refactor: reducer.js
    - reducer 에 action type 추가

--- src/reducer.js
+++ src/reducer.js
@@ -5,5 +5,14 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
-  return { tasks: [{ id: 1, title: '첫번째 할일' }] };
+  if (action.type === 'ADD_TASK') {
+    const { newId, taskTitle, tasks } = state;
+    return {
+      ...state,
+      newId: newId + 1,
+      taskTitle: '',
+      tasks: [...tasks, { id: newId, title: taskTitle }],
+    };
+  }
+  return state;
 }
