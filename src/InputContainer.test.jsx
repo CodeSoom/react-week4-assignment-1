781b8925a70df1545502b9ef950963025515108e
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 21:54:09 +0900

    :white_check_mark: Test:Refactor
    - describe 안에 test 넣음

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { useSelector, useDispatch } from 'react-redux';
+import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import InputContainer from './InputContainer';
-import { ADD_TASK } from './actions';
 
 jest.mock('react-redux');
 
-test('InputContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    taskTitle: '손씻기',
-  }));
+describe('InputContainer', () => {
+  it('should render label, input, button', () => {
+    useSelector.mockImplementationOnce((selector) => selector({
+      taskTitle: '손씻기',
+    }));
 
-  const dispatch = jest.fn();
-  useDispatch.mockImplementation(() => dispatch);
+    const { getByText, getByPlaceholderText } = render((
+      <InputContainer />
+    ));
 
-  const { getByText, getByPlaceholderText } = render((
-    <InputContainer />
-  ));
-
-  expect(getByText('추가')).toBeInTheDocument();
-  expect(getByText('할 일')).toBeInTheDocument();
-  expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe('손씻기');
-
-  fireEvent.click(getByText('추가'));
-
-  expect(dispatch).toHaveBeenCalledWith({ type: ADD_TASK });
+    expect(getByText('추가')).toBeInTheDocument();
+    expect(getByText('할 일')).toBeInTheDocument();
+    expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe('손씻기');
+  });
 });
