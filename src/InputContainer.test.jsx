6d8667fd73bb8d5550fabcd8b946aa524374568c
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 21:57:48 +0900

    :white_check_mark: Red-Test: Add append task test

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import InputContainer from './InputContainer';
+import { ADD_TASK } from './actions';
 
 jest.mock('react-redux');
 
@@ -19,5 +20,18 @@ describe('InputContainer', () => {
     expect(getByText('추가')).toBeInTheDocument();
     expect(getByText('할 일')).toBeInTheDocument();
     expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe('손씻기');
+  });
+
+  it('should dispatch ADD_TASK when click button', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementationOnce(() => dispatch);
+
+    const { getByText } = render((
+      <InputContainer />
+    ));
+
+    fireEvent.click(getByText('추가'));
+
+    expect(dispatch).toHaveBeenCalledWith({ type: ADD_TASK });
   });
 });
