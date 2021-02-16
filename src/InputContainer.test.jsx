649900263b8685a0f759e67c62ca35eb6cc98894
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-16 17:27:01 +0900

    :white_check_mark: Red-Test: Add input dispatch test

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import InputContainer from './InputContainer';
+import { ADD_TASK } from './actions';
 
 jest.mock('react-redux');
 
@@ -11,11 +12,18 @@ test('InputContainer', () => {
     taskTitle: '손씻기',
   }));
 
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
   const { getByText, getByPlaceholderText } = render((
     <InputContainer />
   ));
 
-  expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe('손씻기');
-  expect(getByText('할 일')).toBeInTheDocument();
   expect(getByText('추가')).toBeInTheDocument();
+  expect(getByText('할 일')).toBeInTheDocument();
+  expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe('손씻기');
+
+  fireEvent.click(getByText('추가'));
+
+  expect(dispatch).toHaveBeenCalledWith({ type: ADD_TASK });
 });
