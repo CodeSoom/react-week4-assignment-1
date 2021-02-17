714b87f25cb28190e8950db916474515137d2d6e
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-18 05:05:24 +0900

    ListContainer.test.jsx: 테스트 케이스 추가 및 테스트 커버리지 달성

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('ListContainer', () => {
+describe('ListContainer', () => {
   useSelector.mockImplementation((selector) => selector({
     tasks: [
       { id: 1, title: '아무 것도 하지 않기 #1' },
       { id: 2, title: '아무 것도 하지 않기 #2' },
     ],
   }));
-  const { getByText } = render((
-    <ListContainer />
-  ));
 
-  expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
-  expect(getByText(/아무 것도 하지 않기 #2/)).not.toBeNull();
+  context('without "완료" clicked', () => {
+    it("handleClickDelete doesn't work", () => {
+      const { queryByText } = render((<ListContainer />));
+
+      expect(queryByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+      expect(queryByText(/아무 것도 하지 않기 #2/)).not.toBeNull();
+    });
+  });
+
+  context('with "완료" clicked', () => {
+    it('handleClickDelete works', () => {
+      const dispatch = jest.fn();
+
+      useDispatch.mockImplementation(() => dispatch);
+
+      const { queryAllByText } = render((<ListContainer />));
+
+      fireEvent.click(queryAllByText(/완료/)[0]);
+
+      expect(dispatch).toBeCalledWith({
+        type: 'deleteTask',
+        payload: { id: 1 },
+      });
+    });
+  });
 });
