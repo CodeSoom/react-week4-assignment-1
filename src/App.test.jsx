5ba6dcaa8a1cc6dd3fce30f68e2ed0902a290213
datalater <the7mincheol@gmail.com>
2021-02-17 15:47:31 +0900

    App 컴포넌트에서 updateTaskTitle을 테스트하라

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
@@ -25,5 +25,17 @@ describe('App', () => {
     const { getByText } = renderApp();
 
     expect(getByText(/추가/)).not.toBeNull();
+  });
+
+  it('updates task title', () => {
+    const { getByLabelText, getByDisplayValue } = render((
+      <App />
+    ));
+
+    fireEvent.change(getByLabelText('할 일'), {
+      target: { value: 'task-1' },
+    });
+
+    expect(getByDisplayValue('task-1')).not.toBeNull();
   });
 });
