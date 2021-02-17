47897f23bbe8b0f3ba68983ebf656ac4eb490350
dodoongtak <themighty.tak@gmail.com>
2021-02-17 16:46:35 +0900

    Refactor InputContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
 import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
-import {
-  updateTaskTitle,
-  addTask,
-  deleteTask,
-} from './actions';
-
-import Page from './Page';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const { taskTitle, tasks } = useSelector((state) => ({
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  }));
-
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <div>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
