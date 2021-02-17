cddf99fba9a1ce86c9acc225762c4dd4fd04adfe
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-18 02:28:44 +0900

    Add reducer test code of add and delete case todo
    할일을 추가하거나 삭제하는 경우의 reducer test code를 추가했습니다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,58 @@
+import reducer from './reducer';
+
 describe('reducer', () => {
   it('사용자 입력을 반영합니다.', () => {
-    const state = reducer({ taskTitle: '' }, { taskTitle: '할 일2' });
+    const state = reducer({ taskTitle: '' },
+      {
+        type: 'updateTaskTitle',
+        payload: { taskTitle: '할 일' },
+      });
+
+    expect(state.taskTitle).toBe('할 일');
+    expect(state.tasks).toHaveLength(0);
+  });
+
+  it('할 일을 추가합니다.', () => {
+    const state = reducer({
+      newId: 0,
+      taskTitle: '',
+      tasks: [],
+    },
+    {
+      type: 'addTask',
+      payload: {
+        newId: 1,
+        taskTitle: '할 일1',
+        tasks: [{
+          id: this.newId,
+          title: this.taskTitle,
+        }],
+      },
+    });
+
+    expect(state.taskTitle).toBe('할 일1');
+    expect(state.tasks).toHaveLength(1);
+  });
+
+  it('완료한 일을 삭제합니다.', () => {
+    const state = reducer({
+      newId: 1,
+      taskTitle: '할 일1',
+      tasks: [{
+        id: 1,
+        title: '할 일1',
+      }],
+    },
+    {
+      type: 'delteTask',
+      payload: {
+        newId: 0,
+        taskTitle: '',
+        tasks: [],
+      },
+    });
+
+    expect(state.taskTitle).toBe('할 일1');
+    expect(state.tasks).toHaveLength(0);
   });
 });
