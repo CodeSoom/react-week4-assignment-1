218decef558088610a9b19232d0a1feca53121fb
EL <ickyicky@gmail.com>
2021-02-17 22:52:17 +0900

    TST: 테스트 수정

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('ListContainer', () => {
-  const tasks = [
-    { id: 1, title: '뭐라도 하기 #1' },
-    { id: 2, title: '뭐라도 하기 #2' },
-  ];
+describe('deleteTask', () => {
+  it(' 할일을 보여준다. ', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => (dispatch));
 
-  useSelector.mockImplementation((selector) => selector({
-    tasks,
-  }));
+    const tasks = [
+      { id: 1, title: '뭐라도 하기 #1' },
+      { id: 2, title: '뭐라도 하기 #2' },
+    ];
 
-  const { getByText } = render((
-    <ListContainer />
-  ));
+    useSelector.mockImplementation((selector) => selector({
+      tasks,
+    }));
 
-  expect(getByText(/뭐라도 하기 #1/)).not.toBeNull();
+    const { getByText } = render((
+      <ListContainer />
+    ));
+
+    expect(getByText(/뭐라도 하기 #1/)).not.toBeNull();
+  });
+
+  // it(' 할일을 삭제한다. ', () => {
+  //   const dispatch = jest.fn();
+  //   useDispatch.mockImplementationOnce(() => dispatch);
+  //   useSelector.mockImplementation((selector) => selector({ 
+  //     tasks: [{ id: 1, taskTitle: '할일' }],
+  //   }));
+
+  //   const { getByText } = render(<ListContainer />);
+
+  //   fireEvent.click(getByText('완료'));
+
+  //   expect(dispatch).toBeCalled();
+  // });
 });
