fb8604125c6ab4062559bd6d7a00e7a616d8c1a2
Yoon <yoonhe@gmail.com>
2021-02-17 19:19:32 +0900

    addTask 액션에 대한 테스트를 추가한다

---
+++ src/InputContainer.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+import InputContainer from './InputContainer';
+
+jest.mock('react-redux');
+
+beforeEach(() => {
+  useSelector.mockImplementation((selector) => selector({
+    taskTitle: 'New Title',
+  }));
+
+  jest.clearAllMocks();
+});
+
+describe('InputContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  it('renders tasks', () => {
+    const { getByDisplayValue, getByText } = render((
+      <InputContainer />
+    ));
+
+    expect(getByDisplayValue('New Title')).not.toBeNull();
+    expect(getByText('추가')).not.toBeNull();
+  });
+
+  it('listens `추가` button click event', () => {
+    const { getByText } = render((
+      <InputContainer />
+    ));
+
+    fireEvent.click(getByText('추가'));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'addTask',
+    });
+  });
+});
