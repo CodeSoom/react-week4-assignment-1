f8cddb8eca1e41c36d9c016a8f71867ae458282e
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-18 20:29:42 +0900

    Add reducer test code in case nothing taskTitle
    taskTitle이 없는 경우를 test하는 reducer test code를 추가합니다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -11,28 +11,51 @@ describe('reducer', () => {
     expect(state.taskTitle).toBe('할 일');
   });
 
-  it('할 일을 추가합니다.', () => {
-    const state = reducer({
-      newId: 0,
-      taskTitle: '할 일1',
-      tasks: [],
-    },
-    {
-      type: 'addTask',
-      payload: {
-        newId: 1,
-        taskTitle: '',
-        tasks: [{
-          id: 1,
-          title: '할 일1',
-        }],
+  context('taskTitle이 있는 경우', () => {
+    it('할 일을 추가합니다.', () => {
+      const state = reducer({
+        newId: 0,
+        taskTitle: '할 일1',
+        tasks: [],
       },
-    });
+      {
+        type: 'addTask',
+        payload: {
+          newId: 1,
+          taskTitle: '',
+          tasks: [{
+            id: 1,
+            title: '할 일1',
+          }],
+        },
+      });
 
-    expect(state.newId).toBe(1);
-    expect(state.taskTitle).toBe('');
-    expect(state.tasks).toHaveLength(1);
-    expect(state.tasks[0].title).toBe('할 일1');
+      expect(state.newId).toBe(1);
+      expect(state.taskTitle).toBe('');
+      expect(state.tasks).toHaveLength(1);
+      expect(state.tasks[0].title).toBe('할 일1');
+    });
+  });
+
+  context('taskTitle이 없는 경우', () => {
+    it('아무 변화가 없습니다.', () => {
+      const state = reducer({
+        newId: 0,
+        taskTitle: '',
+        tasks: [],
+      }, {
+        type: 'addTask',
+        payload: {
+          newId: 0,
+          taskTitle: '',
+          tasks: [],
+        },
+      });
+
+      expect(state.newId).toBe(0);
+      expect(state.taskTitle).toBe('');
+      expect(state.tasks).toHaveLength(0);
+    });
   });
 
   it('완료한 일을 삭제합니다.', () => {
