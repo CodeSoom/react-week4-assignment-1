bb9bb126b486410478564707484f173054c2aef0
김민성 <yssj2049@gmail.com>
2021-02-18 23:04:03 +0900

    Initial commit for reducer test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import {useSelector} from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+
+  useSelector.mockImplementation((selector) => selector({
+    taskTitle:'',
+    tasks:[
+      {id:1, title:'아무것도 하지 않기 #1'},
+      {id:2, title:'아무것도 하지 않기 #2'},
+    ],
+  }));
+
   const { getByText } = render((
     <App />
   ));
