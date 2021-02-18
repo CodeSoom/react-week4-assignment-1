69143d79998eba7e99ca868aae1660ec9c3c9b7d
Yoon <yoonhe@gmail.com>
2021-02-18 10:30:53 +0900

    ListContainer 테스트코드 리팩토링

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -7,35 +7,31 @@ import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-beforeEach(() => {
-  jest.clearAllMocks();
-
-  const tasks = [
-    { id: 1, title: 'Task-1' },
-  ];
-
-  useSelector.mockImplementation((selector) => selector({
-    tasks,
-  }));
-});
-
 describe('ListContainer', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    const tasks = [
+      { id: 1, title: 'Task-1' },
+    ];
+
+    useSelector.mockImplementation((selector) => selector({
+      tasks,
+    }));
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
 
   it('renders tasks', () => {
-    const { getByText } = render((
-      <ListContainer />
-    ));
+    const { getByText } = render(<ListContainer />);
 
     expect(getByText('Task-1')).not.toBeNull();
   });
 
   it('listens `완료` button click event', () => {
-    const { getByText } = render((
-      <ListContainer />
-    ));
+    const { getByText } = render(<ListContainer />);
 
     fireEvent.click(getByText('완료'));
 
