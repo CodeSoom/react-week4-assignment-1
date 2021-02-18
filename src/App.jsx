f746c399d0e29abff4d9b3c4d27c1ab142a47489
daniel <daniel@innosonian.com>
2021-02-18 19:04:36 +0900

    updateTaskTitle 함수 추가, 관심사 분리

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,20 @@ const initalState = {
   tasks: [],
 };
 
+function updateTaskTitle(state, taskTitle) {
+  return {
+    ...state,
+    taskTitle,
+  };
+}
+
 export default function App() {
   const [state, setState] = useState(initalState);
 
   const { newId, taskTitle, tasks } = state;
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+    setState(updateTaskTitle(state, event.target.value));
   }
 
   function handleClickAddTask() {
