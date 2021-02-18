c73bb884617135a9459b1c7d5b560cd33b0501cf
김민성 <yssj2049@gmail.com>
2021-02-18 23:08:25 +0900

    Initial commit for reducer test

---
+++ src/__mocks__/react-redux.js
@@ -0,0 +1,8 @@
+export const useDispatch = jest.fn();
+export const useSelector = jest.fn((selector) => selecotr(
+  {tasks:[
+    {id:1, title:''},
+    {}
+  ],
+  }
+));
---
+++ src/actions.js
@@ -0,0 +1,23 @@
+export function addTask(){
+  return{
+    type:'addTask',
+  }
+}
+export function updateTaskTitle(taskTitle){
+  return {
+    type:'updateTaskTitle',
+    payload:{ 
+      taskTitle,
+    },  
+  }
+
+}
+export function deleteTask(id){
+return {
+  type:'deleteTask',
+  payload:{ 
+    id,
+  }
+
+}
+}
