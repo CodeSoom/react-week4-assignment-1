d514ba54f6e02b0a01225842ba9ae6e7ff7a4dc9
최창현 <chchoi_einz@einz.co.kr>
2021-02-18 11:24:13 +0900

    REFACTORING: Clean up codes

--- src/reducer.js
+++ src/reducer.js
@@ -4,30 +4,34 @@ const initialState = {
   tasks: [],
 };
 
-export default function reducer(state = initialState, action) {
-  const actionsTable = {
-    updateTaskTitle: () => ({
-      ...state,
-      taskTitle: action.payload.taskTitle,
-    }),
+const reducers = {
+  updateTaskTitle: ({ state, action }) => ({
+    ...state,
+    taskTitle: action.payload.taskTitle,
+  }),
 
-    addTask: () => {
-      if (!state.taskTitle) {
-        return state;
-      }
+  addTask: ({ state }) => {
+    if (!state.taskTitle) {
+      return state;
+    }
 
-      return {
-        newId: state.newId + 1,
-        taskTitle: '',
-        tasks: [...state.tasks, { id: state.newId, title: state.taskTitle }],
-      };
-    },
+    return {
+      newId: state.newId + 1,
+      taskTitle: '',
+      tasks: [...state.tasks, { id: state.newId, title: state.taskTitle }],
+    };
+  },
 
-    deleteTask: () => ({
-      ...state,
-      tasks: state.tasks.filter((task) => task.id !== action.payload.id),
-    }),
-  };
+  deleteTask: ({ state, action }) => ({
+    ...state,
+    tasks: state.tasks.filter((task) => task.id !== action.payload.id),
+  }),
+};
 
-  return actionsTable[(action?.type)] ? actionsTable[(action?.type)]() : state;
+export default function reducer(state = initialState, action = { type: '' }) {
+  return (
+    reducers[action.type]
+      ? reducers[action.type]({ state, action })
+      : state
+  );
 }
