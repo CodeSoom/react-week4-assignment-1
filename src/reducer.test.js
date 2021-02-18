0286fb3e65603e3dfe3659984b526d5bbad6a308
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-18 20:38:01 +0900

    Refactor action of updating title
    taskTitle을 업데이트하는 경우의 액션에 해당하는 함수를 import해서
    사용하도록 리팩토링했습니다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,12 +1,10 @@
+import updateTaskTitle from './actions';
 import reducer from './reducer';
 
 describe('reducer', () => {
   it('사용자 입력을 반영합니다.', () => {
     const state = reducer({ taskTitle: '' },
-      {
-        type: 'updateTaskTitle',
-        payload: { taskTitle: '할 일' },
-      });
+      updateTaskTitle('할 일'));
 
     expect(state.taskTitle).toBe('할 일');
   });
