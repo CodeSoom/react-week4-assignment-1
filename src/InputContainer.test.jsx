3a921c0cdd3f1907bc551b37defcd9a8f2152ee5
김민성 <yssj2049@gmail.com>
2021-02-19 21:51:27 +0900

    Change getBy with qureyBy

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -12,15 +12,11 @@ test('InputContainer', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-
+  beforeEach(() => useSelector.mockImplementation((selector) => selector({
     taskTitle: 'New Title',
+  })));
 
-  }));
-
-  const { getByText, getByDisplayValue } = render((
-    <InputContainer />
-  ));
+  const { getByText, getByDisplayValue } = render((<InputContainer />));
 
   expect(getByText(/추가/)).not.toBeNull();
   expect(getByDisplayValue(/New Title/)).not.toBeNull();
@@ -28,8 +24,4 @@ test('InputContainer', () => {
   fireEvent.click(getByText(/추가/));
 
   expect(dispatch).toBeCalledWith({ type: 'addTask' });
-  // expect(getByText(/New Title/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
