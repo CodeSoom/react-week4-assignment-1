75b160b4065ca5e02b2b487f053b7966d6cfd439
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-19 21:46:44 +0900

    Make InputContainer using useDispatch and useSelector
    useDispatch와 useSelector을 이용해서 InputContainer를 구현했습니다.

--- src/InputContainer.jsx
+++ src/InputContainer.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
-export default function InputContainer({
-  taskTitle, onChangeTitle, onClickAddTask,
-}) {
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  updateTaskTitle,
+  addTask,
+} from './actions';
+
+export default function InputContainer() {
+  const dispatch = useDispatch;
+  const { taskTitle } = useSelector((state) => ({
+    taskTitle: state.title,
+  }));
+
+  function handleChangeTitle(event) {
+    dispatch(updateTaskTitle(event.target.value));
+  }
+
+  function handleClickAddTask() {
+    dispatch(addTask());
+  }
+
   return (
     <InputContainer
       value={taskTitle}
-      onChange={onChangeTitle}
-      onClick={onClickAddTask}
+      onChange={handleChangeTitle}
+      onClick={handleClickAddTask}
     />
   );
 }
