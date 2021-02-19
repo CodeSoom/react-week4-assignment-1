1988949324d793db487631208d921529ca854c6c
seokho <loyu78@gmail.com>
2021-02-19 18:30:46 +0900

    상태를 관리하는 부분과 화면에 그려주는 부분을 분리

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,56 @@ import React, { useState } from 'react';
 
 import Page from './Page';
 
-export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
 
-  const { newId, taskTitle, tasks } = state;
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+function updateTaskTitle(state, taskTitle) {
+  return {
+    ...state,
+    taskTitle,
+  };
+}
+
+function addTask(state) {
+  const {newId, taskTitle, tasks} = state;
+  return{
+    ...state,
+    newId: newId + 1,
+    taskTitle: '',
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  };
+}
+
+function deleteTask(state, id){
+  const { tasks } = state;
+  
+  return{
+    ...state,
+    tasks: tasks.filter((task) => task.id !== id),
+  };
+}
+
+export default function App() {
+
+  const [state, setState] = useState(initialState);
+
+  const { taskTitle, tasks } = state;
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+    setState(updateTaskTitle(state, event.target.value));
   }
 
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
+    setState(addTask(state));
+
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
+    setState(deleteTask(state, id));
   }
 
   return (
