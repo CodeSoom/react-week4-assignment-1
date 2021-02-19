c386d3112a3f63cab4f7bda0c7073a8e406a45bb
datalater <the7mincheol@gmail.com>
2021-02-19 20:27:06 +0900

    이전 상태가 주어졌을 때와 그렇지 않을 때로 테스트 context를 구분하라

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -7,73 +7,7 @@ import {
 } from './actions';
 
 describe('reducer', () => {
-  describe('updateTaskTitle', () => {
-    const previousState = {
-      newId: 100,
-      taskTitle: '',
-      tasks: [],
-    };
-
-    it('changes task title', () => {
-      const state = reducer(previousState, updateTaskTitle('new-title'));
-
-      expect(state.taskTitle).toBe('new-title');
-    });
-
-    context('without previous state offered', () => {
-      it('changes task title using initalState', () => {
-        const state = reducer(undefined, updateTaskTitle('new-title'));
-
-        expect(state.taskTitle).toBe('new-title');
-      });
-    });
-  });
-
-  describe('addTask', () => {
-    it('adds new task', () => {
-      const previousState = {
-        newId: 100,
-        taskTitle: 'new-title',
-        tasks: [],
-      };
-
-      const state = reducer(previousState, addTask());
-
-      expect(state.tasks).toHaveLength(1);
-    });
-
-    context('with empty task title', () => {
-      const previousState = {
-        newId: 100,
-        taskTitle: '',
-        tasks: [],
-      };
-
-      it('does not add new task', () => {
-        const state = reducer(previousState, addTask());
-
-        expect(state.tasks).toHaveLength(0);
-      });
-    });
-  });
-
-  describe('deleteTask', () => {
-    const previousState = {
-      newId: 100,
-      taskTitle: '',
-      tasks: [
-        { id: 1, taskTitle: 'task to be deleted' },
-      ],
-    };
-
-    it('deletes new task', () => {
-      const state = reducer(previousState, deleteTask(1));
-
-      expect(state.tasks).toHaveLength(0);
-    });
-  });
-
-  describe('unregisteredAction', () => {
+  context('without registerd action type', () => {
     const previousState = {
       newId: 100,
       taskTitle: '',
@@ -89,6 +23,86 @@ describe('reducer', () => {
       });
 
       expect(state).toEqual(previousState);
+    });
+  });
+
+  context('with registered action type', () => {
+    describe('updateTaskTitle', () => {
+      context('without previous state', () => {
+        it('changes task title using initalState', () => {
+          const state = reducer(undefined, updateTaskTitle('new-title'));
+
+          expect(state.taskTitle).toBe('new-title');
+        });
+      });
+
+      context('with previous state', () => {
+        const previousState = {
+          newId: 100,
+          taskTitle: '',
+          tasks: [],
+        };
+
+        it('changes task title', () => {
+          const state = reducer(previousState, updateTaskTitle('new-title'));
+
+          expect(state.taskTitle).toBe('new-title');
+        });
+
+        context('without previous state offered', () => {
+          it('changes task title using initalState', () => {
+            const state = reducer(undefined, updateTaskTitle('new-title'));
+
+            expect(state.taskTitle).toBe('new-title');
+          });
+        });
+      });
+    });
+
+    describe('addTask', () => {
+      context('with task title', () => {
+        it('adds new task', () => {
+          const previousState = {
+            newId: 100,
+            taskTitle: 'new-title',
+            tasks: [],
+          };
+
+          const state = reducer(previousState, addTask());
+
+          expect(state.tasks).toHaveLength(1);
+        });
+      });
+
+      context('without task title', () => {
+        const previousState = {
+          newId: 100,
+          taskTitle: '',
+          tasks: [],
+        };
+
+        it('does not add new task', () => {
+          const state = reducer(previousState, addTask());
+
+          expect(state.tasks).toHaveLength(0);
+        });
+      });
+    });
+
+    describe('deleteTask', () => {
+      const previousState = {
+        newId: 100,
+        taskTitle: '',
+        tasks: [
+          { id: 1, taskTitle: 'task to be deleted' },
+        ],
+      };
+
+      it('deletes new task', () => {
+        const state = reducer(previousState, deleteTask(1));
+
+        expect(state.tasks).toHaveLength(0);
+      });
     });
   });
 });
