a15129ef99ec62cefb80ebe06b7631c9ebb4da24
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-20 02:54:56 +0900

    ListContainer을 테스트하는데, 할 일없는 경우를 추가합니다.

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -8,16 +8,35 @@ import ListContainer from './ListContainer';
 
 jest.mock('reat-redux');
 
-test('ListContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    tasks: [
-      { id: '1', title: 'TDD연습하기' },
-      { id: '2', title: 'TDD연습 두번하기' },
-    ],
-  }));
-  const { getByText } = render((
-    <ListContainer />
-  ));
+describe('ListContainer', () => {
+  context('할 일이 없는 경우', () => {
+    it('할 일이 없음을 출력합니다.', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [],
+      }));
 
-  expect(getByText(/TDD연습하기/)).not.toBeNull();
+      const { getByText } = render((
+        <ListContainer />
+      ));
+
+      expect(getByText(/할 일이 없어요/)).not.toBeNull();
+    });
+  });
+
+  context('할 일이 있는 경우', () => {
+    it('할 일이 있음을 출력합니다.', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [
+          { id: '1', title: 'TDD연습하기' },
+          { id: '2', title: 'TDD연습 두번하기' },
+        ],
+      }));
+
+      const { getByText } = render((
+        <ListContainer />
+      ));
+
+      expect(getByText(/TDD연습하기/)).not.toBeNull();
+    });
+  });
 });
