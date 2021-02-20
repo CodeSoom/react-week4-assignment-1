ed65e696c771851f7fd94a3a3643bdd561f5cd2b
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-20 19:58:58 +0900

    Refactoring) ListContainer.test.jsx

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -9,15 +9,20 @@ import ListContainer from './ListContainer';
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    tasks: [
-      { id: 1, title: '아무 것도 하지 않기 #1' },
-      { id: 2, title: '아무 것도 하지 않기 #2' },
-    ],
-  }));
+  const dispatch = jest.fn();
 
-  context('without "완료" clicked', () => {
-    it("handleClickDelete doesn't work", () => {
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      tasks: [
+        { id: 1, title: '아무 것도 하지 않기 #1' },
+        { id: 2, title: '아무 것도 하지 않기 #2' },
+      ],
+    }));
+  });
+
+  context("when '완료' button isn't clicked", () => {
+    it('shows nothing deleted', () => {
       const { queryByText } = render((<ListContainer />));
 
       expect(queryByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
@@ -25,12 +30,8 @@ describe('ListContainer', () => {
     });
   });
 
-  context('with "완료" clicked', () => {
-    it('handleClickDelete works', () => {
-      const dispatch = jest.fn();
-
-      useDispatch.mockImplementation(() => dispatch);
-
+  context("when '완료' button is clicked", () => {
+    it('shows handleClickDelete working', () => {
       const { queryAllByText } = render((<ListContainer />));
 
       fireEvent.click(queryAllByText(/완료/)[0]);
