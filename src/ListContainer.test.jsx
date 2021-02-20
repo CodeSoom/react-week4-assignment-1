62ef627ff0d05d9e5c5af2ad1d5bf444cc3d3cf8
ErickRyu <rkggmdii@gmail.com>
2021-02-20 19:59:37 +0900

    Change test name in ListContainer

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -8,18 +8,20 @@ import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('ListContainer', () => {
-  const tasks = [
-    { id: 1, title: '아무것도 하지 않기 1' },
-    { id: 2, title: '아무것도 하지 않기 2' },
-  ];
+describe('ListContainer', () => {
+  it('list tasks', () => {
+    const tasks = [
+      { id: 1, title: '아무것도 하지 않기 1' },
+      { id: 2, title: '아무것도 하지 않기 2' },
+    ];
 
-  useSelector.mockImplementation((selector) => selector({
-    tasks,
-  }));
-  const { getByText } = render((
-    <ListContainer />
-  ));
+    useSelector.mockImplementation((selector) => selector({
+      tasks,
+    }));
+    const { getByText } = render((
+      <ListContainer />
+    ));
 
-  expect(getByText(/아무것도 하지 않기 1/)).not.toBeNull();
+    expect(getByText(/아무것도 하지 않기 1/)).not.toBeNull();
+  });
 });
