1d25af3b81323e2da9ec11abd56ea960521915a8
ErickRyu <rkggmdii@gmail.com>
2021-02-20 19:00:54 +0900

    Extract InputContainer

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 import Page from './Page';
@@ -12,21 +12,11 @@ test('Page', () => {
       { id: 2, title: 'Task-2' },
     ],
   }));
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTask = jest.fn();
 
   const { getByText } = render((
-    <Page
-      taskTitle=""
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <Page />
   ));
 
   expect(getByText(/Task-1/)).not.toBeNull();
   expect(getByText(/Task-2/)).not.toBeNull();
-
-  fireEvent.click(getByText('추가'));
-
-  expect(handleClickAddTask).toBeCalled();
 });
