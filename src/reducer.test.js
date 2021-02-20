342c34716ed590b6aed65a9a4945b170bbcc928d
daniel <daniel@innosonian.com>
2021-02-20 14:07:04 +0900

    reducer 테스트 'updateTaskTitle' 추가

---
+++ src/reducer.test.js
@@ -0,0 +1,21 @@
+import reducer from './reducer';
+
+describe('reducer', () => {
+  describe('updateTaskTitle', () => {
+    it('returns new state with new task title', () => {
+      const previousState = {
+        taskTitle: '',
+      };
+
+      const action = {
+        type: 'updateTaskTitle',
+        payload: {
+          taskTitle: 'New Title',
+        },
+      };
+      const state = reducer(previousState, action);
+
+      expect(state.taskTitle).toBe('New Title');
+    });
+  });
+});
