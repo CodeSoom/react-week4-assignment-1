79d00a06f690552760c796aa9329c173503edddc
ErickRyu <rkggmdii@gmail.com>
2021-02-20 19:47:11 +0900

    Test onChange in InputContainer

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -4,16 +4,42 @@ import { useDispatch, useSelector } from 'react-redux';
 import InputContainer from './InputContainer';
 
 jest.mock('react-redux');
-test('InputContainer', () => {
-  const dispatch = jest.fn();
-  useDispatch.mockImplementation(() => dispatch);
-  useSelector.mockImplementation((selector) => selector({
-    taskTitle: 'New Title',
-  }));
-  const { getByText } = render((
-    <InputContainer />
-  ));
-  expect(getByText(/추가/)).not.toBeNull();
-  fireEvent.click(getByText(/추가/));
-  expect(dispatch).toBeCalledWith({ type: 'addTask' });
+describe('InputContainer', () => {
+  it('adds task', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      taskTitle: 'New Title',
+    }));
+    const { getByText } = render((
+      <InputContainer />
+    ));
+    expect(getByText(/추가/)).not.toBeNull();
+    fireEvent.click(getByText(/추가/));
+    expect(dispatch).toBeCalledWith({ type: 'addTask' });
+  });
+
+  it('changes task title', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      taskTitle: 'New Title',
+    }));
+    const { getByLabelText } = render((
+      <InputContainer />
+    ));
+
+    fireEvent.change(getByLabelText('할 일'), {
+      target: { value: 'Changing Title' },
+    });
+
+    expect(dispatch).toBeCalledWith(
+      {
+        type: 'updateTaskTitle',
+        payload: {
+          taskTitle: 'Changing Title',
+        },
+      },
+    );
+  });
 });
