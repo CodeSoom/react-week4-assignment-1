e5be17cd004b85e9f425286fd7e725d482cf6d50
최한울 <hanul.choi95@nts-corp.com>
2021-05-25 03:43:28 +0900

    App 컴포넌트를 ListContainer와 InputContainer로 분리하라
    - App 컴포넌트 하나에 많은 관심사가 들어있었기에 관심사를 분리한다.
    - 관심사를 잘게 분리 할 수록 테스트가 쉬워지고, 유지보수가 쉬워진다.

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,14 +1,15 @@
+import React from 'react';
+
 import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
-import App from './App';
+import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('App', () => {
+test('ListContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    taskTitle: '헬로우',
     tasks: [
       { id: 1, title: '아무것도 하지 않기 #1' },
       { id: 2, title: '아무것도 하지 않기 #2' },
@@ -16,11 +17,8 @@ test('App', () => {
   }));
 
   const { getByText } = render((
-    <App />
+    <ListContainer />
   ));
 
-  expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  expect(getByText(/아무것도 하지 않기 #1/)).not.toBeNull();
 });
