91685e541bdfd30c52151b19e327abd2325d854e
yujonglee <yujonglee@g.skku.edu>
2021-05-24 13:27:15 +0900

    useSelector, useDispatch를 mock하여 App컴포넌트 테스트를 준비하라

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,13 +2,8 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
 
-  expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
