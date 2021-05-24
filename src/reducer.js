7c0cb26d9291dae9b1c556924ca74c8cf5ff0670
yujonglee <yujonglee@g.skku.edu>
2021-05-24 12:24:07 +0900

    updateTaskTitle 리듀서를 작성하라

--- src/reducer.js
+++ src/reducer.js
@@ -1,2 +1,15 @@
-const reducer = (state = initialState, action) => ({});
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'UpdateTaskTitle') {
+    return {
+      ...state,
+      taskTitle: action.payload.taskTitle,
+    };
+  }
+};
 export default reducer;
