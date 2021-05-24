b891ce66ae89d69f8307360475b59074972fdcfb
최한울 <hanul.choi95@nts-corp.com>
2021-05-25 01:46:19 +0900

    App 컴포넌트를 상태로 부터 분리하라
    - 상태 관련 로직을 가지고 있던 App 컴포넌트를 단계별로 분리
    - Redux를 활용하면 App 컴포넌트는 더이상 상태나 상태를 변경시키는 로직을
    가지고 있지 않아도 됨

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,19 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [
+      { id: 1, title: '아무것도 하지 않기 #1' },
+      { id: 2, title: '아무것도 하지 않기 #2' },
+    ],
+  }));
+
   const { getByText } = render((
     <App />
   ));
