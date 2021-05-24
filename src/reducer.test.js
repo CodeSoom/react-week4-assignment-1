c493676ff598552f90e259d297fd07dba909fc52
yujonglee <yujonglee@g.skku.edu>
2021-05-24 11:32:16 +0900

    updateTaskTitle 리듀서 테스트를 작성하라

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,7 +2,22 @@ import reducer from './reducer';
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
+    it('updates title of task', () => {
+      const state = {
+        taskTitle: '',
+      };
 
+      const action = {
+        type: 'UpdateTaskTitle',
+        payload: {
+          taskTitle: 'newTitle',
+        },
+      };
+
+      const newState = reducer(state, action);
+
+      expect(newState.taskTitle).toBe('newTitle');
+    });
   });
   describe('addTask', () => {
 
