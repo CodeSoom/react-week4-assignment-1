70b673e515502ca07d74896b8c70bb18fc2279b8
yujonglee <yujonglee@g.skku.edu>
2021-05-25 20:02:26 +0900

    InputContainer를 작성하라

--- src/container/InputContainer.jsx
+++ src/container/InputContainer.jsx
@@ -1,3 +1,28 @@
-export default function InputContainer() {
+import { useDispatch, useSelector } from 'react-redux';
 
+import Input from '../presentational/Input';
+import {
+  updateTaskTitle,
+  addTask,
+} from '../redux/actions';
+
+export default function InputContainer() {
+  const { taskTitle } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  function handleChangeTitle(e) {
+    dispatch(updateTaskTitle(e.target.value));
+  }
+
+  function handleClickAddTask() {
+    dispatch(addTask());
+  }
+
+  return (
+    <Input
+      value={taskTitle}
+      onChange={handleChangeTitle}
+      onClick={handleClickAddTask}
+    />
+  );
 }
