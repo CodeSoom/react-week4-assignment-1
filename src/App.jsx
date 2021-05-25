0126875e1fbb12d2cca39b591b00ca1215d1bf4f
yujonglee <yujonglee@g.skku.edu>
2021-05-25 20:12:38 +0900

    InputContainer을 상위에서 Input대신 사용하게하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 
 import {
-  updateTaskTitle,
-  addTask,
   deleteTask,
 } from './redux/actions';
 
 import Page from './presentational/Page';
 
 export default function App() {
-  const { taskTitle, tasks } = useSelector((state) => state);
+  const { tasks } = useSelector((state) => state);
   const dispatch = useDispatch();
-
-  function handleChangeTitle(e) {
-    dispatch(updateTaskTitle(e.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
 
   function handleClickDeleteTask(id) {
     dispatch(deleteTask(id));
@@ -27,9 +17,6 @@ export default function App() {
   return (
     <Page
       tasks={tasks}
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
       onClickDeleteTask={handleClickDeleteTask}
     />
   );
