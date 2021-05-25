afdd9aa9c7ea09c57e20db9cac5a9617e2cd7ca3
yujonglee <yujonglee@g.skku.edu>
2021-05-25 20:31:20 +0900

    ListContainer를 상위 컴포넌트에서 사용하게하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import {
-  deleteTask,
-} from './redux/actions';
-
 import Page from './presentational/Page';
 
 export default function App() {
-  const { tasks } = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <Page />
   );
 }
