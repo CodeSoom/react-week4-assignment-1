658e66295bf0dce2f0f67f74b9ea5501dd9906af
hanvit7 <hanvit7@gmail.com>
2021-05-26 01:29:44 +0900

    테스트 - reducer 테스트 추가
    deleteTask 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,10 @@
 import reducer from './reducer';
 
-import { addTask, updateTaskTitle } from './actions';
+import {
+  addTask,
+  deleteTask,
+  updateTaskTitle,
+} from './actions';
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
@@ -43,6 +47,32 @@ describe('reducer', () => {
         const state = reduceAddTask('');
 
         expect(state.tasks).toHaveLength(0);
+      });
+    });
+  });
+
+  describe('deleteTask', () => {
+    context('with existed task ID', () => {
+      it('remove the task from tasks', () => {
+        const state = reducer({
+          tasks: [
+            { id: 1, title: 'Task' },
+          ],
+        }, deleteTask(1));
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+
+    context('without existed task ID', () => {
+      it("doesn't work", () => {
+        const state = reducer({
+          tasks: [
+            { id: 1, title: 'Task' },
+          ],
+        }, deleteTask(100));
+
+        expect(state.tasks).toHaveLength(1);
       });
     });
   });
