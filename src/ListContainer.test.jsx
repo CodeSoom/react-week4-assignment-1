859a1d10645340827c142533b767c31551cb0d1b
hanvit7 <hanvit7@gmail.com>
2021-05-27 00:24:06 +0900

    테스트 - InputContainer 테스트 추가
    - 완료 버튼이 클릭 가능함

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,12 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
 test('ListContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     tasks: [
       { id: 1, title: '아무 것도 하지 않기 #1' },
@@ -14,12 +18,16 @@ test('ListContainer', () => {
     ],
   }));
 
-  const { getByText } = render((
+  const { getAllByText, getAllByRole, getByText } = render((
     <ListContainer />
   ));
 
   expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+  expect(getAllByRole('button')).toHaveLength(2);
 
-  // TODO: 통합 테스트 코드 작성
+  const buttons = getAllByText(/완료/);
+
+  fireEvent.click(buttons[0]);
+
   // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
