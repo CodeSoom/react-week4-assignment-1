a69fb25fb108696689fc644ca2746aa965839361
hanvit7 <hanvit7@gmail.com>
2021-05-26 12:21:46 +0900

    리팩토링 - InputContainer에 Input 관련 상태관리 추출
    - Input에 대한 상태 관리를 App에서 InputContainer로 리팩토링
    - InputContainer 테스트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
 import Page from './Page';
 
-import {
-  addTask,
-  updateTaskTitle,
-} from './actions';
-
 export default function App() {
-  const { taskTitle } = useSelector((state) => ({
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  }));
-
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <Page />
   );
 }
