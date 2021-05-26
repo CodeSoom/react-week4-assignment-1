d6b35035268409e8f5e3716a94bba7ce5add9996
hanvit7 <hanvit7@gmail.com>
2021-05-26 15:20:48 +0900

    테스트 - InputContainer 테스트 추가
    with taskTitle
    - 추가 버튼 클릭
    
    without taskTitle
    - 입력 값 변경 할 경우

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -6,19 +6,68 @@ import InputContainer from './InputContainer';
 
 jest.mock('react-redux');
 
-test('InputContainer', () => {
-  const dispatch = jest.fn();
+describe('InputContainer', () => {
+  context('with taskTitle', () => {
+    const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+    it('initial state', () => {
+      useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    taskTitle: 'New Title',
-  }));
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: 'New Title',
+      }));
 
-  const { getByText, getByDisplayValue } = render((
-    <InputContainer />
-  ));
+      const { getByText, getByDisplayValue } = render((
+        <InputContainer />
+      ));
 
-  expect(getByText(/추가/)).not.toBeNull();
-  expect(getByDisplayValue(/New Title/)).not.toBeNull();
+      expect(getByText(/추가/)).not.toBeNull();
+      expect(getByDisplayValue(/New Title/)).not.toBeNull();
+    });
+
+    it('추가 버튼 클릭 할 경우', () => {
+      useDispatch.mockImplementation(() => dispatch);
+
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: 'New Title',
+      }));
+
+      const { getByText } = render((
+        <InputContainer />
+      ));
+
+      fireEvent.click(getByText(/추가/));
+
+      expect(dispatch).toBeCalledWith({
+        type: 'addTask',
+      });
+    });
+  });
+
+  context('without taskTitle', () => {
+    const dispatch = jest.fn();
+
+    it('입력 값 변경', () => {
+      useDispatch.mockImplementation(() => dispatch);
+
+      useSelector.mockImplementation((selector) => selector({
+        taskTitle: '',
+      }));
+
+      const { getByLabelText } = render((
+        <InputContainer />
+      ));
+
+      fireEvent.change(getByLabelText('할 일'), {
+        target: { value: '무언가 하기' },
+      });
+
+      expect(dispatch).toBeCalledWith({
+        type: 'updateTaskTitle',
+        payload: {
+          taskTitle: '무언가 하기',
+        },
+      });
+    });
+  });
 });
