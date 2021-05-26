01ab856dfc148f4b7cf00f7d81bc692e10ca39f5
hanvit7 <hanvit7@gmail.com>
2021-05-26 15:17:53 +0900

    구현 - reducer의 undefined action type 예외 추가
    - 정의 되지 않은 action type이 들어올 경우 예외 처리

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,7 @@
 const initailState = {
   newId: 100,
   taskTitle: '',
-  tasks: [
-    { id: 1, title: '아무 것도 하지 않기 #1' },
-    { id: 2, title: '아무 것도 하지 않기 #2' },
-  ],
+  tasks: [],
 };
 
 // Redux action
