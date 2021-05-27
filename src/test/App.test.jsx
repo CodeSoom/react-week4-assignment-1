400f4440aaf38778f5d08fc839029ee591272511
dngwoodo <dngwoodo@gmail.com>
2021-05-28 03:20:31 +0900

    refactor: App test
    테스트 assert를 좀 더 추가

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -14,10 +14,22 @@ test('App', () => {
 
   useSelector.mockImplementation((selector) => selector({ tasks }));
 
-  const { getByText } = render((
+  const {
+    getByText,
+    getByRole, getAllByRole,
+    getByLabelText,
+  } = render((
     <App />
   ));
 
-  expect(getByText(/추가/)).not.toBeNull();
-  expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+  expect(getByRole('heading', { name: 'To-do' })).toBeInTheDocument();
+
+  // 프로젝트가 커진다면 이 부분은 이렇게 전부 테스트를 할 수가 없을 것 같은데 빼는게 맞을까?
+  expect(getByLabelText('할 일')).toBeInTheDocument();
+  expect(getByRole('button', { name: '추가' })).toBeInTheDocument();
+
+  tasks.forEach((task, index) => {
+    expect(getByText(task.title)).toBeInTheDocument();
+    expect(getAllByRole('button', { name: '완료' })[index]).toBeInTheDocument();
+  });
 });
