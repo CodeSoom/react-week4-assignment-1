704a057cfbccca8f7515e64d35c1117221ef4a78
dngwoodo <dngwoodo@gmail.com>
2021-05-28 02:52:50 +0900

    test: Add ListContainer test
    완료 버튼을 눌렀을 때 dispatch가 실행되는 테스트 추가. 근데 에러가 뜸. 디버깅 필요

--- src/test/container/ListContainer.test.jsx
+++ src/test/container/ListContainer.test.jsx
@@ -1,40 +1,56 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from '../../container/ListContainer';
 
 jest.mock('react-redux');
 
 describe('<ListContainer />', () => {
-  function renderListContainer() {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  function renderListContainer(tasks) {
+    useSelector.mockImplementation((selector) => selector({ tasks }));
+
     return render(
       <ListContainer />,
     );
   }
 
   context('with tasks', () => {
+    const tasks = [
+      { id: 1, title: '아무 것도 하지 않기 #1' },
+      { id: 2, title: '아무 것도 하지 않기 #2' },
+    ];
+
     it('renders tasks', () => {
-      const tasks = [
-        { id: 1, title: '아무 것도 하지 않기 #1' },
-        { id: 2, title: '아무 것도 하지 않기 #2' },
-      ];
-
-      useSelector.mockImplementation((selector) => selector({ tasks }));
-
-      const { getByText } = renderListContainer();
+      const { getByText } = renderListContainer(tasks);
 
       tasks.forEach((task) => expect(getByText(task.title)).toBeInTheDocument());
+    });
+
+    it('calls dispatch with "todos/deleteTask" action when click "완료" button', () => {
+      // onClickDelete is not a function이라고 뜨면서 테스트 실패
+      const { getAllByRole } = renderListContainer(tasks);
+
+      tasks.forEach((_, index) => {
+        fireEvent.click(getAllByRole('button', { name: '완료' })[index]);
+
+        expect(dispatch).toBeCalledWith({
+          type: 'todos/deleteTask',
+          payload: { id: tasks[index].id },
+        });
+      });
     });
   });
 
   context('without tasks', () => {
+    const tasks = [];
+
     it('renders no task message', () => {
-      const tasks = [];
-
-      useSelector.mockImplementation((selector) => selector({ tasks }));
-
-      const { getByText } = renderListContainer();
+      const { getByText } = renderListContainer(tasks);
 
       expect(getByText(/할 일이 없어요/)).toBeInTheDocument();
     });
