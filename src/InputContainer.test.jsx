4585e2d6152891b494dc56c93a72b8ca0202b167
seong-dodo <dyrnxm52@hanmail.net>
2021-05-28 22:43:20 +0900

    InputContainer 테스트 커버리지 완성 시키기
    - props로 받은 메서드에 대한 테스트 검증 추가

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -4,9 +4,11 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import InputContainer from './InputContainer';
 
+import { addTask, updateTaskTitle } from './actions';
+
 jest.mock('react-redux');
 
-test('InputContainer', () => {
+describe('InputContainer 관련 테스트', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
@@ -15,12 +17,25 @@ test('InputContainer', () => {
     taskTitle: 'New Title',
   }));
 
-  const { getByText, getByDisplayValue } = render(<InputContainer />);
+  it('handleClickAddTask가 호출되는지 확인하는 테스트입니다', () => {
+    const { getByText, getByDisplayValue } = render(<InputContainer />);
 
-  expect(getByText(/추가/)).not.toBeNull();
-  expect(getByDisplayValue(/New Title/)).not.toBeNull();
+    expect(getByText(/추가/)).not.toBeNull();
+    expect(getByDisplayValue(/New Title/)).not.toBeNull();
 
-  fireEvent.click(getByText(/추가/));
+    fireEvent.click(getByText(/추가/));
 
-  expect(dispatch).toBeCalledWith({ type: 'addTask' });
+    expect(dispatch).toBeCalledWith(addTask());
+  });
+
+  it('handleChangeTitle이 호출되는지 확인하는 테스트입니다', () => {
+    const { getByPlaceholderText } = render(<InputContainer />);
+
+    fireEvent.change(
+      getByPlaceholderText('할 일을 입력해 주세요'),
+      { target: { value: '할일1' } },
+    );
+
+    expect(dispatch).toBeCalledWith(updateTaskTitle('할일1'));
+  });
 });
