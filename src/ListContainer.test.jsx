265dcffe36abdaba5fa0693ff6c2c34bc0be52cd
seong-dodo <dyrnxm52@hanmail.net>
2021-05-28 23:13:52 +0900

    ListContainer 테스트 커버리지 완성 시키기&  과제 요구사항 완료
    - props로 받은 메서드에 대한 테스트 검증 추가
    - ESLint, 인수테스트, 테스트 커버리지 100% 완료

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,20 +1,41 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
+import { deleteTask } from './actions';
+
 jest.mock('react-redux');
 
-test('ListContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    tasks: [
-      { id: 1, title: 'Task-1' },
-      { id: 2, title: 'Task-2' },
-    ],
-  }));
+describe('ListContainer 관련 테스트', () => {
+  it('tasks', () => {
+    useSelector.mockImplementation((selector) => selector({
+      tasks: [
+        { id: 1, title: 'Task-1' },
+        { id: 2, title: 'Task-2' },
+      ],
+    }));
 
-  const { getByText } = render(<ListContainer />);
+    const { getByText } = render(<ListContainer />);
 
-  expect(getByText(/Task-1/)).not.toBeNull();
+    expect(getByText(/Task-1/)).not.toBeNull();
+  });
+
+  it('handleClickDeleteTask가 호출되는지 확인하는 테스트입니다', () => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      tasks: [
+        { id: 1, title: 'Task-1' },
+      ],
+    }));
+
+    const { getByText } = render(<ListContainer />);
+
+    fireEvent.click(getByText('완료'));
+
+    expect(dispatch).toBeCalledWith(deleteTask(1));
+  });
 });
