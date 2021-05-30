ae83314814d907e1d703e1f7d16bff095a1c4cda
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-30 17:34:53 +0900

    refactor: Page Component 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
-import { updateTaskTitle, addTask } from '../redux/actions';
+import InputContainer from './Input/InputContainer';
+import ListContainer from './List/ListContainer';
 
-import Page from './Page';
-
-const selector = (state) => ({
-  taskTitle: state.taskTitle,
-  tasks: state.tasks,
-});
-
-const App = () => {
-  const { taskTitle } = useSelector((state) => selector(state));
-
-  const dispatch = useDispatch();
-
-  const handleChangeTitle = ({ target: { value } }) => dispatch(updateTaskTitle(value));
-  const handleClickAddTask = () => dispatch(addTask());
-
-  return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
-  );
-};
+const App = () => (
+  <div>
+    <h1>To-do</h1>
+    <InputContainer />
+    <ListContainer />
+  </div>
+);
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,15 +6,20 @@ import App from './App';
 jest.mock('react-redux');
 
 test('App', () => {
-  const tasks = [{ id: 1, title: '아무것도 하지 않기' }, { id: 2, title: '아무것도 하지 않기2' }];
-  useSelector.mockImplementation((selector) => selector({ tasks }));
+  const state = {
+    taskTitle: '',
+    tasks: [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ],
+  };
+  useSelector.mockImplementation((selector) => selector(state));
 
   const { getByText } = render((
     <App />
   ));
 
+  expect(getByText(/Task-1/)).not.toBeNull();
+  expect(getByText(/Task-2/)).not.toBeNull();
   expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
