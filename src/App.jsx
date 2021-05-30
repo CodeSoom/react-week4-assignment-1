9b1abac5aefa083336552b3a0891ba93a227e7b0
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-30 16:44:29 +0900

    feat: ListContainer Component 생성 및 로직 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { updateTaskTitle, addTask, deleteTask } from '../redux/actions';
+import { updateTaskTitle, addTask } from '../redux/actions';
 
 import Page from './Page';
 
@@ -11,21 +11,18 @@ const selector = (state) => ({
 });
 
 const App = () => {
-  const { taskTitle, tasks } = useSelector((state) => selector(state));
+  const { taskTitle } = useSelector((state) => selector(state));
 
   const dispatch = useDispatch();
 
   const handleChangeTitle = ({ target: { value } }) => dispatch(updateTaskTitle(value));
   const handleClickAddTask = () => dispatch(addTask());
-  const handleClickDeleteTask = (id) => dispatch(deleteTask(id));
 
   return (
     <Page
       taskTitle={taskTitle}
       onChangeTitle={handleChangeTitle}
       onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
     />
   );
 };
