c21da086a7698f4582d80eac80f6987861d0573e
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-30 15:27:17 +0900

    test: redux 적용한 테스트 코드 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,14 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const tasks = [{ id: 1, title: '아무것도 하지 않기' }, { id: 2, title: '아무것도 하지 않기2' }];
+  useSelector.mockImplementation((selector) => selector({ tasks }));
+
   const { getByText } = render((
     <App />
   ));
