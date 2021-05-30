559187628d13c4805a865110db89b517f53653e9
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-30 15:59:59 +0900

    test: reducer 테스트

--- redux/store.js
+++ redux/store.js
@@ -1,41 +1,5 @@
 import { createStore } from 'redux';
-
-const initialState = {
-  newId: 100,
-  taskTitle: '',
-  tasks: [],
-};
-
-const reducer = (state = initialState, action) => {
-  if (action.type === 'updateTaskTitle') {
-    return {
-      ...state,
-      taskTitle: action.payload.taskTitle,
-    };
-  }
-
-  if (action.type === 'addTask') {
-    const { newId, taskTitle, tasks } = state;
-
-    return {
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    };
-  }
-
-  if (action.type === 'deleteTask') {
-    const { tasks } = state;
-
-    return {
-      ...state,
-      tasks: tasks.filter((task) => task.id !== action.payload.id),
-    };
-  }
-
-  return state;
-};
+import reducer from './reducer';
 
 const store = createStore(reducer);
 
