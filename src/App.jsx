c3bfbea68e9a52ca604a454cce0eaed17f51874b
studioLuda <studio.luda.work@gmail.com>
2021-06-06 21:42:30 +0900

    진행중 : redux 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,54 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Page from './Page';
 
-export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
+/* 목표 : hook을 제거하고, redux를 사용하라.
 
-  const { newId, taskTitle, tasks } = state;
+할 일 순 서
+1. 리덕스 설치
+2. handleChangeTitle을 리팩토링 하기 -> redux객체에 저장하고 불러오기.
+
+리덕스 객체를 저장하고 사용하려면 어떻게 하지?
+(1) store를 작성하고, index.jsx 에서 사용할 것을 선언하기.
+(2) reducer를 작성하기. (계획하기)
+(3) taskTitle을 재 정의 하기 = redux로부터 useSelector를 통해 값 얻기.
+
+3. addTask 리팩토링하기
+4. deleteTask 리팩토링하기
+5. action파일 분리하기
+6. reducer파일 분리하기
+*/
+
+export default function App() {
+  const { taskTitle, tasks } = useSelector((reduxState) => ({
+    taskTitle: reduxState.taskTitle,
+    tasks: reduxState.tasks,
+  }));
+
+  const dispatch = useDispatch();
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
+    const input = event.target.value;
+    dispatch({
+      type: 'updateTaskTitle',
+      payload: {
+        taskTitle: input,
+      },
     });
   }
 
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
+    dispatch({
+      type: 'addTask',
     });
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
+    dispatch({
+      type: 'deleteTask',
+      payload: {
+        id,
+      },
     });
   }
 
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,19 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
+jest.mock('react-redux');
 
-  expect(getByText(/추가/)).not.toBeNull();
-
+describe('App', () => {
   // TODO: 통합 테스트 코드 작성
   // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [],
+  }));
+  it('renders value', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText(/추가/)).not.toBeNull();
+  });
 });
