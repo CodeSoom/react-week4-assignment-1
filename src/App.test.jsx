2a407b4476f8b3637c63f834549989a65b728202
passwd10 <parkinseo94@naver.com>
2021-08-31 22:32:07 +0900

    feat: 4-3강 실습

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,6 +8,7 @@ jest.mock('react-redux');
 
 test('App', () => {
   useSelector.mockImplementation((selector) => selector({
+    taskTitle: '',
     tasks: [
       { id: 1, title: '아무것도 하지않기 #1' },
       { id: 2, title: '아무것도 하지않기 #2' },
@@ -19,6 +20,7 @@ test('App', () => {
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
+  expect(getByText(/아무것도 하지않기 #1/)).not.toBeNull();
 
   // TODO: 통합 테스트 코드 작성
   // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
