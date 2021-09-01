737f57f7df52a7ee9b0e260648b73b40055f4c77
kokoball <kbp4154@naver.com>
2021-09-01 22:42:55 +0900

    React-week4-assignment-1
    강의 따라잡기

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -4,13 +4,12 @@ import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
-import App from './App';
+import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('App', () => {
+test('ListContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    taskTitle: '',
     tasks: [
       { id: 1, title: '아무 것도 하지 않기 #1' },
       { id: 2, title: '아무 것도 하지 않기 #2' },
@@ -18,12 +17,8 @@ test('App', () => {
   }));
 
   const { getByText } = render((
-    <App />
+    <ListContainer />
   ));
 
-  expect(getByText(/추가/)).not.toBeNull();
   expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
