6696a08363c4ca1ddc644990ce9fda3fd4bbc998
psy082 <zhappy114@gmail.com>
2021-09-01 23:15:23 +0900

    refactor: 상태 관리 로직 store로 이동
    - App component에 있는 상태 관리 로직 store로 이동
    - App component는 redux의 dispatch와 selector를 사용하도록 변경

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,37 @@
 import { createStore } from 'redux';
 
-const reducer = () => ({});
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+function reducer(state = initialState, action) {
+  const { newId, taskTitle, tasks } = state;
+  switch (action.type) {
+  case 'updateTaskTitle':
+    return {
+      ...state,
+      taskTitle: action.payload.taskTitle,
+    };
+  case 'addTask':
+    return {
+      ...state,
+      newId: newId + 1,
+      taskTitle: '',
+      tasks: [...state.tasks, { id: newId, title: taskTitle }],
+    };
+  case 'deleteTask':
+    return {
+      ...state,
+      tasks: tasks.filter((task) => task.id !== action.payload.id),
+    };
+  default:
+    break;
+  }
+
+  return state;
+}
 
 const store = createStore(reducer);
 
