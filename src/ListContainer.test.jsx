ca638dc615fbe58dc702680e36ded72b9bdea6aa
hanabyhana <yaong1002@gmail.com>
2021-09-02 20:49:02 +0900

    test: ListContainer에 대한 테스트를 추가한다

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,12 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('ListContainer', () => {
+describe('ListContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     tasks: [
       { id: 1, title: '아무것도 하지 않기 #1' },
@@ -14,9 +18,27 @@ test('ListContainer', () => {
     ],
   }));
 
-  const { getByText } = render((
-    <ListContainer />
-  ));
+  it('renders tasks', () => {
+    const { getByText } = render((
+      <ListContainer />
+    ));
 
-  expect(getByText(/아무것도 하지 않기 #1/)).not.toBeNull();
+    expect(getByText(/아무것도 하지 않기 #1/)).not.toBeNull();
+    expect(getByText(/아무것도 하지 않기 #2/)).not.toBeNull();
+  });
+
+  it('deletes a task', () => {
+    const { getAllByText } = render((
+      <ListContainer />
+    ));
+
+    fireEvent.click(getAllByText(/완료/)[0]);
+
+    expect(dispatch).toBeCalledWith({
+      type: 'deleteTask',
+      payload: {
+        id: 1,
+      },
+    });
+  });
 });
