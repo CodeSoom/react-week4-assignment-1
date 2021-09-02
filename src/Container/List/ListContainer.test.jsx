051435f1676abc772e75dd50355ab5f223bf50db
psy082 <zhappy114@gmail.com>
2021-09-02 23:41:39 +0900

    test: ListContainer test 수정
    - 모든 task 문구에 대한 test 추가

--- src/Container/List/ListContainer.test.jsx
+++ src/Container/List/ListContainer.test.jsx
@@ -19,6 +19,5 @@ test('ListContainer', () => {
   ));
 
   expect(getByText(/아무것도 하지 않기 #1/)).not.toBeNull();
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  expect(getByText(/아무것도 하지 않기 #2/)).not.toBeNull();
 });
