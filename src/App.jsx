1b68f684b44ae7ce6414c57826dfd7099887cc06
psy082 <zhappy114@gmail.com>
2021-09-03 00:07:05 +0900

    refactor: InputContainer 분리
    - taskTitle 상태와 관련된 로직 이동
    - InputContiner를 위한 test 작성
    - Page, Page test 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import Page from './Container/Page/Page';
-
-import {
-  updateTaskTitle,
-  addTask,
-} from './store/actions';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-  };
-}
+import InputContainer from './Container/Input/InputContainer';
+import ListContainer from './Container/List/ListContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  const { taskTitle } = useSelector(selector);
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
 jest.mock('react-redux');
 
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     tasks: [
       { id: 1, title: '아무것도 하지 않기 #1' },
@@ -19,7 +23,9 @@ test('App', () => {
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
+  expect(getByText(/아무것도 하지 않기 #1/)).not.toBeNull();
+  expect(getByText(/아무것도 하지 않기 #2/)).not.toBeNull();
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  fireEvent.click(getByText(/추가/));
+  expect(dispatch).toBeCalled();
 });
