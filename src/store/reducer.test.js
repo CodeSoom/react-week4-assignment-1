59c8e61b3ce5e4f0bc0ab0cd7209cdde9851fcbe
passwd10 <parkinseo94@naver.com>
2021-09-03 10:31:05 +0900

    test: reducer의 테스트 명세를 수정한다
    - action의 존재여부를 파악한다

--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -3,83 +3,83 @@ import reducer from './reducer';
 import { updateTaskTitle, addTask, deleteTask } from './actions';
 
 describe('reducer', () => {
-  describe('updateTaskTitle', () => {
-    it('changes task title', () => {
-      const state = reducer({
-        taskTitle: '',
-      }, updateTaskTitle('New Title'));
-
-      expect(state.taskTitle).toBe('New Title');
-    });
-  });
-
-  describe('addTask', () => {
-    function reduceAddTask(taskTitle) {
-      return reducer({
-        newId: 100,
-        taskTitle,
-        tasks: [],
-      }, addTask());
-    }
-
-    context('with task title', () => {
-      it('appends a new task into tasks', () => {
-        const state = reduceAddTask('New Task');
-
-        expect(state.tasks).toHaveLength(1);
-        expect(state.tasks[0].id).not.toBeUndefined();
-        expect(state.tasks[0].title).toBe('New Task');
-      });
-
-      it('clears task title', () => {
-        const state = reduceAddTask('New Task');
-
-        expect(state.taskTitle).toBe('');
-      });
-    });
-
-    context('without task title', () => {
-      it("doesn't work", () => {
-        const state = reduceAddTask('');
-
-        expect(state.tasks).toHaveLength(0);
-      });
-    });
-  });
-
-  describe('deleteTask', () => {
-    context('with existed task ID', () => {
-      it('removes the task from tasks', () => {
+  context('with existed action', () => {
+    describe('updateTaskTitle', () => {
+      it('changes task title', () => {
         const state = reducer({
-          tasks: [{ id: 1, title: 'Task' }],
-        }, deleteTask(1));
+          taskTitle: '',
+        }, updateTaskTitle('New Title'));
 
-        expect(state.tasks).toHaveLength(0);
+        expect(state.taskTitle).toBe('New Title');
       });
     });
 
-    context('without existed task ID', () => {
-      it("doesn't work", () => {
-        const state = reducer({
-          tasks: [{ id: 1, title: 'Task' }],
-        }, deleteTask(100));
+    describe('addTask', () => {
+      function reduceAddTask(taskTitle) {
+        return reducer({
+          newId: 100,
+          taskTitle,
+          tasks: [],
+        }, addTask());
+      }
 
-        expect(state.tasks).toHaveLength(1);
+      context('with task title', () => {
+        it('appends a new task into tasks', () => {
+          const state = reduceAddTask('New Task');
+
+          expect(state.tasks).toHaveLength(1);
+          expect(state.tasks[0].id).not.toBeUndefined();
+          expect(state.tasks[0].title).toBe('New Task');
+        });
+
+        it('clears task title', () => {
+          const state = reduceAddTask('New Task');
+
+          expect(state.taskTitle).toBe('');
+        });
+      });
+
+      context('without task title', () => {
+        it("doesn't work", () => {
+          const state = reduceAddTask('');
+
+          expect(state.tasks).toHaveLength(0);
+        });
+      });
+    });
+
+    describe('deleteTask', () => {
+      context('with existed task ID', () => {
+        it('removes the task from tasks', () => {
+          const state = reducer({
+            tasks: [{ id: 1, title: 'Task' }],
+          }, deleteTask(1));
+
+          expect(state.tasks).toHaveLength(0);
+        });
+      });
+
+      context('without existed task ID', () => {
+        it("doesn't work", () => {
+          const state = reducer({
+            tasks: [{ id: 1, title: 'Task' }],
+          }, deleteTask(100));
+
+          expect(state.tasks).toHaveLength(1);
+        });
       });
     });
   });
 
-  it("returns previous state when action doesn't exist", () => {
-    const clearAllTask = () => ({
-      type: 'clearAllTask',
+  context('with none existed action', () => {
+    it('returns previous state', () => {
+      const previousState = {
+        tasks: [{ id: 1, title: 'Task' }],
+      };
+
+      const nextState = reducer(previousState, { type: undefined });
+
+      expect(previousState).toEqual(nextState);
     });
-
-    const previousState = {
-      tasks: [{ id: 1, title: 'Task' }],
-    };
-
-    const nextState = reducer(previousState, clearAllTask());
-
-    expect(previousState).toEqual(nextState);
   });
 });
