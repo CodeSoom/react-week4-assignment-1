59f776b82e2c577f3d497286d0dea731ff8ad5c0
psy082 <zhappy114@gmail.com>
2021-09-04 19:26:44 +0900

    refactor: reducer action 처리 로직 switch에서 if문으로 수정

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,30 +6,29 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   const { newId, taskTitle, tasks } = state;
-  switch (action.type) {
-  case 'updateTaskTitle':
+  if (action.type === 'updateTaskTitle') {
     return {
       ...state,
       taskTitle: action.payload.taskTitle,
     };
-  case 'addTask':
+  }
+  if (action.type === 'addTask') {
     if (!taskTitle) {
-      break;
+      return state;
     }
 
     return {
       ...state,
       newId: newId + 1,
       taskTitle: '',
-      tasks: [...state.tasks, { id: newId, title: taskTitle }],
+      tasks: [...tasks, { id: newId, title: taskTitle }],
     };
-  case 'deleteTask':
+  }
+  if (action.type === 'deleteTask') {
     return {
       ...state,
       tasks: tasks.filter((task) => task.id !== action.payload.id),
     };
-  default:
-    break;
   }
 
   return state;
