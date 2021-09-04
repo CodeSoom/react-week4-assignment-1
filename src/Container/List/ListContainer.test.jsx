9d244cc07684555b0afdb8063a7147d0c04ea7a0
psy082 <zhappy114@gmail.com>
2021-09-04 22:37:50 +0900

    ListContainer test 수정
    - 테스트 명 수정
    - 완료 버튼을 눌렀을 때의 deleteTask action 호출 테스트 추가

--- src/Container/List/ListContainer.test.jsx
+++ src/Container/List/ListContainer.test.jsx
@@ -1,23 +1,51 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('ListContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    tasks: [
-      { id: 1, title: '아무것도 하지 않기 #1' },
-      { id: 2, title: '아무것도 하지 않기 #2' },
-    ],
-  }));
+describe('ListContainer', () => {
+  it('renders tasks of redux state', () => {
+    useSelector.mockImplementation((selector) => selector({
+      tasks: [
+        { id: 1, title: '아무것도 하지 않기 #1' },
+        { id: 2, title: '아무것도 하지 않기 #2' },
+      ],
+    }));
 
-  const { getByText } = render((
-    <ListContainer />
-  ));
+    const { getByText } = render((
+      <ListContainer />
+    ));
 
-  expect(getByText(/아무것도 하지 않기 #1/)).not.toBeNull();
-  expect(getByText(/아무것도 하지 않기 #2/)).not.toBeNull();
+    expect(getByText(/아무것도 하지 않기 #1/)).not.toBeNull();
+    expect(getByText(/아무것도 하지 않기 #2/)).not.toBeNull();
+  });
+
+  it('calls deleteTask action when complete button is clicked', () => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      tasks: [
+        { id: 1, title: '아무것도 하지 않기 #1' },
+        { id: 2, title: '아무것도 하지 않기 #2' },
+      ],
+    }));
+
+    const { getAllByText } = render((
+      <ListContainer />
+    ));
+
+    const buttons = getAllByText('완료');
+
+    fireEvent.click(buttons[0]);
+
+    expect(dispatch).toBeCalledWith({
+      type: 'deleteTask',
+      payload: { id: 1 },
+    });
+  });
 });
