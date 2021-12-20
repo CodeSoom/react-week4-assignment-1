71330eb02945d09f6c8f06e761a2edd9d1c17602
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-21 08:41:55 +0900

    [test] addTask 리듀서 테스트 구현 - taskTitle 값이 있는 경우 새로운 task 추가 케이스

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,5 @@
 import reducer from './reducer';
-import { updateTaskTitle } from './action';
+import { updateTaskTitle, addTask } from './action';
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
@@ -9,6 +9,19 @@ describe('reducer', () => {
       }, updateTaskTitle('New Task'));
 
       expect(state.taskTitle).toBe('New Task');
+    });
+  });
+
+  describe('addTask', () => {
+    context('with taskTitle', () => {
+      it('append new task into tasks', () => {
+        const state = reducer({
+          taskTitle: 'New Task',
+          tasks: [],
+        }, addTask());
+
+        expect(state.tasks).toHaveLength(1);
+      });
     });
   });
 });
