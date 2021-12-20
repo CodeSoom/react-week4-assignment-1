a3e6e87ee2029731bd50145674fed5a67b485f92
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-21 08:45:20 +0900

    [feat] addTask 리듀서 기능 구혀

--- src/action.js
+++ src/action.js
@@ -5,4 +5,8 @@ export const updateTaskTitle = (taskTitle) => ({
   },
 });
 
+export const addTask = () => ({
+  type: 'addTask',
+});
+
 export default {};
--- src/reducer.js
+++ src/reducer.js
@@ -12,5 +12,16 @@ export default function reducer(state = initialState, action) {
     };
   }
 
+  if (action.type === 'addTask') {
+    const { newId, taskTitle, tasks } = state;
+
+    return {
+      ...state,
+      newId: newId + 1,
+      taskTitle: '',
+      tasks: [...tasks, { id: newId, title: taskTitle }],
+    };
+  }
+
   return state;
 }
