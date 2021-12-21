199bc4c0ec0aa2d1ef1d6a83aecb0edb0a2ec5f6
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-21 23:08:29 +0900

    [test] reducer 테스트 케이스 추가 - 유효하지 않는 타입 요청시 이전 상태 반환 테스트 추가 - 재 사용하는 기능 함수화

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,12 +1,21 @@
 import reducer from './reducer';
 import { updateTaskTitle, addTask, deleteTask } from './action';
 
+import { tasks as stubTasks } from '../fixtures/tasks';
+
 describe('reducer', () => {
+  const renderReducer = ({ newId = 100, taskTitle = '', tasks = [] }, actionCreator) => reducer({
+    newId,
+    taskTitle,
+    tasks,
+  }, actionCreator);
+
   describe('updateTaskTitle', () => {
     it('changes task title', () => {
-      const state = reducer({
-        taskTitle: '',
-      }, updateTaskTitle('New Task'));
+      const state = renderReducer(
+        { newId: 100, taskTitle: '', tasks: [] },
+        updateTaskTitle('New Task'),
+      );
 
       expect(state.taskTitle).toBe('New Task');
     });
@@ -15,22 +24,20 @@ describe('reducer', () => {
   describe('addTask', () => {
     context('with taskTitle', () => {
       it('append new task into tasks', () => {
-        const state = reducer({
-          newId: 1,
-          taskTitle: 'New Task',
-          tasks: [],
-        }, addTask());
+        const state = renderReducer(
+          { newId: 100, taskTitle: 'New Task', tasks: [] },
+          addTask(),
+        );
 
         expect(state.tasks).toHaveLength(1);
         expect(state.tasks[0].id).not.toBeUndefined();
       });
 
       it('clears taskTitle', () => {
-        const state = reducer({
-          newId: 1,
-          taskTitle: 'New Task',
-          tasks: [],
-        }, addTask());
+        const state = renderReducer(
+          { newId: 100, taskTitle: 'New Task', tasks: [] },
+          addTask(),
+        );
 
         expect(state.taskTitle).toBe('');
       });
@@ -38,11 +45,10 @@ describe('reducer', () => {
 
     context('without taskTitle', () => {
       it('not working', () => {
-        const state = reducer({
-          newId: 1,
-          taskTitle: '',
-          tasks: [],
-        }, addTask());
+        const state = renderReducer(
+          { newId: 100, taskTitle: '', tasks: [] },
+          addTask(),
+        );
 
         expect(state.tasks).toHaveLength(0);
       });
@@ -52,14 +58,10 @@ describe('reducer', () => {
   describe('deleteTask', () => {
     context('with valid task Id', () => {
       it('delete the task into Tasks', () => {
-        const state = reducer({
-          newId: 100,
-          taskTitle: '',
-          tasks: [
-            { id: 1, title: 'Task-1' },
-            { id: 2, title: 'Task-2' },
-          ],
-        }, deleteTask(1));
+        const state = renderReducer(
+          { newId: 100, taskTitle: '', tasks: stubTasks },
+          deleteTask(1),
+        );
 
         expect(state.tasks).toHaveLength(1);
       });
@@ -67,17 +69,26 @@ describe('reducer', () => {
 
     context('with invalid task Id', () => {
       it('not working', () => {
-        const state = reducer({
-          newId: 100,
-          taskTitle: '',
-          tasks: [
-            { id: 1, title: 'Task-1' },
-            { id: 2, title: 'Task-2' },
-          ],
-        }, deleteTask(200));
+        const state = renderReducer(
+          { newId: 100, taskTitle: '', tasks: stubTasks },
+          deleteTask(200),
+        );
 
         expect(state.tasks).toHaveLength(2);
       });
+    });
+  });
+
+  describe('request invalid action type', () => {
+    it('return previous state', () => {
+      const state = renderReducer(
+        { newId: 100, taskTitle: 'exist taskTitle', tasks: stubTasks },
+        { type: 'invalid type' },
+      );
+
+      expect(state.newId).toBe(100);
+      expect(state.taskTitle).toBe('exist taskTitle');
+      expect(state.tasks).toEqual(stubTasks);
     });
   });
 });
