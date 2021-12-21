d8fea3c1d0efba1d780bd97ab86fdfe8062e795d
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-21 22:52:26 +0900

    [test] test통과하도록 useSelector 가짜함수 이용 jest.mock('react-redux')로 react-router 모듈을 mock으로 만들었다. 그래서 하위 함수들을 모두 mock으로 만들었다.
    13번줄
    userSelector.mockImplemention()
    로 selector함수 받아서 tasks 데이터 받고 가짜 useSelector을 실행시킨다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,19 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const tasks = [
+    { id: 100, title: '숨 쉬기' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
   const { getByText } = render((
     <App />
   ));
