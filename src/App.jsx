495fcc0746b371dd7991da80f29fbbcce3a881e2
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-21 22:44:56 +0900

    [refactor] App 컴포넌트 구조 변경 - Page 컴포넌트 삭제 및 Input, List Container 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import { updateTaskTitle, addTask, deleteTask } from './action';
-
-import Page from './Page';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
-  const { taskTitle, tasks } = state;
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <>
+      <InputContainer />
+      <ListContainer />
+    </>
   );
 }
