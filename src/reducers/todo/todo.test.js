9e1cd3efc2dca5424a1151aeb1969a86792f256d
wallababa <ansrlansrl@naver.com>
2021-12-22 00:17:23 +0900

    todoReducer 테스트에 action creator 적용

--- src/reducers/todo/todo.test.js
+++ src/reducers/todo/todo.test.js
@@ -1,34 +1,34 @@
 import todoReducer, { initialState } from './index';
+import {
+  addTask,
+  completeTask,
+} from '../../actions/todo';
 
 describe('todoReducer', () => {
   it('기존 상태와 task를 추가하는 action을 전달하면 tasks에 task가 추가된 새로운 상태를 반환합니다.', () => {
     const title = 'foo';
-    const action = { type: 'ADD_TASK', payload: { title } };
 
-    const nextState = todoReducer(initialState, action);
+    const nextState = todoReducer(initialState, addTask(title));
 
     expect(nextState.tasks).toHaveLength(1);
     expect(nextState.tasks[0].title).toBe(title);
   });
 
   it('tasks에 task를 추가할 때 새로운 id를 함께 추가합니다.', () => {
-    const action1 = { type: 'ADD_TASK', payload: { title: 'foo1' } };
-    const action2 = { type: 'ADD_TASK', payload: { title: 'foo2' } };
     const idSet = new Set();
 
-    const state1 = todoReducer(initialState, action1);
-    const state2 = todoReducer(state1, action2);
+    const state1 = todoReducer(initialState, addTask('foo1'));
+    const state2 = todoReducer(state1, addTask('foo2'));
 
     state2.tasks.forEach((task) => idSet.add(task.id));
     expect(state2.tasks.length).toBe(idSet.size);
   });
 
   it('기존 상태와 task의 id를 갖는 삭제 action을 전달하면 tasks에서 해당 task를 제거한 새로운 상태를 반환합니다.', () => {
-    const action = { type: 'ADD_TASK', payload: { title: 'foo' } };
-    const prevState = todoReducer(initialState, action);
+    const prevState = todoReducer(initialState, addTask('foo'));
     const { id } = prevState.tasks[0];
 
-    const nextState = todoReducer(prevState, { type: 'COMPLETE_TASK', payload: { id } });
+    const nextState = todoReducer(prevState, completeTask(id));
 
     expect(nextState.tasks.find((task) => task.id === id)).toBeUndefined();
   });
