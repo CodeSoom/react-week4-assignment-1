cb27f38a9d808145ae88ed1cd7db4f442928a6b5
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-21 09:16:15 +0900

    [refactor] 기존 상태 관리 리듀서로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,24 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { updateTaskTitle, addTask, deleteTask } from './action';
 
 import Page from './Page';
 
 export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
-
-  const { newId, taskTitle, tasks } = state;
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { taskTitle, tasks } = state;
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+    dispatch(updateTaskTitle(event.target.value));
   }
 
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
+    dispatch(addTask());
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
+    dispatch(deleteTask(id));
   }
 
   return (
