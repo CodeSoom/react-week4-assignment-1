83b4814a93d736b6e0c839e215301b9a6f46651c
shongs27 <shongs27@naver.com>
2021-12-22 00:01:59 +0900

    Fix - App.test BDD로 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,17 +5,32 @@ import { useSelector } from "react-redux";
 jest.mock("react-redux");
 
 describe("App", () => {
-  it("Check if the useSelector works well", () => {
-    const tasks = [
-      { id: 100, title: "4주차 인강보기" },
-      { id: 101, title: "4주차 1과제하기" },
-    ];
-    useSelector.mockImplementation((selector) => selector({ tasks }));
-    const { getByText } = render(<App />);
+  context("useSelector works well", () => {
+    it("show me tasks in store", () => {
+      //given
+      const tasks = [
+        { id: 100, title: "4주차 인강보기" },
+        { id: 101, title: "4주차 1과제하기" },
+      ];
+      useSelector.mockImplementation((selector) => selector({ tasks }));
+      const { getByText } = render(<App />);
 
-    expect(getByText(/추가/)).not.toBeNull();
-    expect(getByText(/4주차 인강보기/)).not.toBeNull();
-    expect(getByText(/4주차 1과제하기/)).not.toBeNull();
+      //then
+      expect(getByText(/추가/)).not.toBeNull();
+      expect(getByText(/4주차 인강보기/)).not.toBeNull();
+      expect(getByText(/4주차 1과제하기/)).not.toBeNull();
+    });
+  });
+
+  context("useSelector has error", () => {
+    it("show me '할 일이 없어요", () => {
+      const tasks = [];
+      useSelector.mockImplementation((selector) => selector({ tasks }));
+      const { getByText } = render(<App />);
+
+      expect(getByText(/추가/)).not.toBeNull();
+      expect(getByText(/할 일이/)).not.toBeNull();
+    });
 
     // TODO: 통합 테스트 코드 작성
     // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
