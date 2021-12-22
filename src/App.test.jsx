ecee95c96871c3769311d11b744d8001ee4c133d
jnny1017 <jnny1017@gmail.com>
2021-12-22 22:31:32 +0900

    불필요한 Page컴포넌트를 제거하고 App컴포넌트에 추가하는 리팩터링

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,6 +8,7 @@ jest.mock('react-redux');
 
 test('App', () => {
   useSelector.mockImplementation((selector) => selector({
+    taskTitle: '',
     tasks: [
       { id: 1, title: '아무 것도 하지 않기 #1' },
       { id: 2, title: '아무 것도 하지 않기 #2' },
