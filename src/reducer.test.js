6095b587e7b4e59e1cfc6b7d567f539bffeec66a
emewjin <emewjin@gmail.com>
2021-12-22 20:26:08 +0900

    up: deleteTask 테스트 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,6 @@
 import reducer from './reducer';
 
-import { changeTaskTitle, addTask } from './actions';
+import { changeTaskTitle, addTask, deleteTask } from './actions';
 
 describe('reducer', () => {
   describe('changeTaskTitle', () => {
@@ -47,6 +47,40 @@ describe('reducer', () => {
         const { tasks } = handleAddTask('');
         expect(tasks.length).toBe(0);
       });
+    });
+  });
+
+  describe('deleteTask', () => {
+    function handleDeleteTask(id) {
+      const prevState = {
+        newId: 100,
+        taskTitle: '',
+        tasks: [
+          { id: 1, title: '삭제할 할 일 1' },
+          { id: 2, title: '삭제할 할 일 2' },
+        ],
+      };
+      return reducer(prevState, deleteTask(id));
+    }
+
+    it('특정 id의 할 일이 삭제된다', () => {
+      const { tasks } = handleDeleteTask(1);
+
+      expect(tasks.length).toBe(1);
+      expect(tasks[0].id).not.toBe(1);
+      expect(tasks[0].title).not.toBe('삭제할 할 일 1');
+    });
+
+    it('id가 없으면 삭제되지 않는다', () => {
+      const { tasks } = handleDeleteTask();
+
+      expect(tasks.length).toBe(2);
+    });
+
+    it('할 일 목록에 존재하는 id가 아니면 삭제되지 않는다', () => {
+      const { tasks } = handleDeleteTask(3);
+
+      expect(tasks.length).toBe(2);
     });
   });
 });
