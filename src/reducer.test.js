f6bb8144de589e8f8aa74aa241377678085c7598
CheolWoong Choi <bluesky4381@naver.com>
2021-12-23 01:57:04 +0900

    reducer 테스트 코드 작성
    - updateTitle 액션 테스트 수정
    - addTask, deleteTask 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -20,10 +20,55 @@ describe('reducer', () => {
   });
 
   describe('addTask', () => {
+    it('add a task into tasks', () => {
+      const title = 'newTitle';
 
+      const state = reducer({
+        taskTitle: title,
+        tasks: [],
+      }, addTask());
+
+      expect(state.tasks[0].title).toBe(title);
+      expect(state.tasks).toHaveLength(1);
+    });
+
+    it('taskTitle should be empty', () => {
+      const title = 'newTitle';
+
+      const state = reducer({
+        taskTitle: title,
+        tasks: [],
+      }, addTask());
+
+      expect(state.taskTitle).toBe('');
+    });
   });
 
   describe('deleteTask', () => {
+    it("if task is deleted, tasks's length is decreased by 1", () => {
+      const state = reducer({
+        taskTitle: '',
+        tasks: [
+          { id: 1, title: 'title-1' },
+          { id: 2, title: 'title-2' },
+        ],
+      }, deleteTask(1));
 
+      expect(state.tasks).toHaveLength(1);
+      expect(state.tasks[0].title).not.toBe('title-1');
+    });
+
+    it('if wrong id is passed, doing nothing', () => {
+      const state = reducer({
+        taskTitle: '',
+        tasks: [
+          { id: 1, title: 'title-1' },
+          { id: 2, title: 'title-2' },
+        ],
+      }, deleteTask(100));
+
+      expect(state.tasks).toHaveLength(2);
+      expect(state.tasks[0].title).toBe('title-1');
+    });
   });
 });
