2dc0572c603466204b5a6e3634560a31c356ac8b
emewjin <emewjin@gmail.com>
2021-12-22 20:39:35 +0900

    up: InputContainer 분리 및 그에 따른 Page test 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux';
-
-import { changeTaskTitle, addTask, deleteTask } from './actions';
-
 import Page from './Page';
 
 export default function App() {
-  const { tasks, taskTitle } = useSelector((state) => ({
-    tasks: state.tasks,
-    taskTitle: state.taskTitle,
-  }));
-
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(changeTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-    />
+    <Page />
   );
 }
--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,4 +1,4 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
@@ -15,21 +15,10 @@ test('Page', () => {
     tasks,
   }));
 
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTask = jest.fn();
-
   const { getByText } = render((
-    <Page
-      taskTitle=""
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <Page />
   ));
 
   expect(getByText(/Task-1/)).not.toBeNull();
   expect(getByText(/Task-2/)).not.toBeNull();
-
-  fireEvent.click(getByText('추가'));
-
-  expect(handleClickAddTask).toBeCalled();
 });
