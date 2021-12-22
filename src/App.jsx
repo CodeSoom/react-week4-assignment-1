fe60def242de2bd816ec8ffebed749a9809d45f5
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-22 14:38:39 +0900

    [refactor] Page사용하지 않고 렌더한다. InputConatainer, ListContainer로 렌더한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { addTask, deleteTask, updateTaskTitle } from './action';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
-import Page from './Page';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  };
-}
 export default function App() {
-  // const [state, setState] = useState(initialState);
-  const { taskTitle, tasks } = useSelector(selector);
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <>
+      <InputContainer />
+      <ListContainer />
+    </>
   );
 }
