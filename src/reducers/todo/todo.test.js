0f0bf9854a39c2cc5db6313813c1449812996505
wallababa <ansrlansrl@naver.com>
2021-12-22 22:06:28 +0900

    todoReducer 테스트 리팩토링

--- src/reducers/todo/todo.test.js
+++ src/reducers/todo/todo.test.js
@@ -5,31 +5,35 @@ import {
 } from '../../actions/todo';
 
 describe('todoReducer', () => {
-  it('기존 상태와 task를 추가하는 action을 전달하면 tasks에 task가 추가된 새로운 상태를 반환합니다.', () => {
-    const title = 'foo';
+  describe('addTask action', () => {
+    it('addTask에 title을 전달하면 tasks에 task가 추가된 새로운 상태를 반환합니다.', () => {
+      const title = 'foo';
 
-    const nextState = todoReducer(initialState, addTask(title));
+      const nextState = todoReducer(initialState, addTask(title));
 
-    expect(nextState.tasks).toHaveLength(1);
-    expect(nextState.tasks[0].title).toBe(title);
+      expect(nextState.tasks).toHaveLength(1);
+      expect(nextState.tasks[0].title).toBe(title);
+    });
+
+    it('tasks에 task를 추가할 때 새로운 id를 함께 추가합니다.', () => {
+      const idSet = new Set();
+
+      const state1 = todoReducer(initialState, addTask('foo1'));
+      const state2 = todoReducer(state1, addTask('foo2'));
+
+      state2.tasks.forEach((task) => idSet.add(task.id));
+      expect(state2.tasks.length).toBe(idSet.size);
+    });
   });
 
-  it('tasks에 task를 추가할 때 새로운 id를 함께 추가합니다.', () => {
-    const idSet = new Set();
+  describe('completeTask action', () => {
+    it('completeTask action에 기존 상태와 task의 id를 전달하면 tasks에서 해당 task를 제거한 새로운 상태를 반환합니다.', () => {
+      const prevState = todoReducer(initialState, addTask('foo'));
+      const { id } = prevState.tasks[0];
 
-    const state1 = todoReducer(initialState, addTask('foo1'));
-    const state2 = todoReducer(state1, addTask('foo2'));
+      const nextState = todoReducer(prevState, completeTask(id));
 
-    state2.tasks.forEach((task) => idSet.add(task.id));
-    expect(state2.tasks.length).toBe(idSet.size);
-  });
-
-  it('기존 상태와 task의 id를 갖는 삭제 action을 전달하면 tasks에서 해당 task를 제거한 새로운 상태를 반환합니다.', () => {
-    const prevState = todoReducer(initialState, addTask('foo'));
-    const { id } = prevState.tasks[0];
-
-    const nextState = todoReducer(prevState, completeTask(id));
-
-    expect(nextState.tasks.find((task) => task.id === id)).toBeUndefined();
+      expect(nextState.tasks.find((task) => task.id === id)).toBeUndefined();
+    });
   });
 });
