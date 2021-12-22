94668260b4c1c369d14d0960061c4376fc799877
emewjin <emewjin@gmail.com>
2021-12-22 19:53:25 +0900

    up: 깨진 App test 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,20 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const tasks = [
+    { id: 1, title: 'haha' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
   const { getByText } = render((
     <App />
   ));
