337c68a14e84a146f636deff4cbfefc4be22a32f
jnny1017 <jnny1017@gmail.com>
2021-12-22 22:23:12 +0900

    InputContainer를 생성하고 관심사를 분리하는 리팩터링

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
-
 import Page from './Page';
 
-import { updateTaskTitle, addTask } from './actions';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  };
-}
-
 export default function App() {
-  const { taskTitle } = useSelector(selector);
-
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <Page />
   );
 }
