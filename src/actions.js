25307802d1e17ac57a1bf0e564bf617b678cb910
jeongseop.lee <sjl0614@naver.com>
2021-12-22 20:44:20 +0900

    :sparkles: addTask 테스트 작성

--- src/actions.js
+++ src/actions.js
@@ -7,4 +7,4 @@ export const updateTaskTitle = (taskTitle) => ((
   }
 ));
 
-export default {};
+export const addTask = () => ({});
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,6 +2,7 @@ import reducer from './reducer';
 
 import {
   updateTaskTitle,
+  addTask,
 } from './actions';
 
 describe('reducer', () => {
@@ -10,6 +11,18 @@ describe('reducer', () => {
       const state = reducer({ taskTitle: '' }, updateTaskTitle('New task'));
 
       expect(state.taskTitle).toBe('New task');
+    });
+  });
+
+  describe('addTask', () => {
+    it('renders new task', () => {
+      const state = reducer({
+        newId: 100,
+        taskTitle: 'New task',
+        tasks: [],
+      }, addTask());
+
+      expect(state.tasks).toHaveLength(1);
     });
   });
 });
