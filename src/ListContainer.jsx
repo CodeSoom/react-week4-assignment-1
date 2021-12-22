dc7a472b7fe95bd5f81ff16ab9576cd3aa7c117b
jeongseop.lee <sjl0614@naver.com>
2021-12-22 21:46:11 +0900

    :white_check_mark: ListContainer.test 통과

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,5 +1,29 @@
-const ListContainer = () => {
+import { useDispatch, useSelector } from 'react-redux';
 
+import List from './List';
+
+import {
+  deleteTask,
+} from './actions';
+
+const ListContainer = () => {
+  const dispatch = useDispatch();
+
+  const { tasks } = useSelector((state) => ({
+    taskTitle: state.taskTitle,
+    tasks: state.tasks,
+  }));
+
+  const handleClickDeleteTask = (id) => (
+    dispatch(deleteTask(id))
+  );
+
+  return (
+    <List
+      tasks={tasks}
+      onClickDeleteTask={handleClickDeleteTask}
+    />
+  );
 };
 
 export default ListContainer;
