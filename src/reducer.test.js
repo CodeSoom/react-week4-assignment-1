0156795f594fe07955fc84d2d607359fb1a997fb
emewjin <emewjin@gmail.com>
2021-12-22 20:18:27 +0900

    up: 할 일을 입력하지 않았을 때 테스트 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -30,15 +30,22 @@ describe('reducer', () => {
 
     context('할 일 이름을 입력했을 경우', () => {
       it('새로운 할 일이 추가된다', () => {
-        const state = handleAddTask('새로운');
-        expect(state.tasks.length).toBe(1);
-        expect(state.tasks[0].title).toBe('새로운');
-        expect(state.tasks[0].id).toBe(100);
+        const { tasks } = handleAddTask('새로운');
+        expect(tasks.length).toBe(1);
+        expect(tasks[0].title).toBe('새로운');
+        expect(tasks[0].id).toBe(100);
       });
 
       it('할 일 입력창이 초기화 된다', () => {
-        const state = handleAddTask('새로운');
-        expect(state.taskTitle).toBe('');
+        const { taskTitle } = handleAddTask('새로운');
+        expect(taskTitle).toBe('');
+      });
+    });
+
+    context('할 일 이름을 입력하지 않은 경우', () => {
+      it('할 일이 추가되지 않는다', () => {
+        const { tasks } = handleAddTask('');
+        expect(tasks.length).toBe(0);
       });
     });
   });
