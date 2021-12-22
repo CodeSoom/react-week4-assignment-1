e6f7dcfa8897643411ff6ab8088e2ae79628aaee
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-12-23 03:02:41 +0900

    reducer을 store.js에서 분리하기

---
+++ src/reducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === 'updateTaskTitle') {
+    return ({
+      ...state,
+      taskTitle: action.payload.taskTitle,
+    });
+  }
+
+  if (action.type === 'addTask') {
+    return ({
+      ...state,
+      newId: state.newId + 1,
+      taskTitle: '',
+      tasks: [...state.tasks, { id: state.newId, title: state.taskTitle }],
+    });
+  }
+
+  if (action.type === 'deleteTask') {
+    return ({
+      ...state,
+      tasks: state.tasks.filter((task) => task.id !== action.payload.id),
+    });
+  }
+
+  return state;
+}
