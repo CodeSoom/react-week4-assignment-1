d37b8e22a6286e3e442ddf0ecb9708d345da790c
Superduper-India <sunyoung1542@gmail.com>
2021-12-22 15:12:48 +0900

    Update: reducer test

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,7 +1,7 @@
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
     it('renders new task title', () => {
-      const previousState = {
+      const state = {
         taskTitle: '',
       };
 
@@ -9,10 +9,44 @@ describe('reducer', () => {
         type: 'updateTaskTitle',
         payload: {
           taskTitle: '새로 할 일',
-        }
+        },
       };
 
-      expect(state.taskTitle).toBe('새로 할 일');
+      expect(state.taskTitle).toBe(action.payload.taskTitle);
+    });
+  });
+
+  describe('addTask', () => {
+    it('renders new task', () => {
+      const state = {
+        tasks: [],
+      };
+
+      const action = {
+        type: 'addTask',
+        payload: {
+          tasks: [{ id: 1, title: '새로 할 일' }],
+        },
+      };
+
+      expect(state.tasks).toBe(action.payload.tasks);
+    });
+  });
+
+  describe('deleteTask', () => {
+    it('renders new task', () => {
+      const state = {
+        tasks: [{ id: 1, title: '새로 할 일' }],
+      };
+
+      const action = {
+        type: 'addTask',
+        payload: {
+          tasks: [],
+        },
+      };
+
+      expect(state.tasks).toBe(action.payload.tasks);
     });
   });
 });
