8ea32fa99f3e3a28968ab404b9857d37b478800e
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-22 17:12:06 +0900

    [refactor] 중복제거를 위한 함수 이용
    다음 함수들을 구현
    reduceUpdateTaskTitle
    reduceAddTask
    reduceDeleteTask

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,69 +1,52 @@
-// updateTaskTitle
-// addTask
-// deleteTask
-// 가 작동하는가?
-// 특히
-// addTask는 TaskTitle===''일 떄 작동하지 않는다.
-// deleteTask는 wrongId일떄 작동하지 않는다.
-
 import { addTask, deleteTask, updateTaskTitle } from './action';
 import reducer from './reducer';
 
 describe('reducer', () => {
-  it('updateTaskTitle', () => {
-    const state = {
-      newId: 100,
-      taskTitle: '',
-      tasks: [],
-    };
-    const newState = reducer(state, updateTaskTitle('new TaskTitle'));
+  describe('updateTaskTitle', () => {
+    function reduceUpdateTaskTitle(taskTitle) {
+      const state = {
+        newId: 100,
+        taskTitle: '',
+        tasks: [],
+      };
 
+      return reducer(state, updateTaskTitle(taskTitle));
+    }
+
+    const newState = reduceUpdateTaskTitle('new TaskTitle');
     expect(newState.taskTitle).toBe('new TaskTitle');
   });
-});
-describe('addTask', () => {
-  context('taskTtle이 있을 때', () => {
-    it('작동 한다.', () => {
-      const state = {
-        newId: 100,
-        taskTitle: 'new TaskTitle',
-        tasks: [],
-      };
-      const newState = reducer(state, addTask());
-      expect(newState.tasks).toHaveLength(1);
-    });
-  });
-  context('taskTitle이 없을 때', () => {
-    it('작동 안한다.', () => {
-      const state = {
-        newId: 100,
-        taskTitle: '',
-        tasks: [],
-      };
-      const newState = reducer(state, addTask());
-      expect(newState.tasks).toHaveLength(0);
-    });
-  });
-});
 
-describe('deleteTask', () => {
-  context('wrongId일때', () => {
-    it('작동안한다.', () => {
+  describe('addTask', () => {
+    function reduceAddTask(taskTitle) {
       const state = {
         newId: 100,
-        taskTitle: '',
-        tasks: [
-          {
-            id: 100,
-            title: '독서',
-          }],
+        taskTitle,
+        tasks: [],
       };
-      const newState = reducer(state, deleteTask(200));
-      expect(newState.tasks).toHaveLength(1);
+
+      return reducer(state, addTask());
+    }
+
+    context('taskTtle이 있을 때', () => {
+      it('작동 한다.', () => {
+        const newState = reduceAddTask('new TaskTitle');
+
+        expect(newState.tasks).toHaveLength(1);
+      });
+    });
+
+    context('taskTitle이 없을 때', () => {
+      it('작동 안한다.', () => {
+        const newState = reduceAddTask('');
+
+        expect(newState.tasks).toHaveLength(0);
+      });
     });
   });
-  context('rigthId일때', () => {
-    it('작동한다.', () => {
+
+  describe('deleteTask', () => {
+    function reduceDeleteTask(id) {
       const state = {
         newId: 100,
         taskTitle: '',
@@ -73,8 +56,24 @@ describe('deleteTask', () => {
             title: '독서',
           }],
       };
-      const newState = reducer(state, deleteTask(100));
-      expect(newState.tasks).toHaveLength(0);
+
+      return reducer(state, deleteTask(id));
+    }
+
+    context('wrongId일때', () => {
+      it('작동안한다.', () => {
+        const newState = reduceDeleteTask(200);
+
+        expect(newState.tasks).toHaveLength(1);
+      });
+    });
+
+    context('rigthId일때', () => {
+      it('작동한다.', () => {
+        const newState = reduceDeleteTask(100);
+
+        expect(newState.tasks).toHaveLength(0);
+      });
     });
   });
 });
