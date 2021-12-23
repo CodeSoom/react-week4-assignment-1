320f8314964db6b36c90f39aa10ac95a95f0bcea
CheolWoong Choi <bluesky4381@naver.com>
2021-12-23 22:38:19 +0900

    ListContainer 작성 및 테스트 코드 추가

--- src/containers/ListContainer.test.jsx
+++ src/containers/ListContainer.test.jsx
@@ -1,3 +1,24 @@
+import { render } from '@testing-library/react';
+
+import ListContainer from './ListContainer';
+
 describe('ListContainer', () => {
-  it('test2', () => {});
+  const handleClickDeleteTask = jest.fn();
+
+  it('with tasks, titles should be rendered', () => {
+    const tasks = [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ];
+
+    const { getByText } = render((
+      <ListContainer
+        tasks={tasks}
+        onClickDeleteTask={handleClickDeleteTask}
+      />
+    ));
+
+    expect(getByText(/Task-1/)).not.toBeNull();
+    expect(getByText(/Task-2/)).not.toBeNull();
+  });
 });
