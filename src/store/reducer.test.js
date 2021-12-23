1045979d7436c94d90d5d241201743ec1eea3dcf
Ravi <ravi@inflearn.com>
2021-12-23 22:24:22 +0900

    리뷰 반영 reducer test에 action 함수 사용 describe에 action 정보 추가

--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -1,4 +1,4 @@
-import { ACTION_TYPES } from './actions';
+import { addTask, changeTitle, deleteTask } from './actions';
 import reducer, { initialState } from './reducer';
 
 describe('reducer', () => {
@@ -6,37 +6,32 @@ describe('reducer', () => {
     expect(reducer(undefined, {})).toEqual(initialState);
   });
 
-  it('should handle CHANGE_TITLE', () => {
-    const newTaskTitle = 'new task title';
-    expect(
-      reducer(undefined, {
-        type: ACTION_TYPES.CHANGE_TITLE,
-        payload: { taskTitle: newTaskTitle },
-      }).taskTitle,
-    ).toBe(newTaskTitle);
+  describe('changeTitle action', () => {
+    it('should handle CHANGE_TITLE', () => {
+      const newTaskTitle = 'new task title';
+      expect(reducer(undefined, changeTitle(newTaskTitle)).taskTitle).toBe(
+        newTaskTitle,
+      );
+    });
   });
 
-  it('should handle CHANGE_DESCRIPTION', () => {
-    const newTaskTitle = 'new task title';
-    const newId = 100;
+  describe('addTask action', () => {
+    it('should handle ADD_TASK', () => {
+      const newTaskTitle = 'new task title';
+      const newId = 100;
 
-    expect(
-      reducer(
-        { taskTitle: newTaskTitle, tasks: [], newId },
-        {
-          type: ACTION_TYPES.ADD_TASK,
-        },
-      ).tasks,
-    ).toEqual([{ id: newId, title: newTaskTitle }]);
+      expect(
+        reducer({ taskTitle: newTaskTitle, tasks: [], newId }, addTask()).tasks,
+      ).toEqual([{ id: newId, title: newTaskTitle }]);
+    });
   });
 
-  it('should handle DELETE_TASK', () => {
-    const taskId = 1;
-    expect(
-      reducer({ tasks: [{ id: taskId, title: 'task title' }] }, {
-        type: ACTION_TYPES.DELETE_TASK,
-        payload: { id: taskId },
-      }).tasks,
-    ).toEqual([]);
+  describe('deleteTask action', () => {
+    it('should handle DELETE_TASK', () => {
+      const taskId = 1;
+      expect(
+        reducer({ tasks: [{ id: taskId, title: 'task title' }] }, deleteTask(taskId)).tasks,
+      ).toEqual([]);
+    });
   });
 });
