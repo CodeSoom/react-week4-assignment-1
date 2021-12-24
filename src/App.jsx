3b93bd27bbba65f6ef254ecfba39caf89b1f088d
padawanR0k <padawanR0k@gmail.com>
2021-12-24 19:41:40 +0900

    up: 사용되지 않는 page 컴포넌트 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import Page from './Page';
-
-import { changeTitle, addTask, deleteTask } from './actions';
+import InputContainer from './InputContainer';
+import List from './List';
 
 export default function App() {
-  const { taskTitle, tasks } = useSelector((store) => store);
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(changeTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask(taskTitle));
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <List />
+    </div>
   );
 }
