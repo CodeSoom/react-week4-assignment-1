967bce3e545c28031052f2983bf0adf6f5fde376
padawanR0k <padawanR0k@gmail.com>
2021-12-24 20:40:05 +0900

    up: InputContainer 구현

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,7 +1,7 @@
 import { fireEvent, render } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
 import InputContainer from './InputContainer';
-import { addTask } from './actions';
+import { addTask, changeTitle } from './actions';
 
 jest.mock('react-redux');
 
@@ -10,14 +10,29 @@ describe('InputContainer', () => {
     const dispatch = jest.fn();
     useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
-      title: 'new Title',
+      taskTitle: 'new Title',
     }));
 
     const { getByText } = render(<InputContainer />);
-
-    expect(getByText(/추가/).not.toBeNull());
+    expect(getByText(/추가/)).not.toBeNull();
 
     fireEvent.click(getByText(/추가/));
     expect(dispatch).toBeCalledWith(addTask('new Title'));
+  });
+
+  it('할 일 작성시 taskTitle 변경', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      taskTitle: '',
+    }));
+
+    const { getByLabelText } = render(<InputContainer />);
+    fireEvent.change(getByLabelText('할 일'), {
+      target: {
+        value: 'new Title',
+      },
+    });
+    expect(dispatch).toBeCalledWith(changeTitle('new Title'));
   });
 });
