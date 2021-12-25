4c4b682284417ca36c1014780e07694f17aac11a
syaring <sarah.k.serim@gmail.com>
2021-12-25 15:04:50 +0900

    Add redux and create store for App

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,18 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux'); // __mocks__ 에 저장된 react-redux 를 가져옴
+
 test('App', () => {
+  const tasks = [];
+
+  useSelector.mockImplementation((selector) => ({
+    tasks,
+  }));
+
   const { getByText } = render((
     <App />
   ));
---
+++ src/actions.js
@@ -0,0 +1,24 @@
+// actionCreators
+export function updateTaskTitle(taskTitle) {
+  return {
+    type: 'updateTaskTitle',
+    payload: {
+      taskTitle,
+    },
+  };
+}
+
+export function addTask() {
+  return {
+    type: 'addTask',
+  };
+}
+
+export function deleteTask(id) {
+  return {
+    type: 'deleteTask',
+    payload: {
+      id,
+    },
+  };
+}
