cf7f63df9bfc4c2811e873e0ac9526c855e5431d
kjoobin <imkj1148@gmail.com>
2022-04-12 08:32:47 +0900

    Add delete task test-case in ListContainer

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,40 +1,74 @@
-import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { render, queryByRole, fireEvent } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
+const renderListContainer = () => render((
+  <ListContainer />
+));
+
+const dispatch = jest.fn();
+
+beforeEach(() => {
+  useSelector.mockImplementation(() => ({
+    tasks: [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ],
+  }));
+
+  useDispatch.mockImplementation(() => dispatch);
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('ListContainer', () => {
   context('with tasks', () => {
-    useSelector.mockImplementationOnce(() => ({
-      tasks: [
-        { id: 1, title: 'Task-1' },
-        { id: 2, title: 'Task-2' },
-      ],
-    }));
-
     it('renders tasks', () => {
-      const { getByText } = render((
-        <ListContainer />
-      ));
+      const { getByText } = renderListContainer();
 
       expect(getByText(/Task-1/)).not.toBeNull();
       expect(getByText(/Task-2/)).not.toBeNull();
     });
+
+    it('renders button each task', () => {
+      const { getByText } = renderListContainer();
+
+      expect(queryByRole(getByText(/Task-1/), 'button')).toHaveTextContent('완료');
+      expect(queryByRole(getByText(/Task-2/), 'button')).toHaveTextContent('완료');
+    });
   });
 
   context('without the task', () => {
-    useSelector.mockImplementationOnce(() => ({
-      tasks: [],
-    }));
+    beforeEach(() => {
+      useSelector.mockImplementationOnce(() => ({
+        tasks: [],
+      }));
+    });
 
     it('renders "할 일이 없어요!"', () => {
-      const { container } = render((
-        <ListContainer />
-      ));
+      const { container } = renderListContainer();
 
       expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
+
+  it('listens for click event on delete task', () => {
+    const { getByText } = renderListContainer();
+
+    expect(dispatch).not.toBeCalled();
+
+    fireEvent.click(queryByRole(getByText(/Task-1/), 'button'));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'deleteTask',
+      payload: {
+        id: 1,
+      },
     });
   });
 });
