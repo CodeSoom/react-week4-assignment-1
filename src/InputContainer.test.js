50a20f976f5b3c0c97814569179df7c33d0830fb
hdpyo <hudongpyo.dev@gmail.com>
2022-04-11 19:55:45 +0900

    test: InputContainer 에 대한 테스트 코드를 작성했다.

---
+++ src/InputContainer.test.js
@@ -0,0 +1,30 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import InputContainer from './InputContainer';
+
+jest.mock('react-redux');
+
+describe('InputContainer', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  it('shows task title ', () => {
+    useSelector.mockImplementation((selector) => selector({
+      taskTitle: 'New Title',
+    }));
+
+    const { getByText } = render((
+      <InputContainer />
+    ));
+
+    expect(getByText(/추가/)).not.toBeNull();
+
+    fireEvent.click(getByText(/추가/));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'addTask',
+    });
+  });
+});
