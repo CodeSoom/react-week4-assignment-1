8a3cf7136bc7d89df57277cf00b70abd0c5bf432
kjoobin <imkj1148@gmail.com>
2022-04-12 08:03:00 +0900

    Add input container add task action test-caser

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,10 +1,14 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputContainer from './InputContainer';
 
 jest.mock('react-redux');
+
+const renderInputContainer = () => render((
+  <InputContainer />
+));
 
 describe('InputContainer', () => {
   it('renders task title', () => {
@@ -12,10 +16,35 @@ describe('InputContainer', () => {
       taskTitle: 'New Task!',
     }));
 
-    const { getByDisplayValue } = render((
-      <InputContainer />
-    ));
+    const { queryByDisplayValue } = renderInputContainer();
 
-    expect(getByDisplayValue('New Task!')).not.toBeNull();
+    expect(queryByDisplayValue('New Task!')).not.toBeNull();
+  });
+
+  it('renders add task button', () => {
+    const { queryByRole } = renderInputContainer();
+
+    expect(queryByRole('button')).toHaveTextContent('추가');
+  });
+
+  it('renders placeholder', () => {
+    const { queryByPlaceholderText } = renderInputContainer();
+
+    expect(queryByPlaceholderText('할 일을 입력해 주세요')).not.toBeNull();
+  });
+
+  it('listens for click evnet on add task', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+
+    const { getByText } = renderInputContainer();
+
+    expect(dispatch).not.toBeCalled();
+
+    fireEvent.click(getByText('추가'));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'addTask',
+    });
   });
 });
