0829af164ad52146fee99d2ac49495fbc91064f5
hdpyo <hudongpyo.dev@gmail.com>
2022-04-11 17:44:21 +0900

    refactor: updateTaskTitle 테스트 코드를 리팩토링했다.
    - actions.js 로부터 updateTaskTitle 을 import 한다.
    - 기존에 작성한 actions 를 import 한 updateTaskTitle 로 대체한다.
    - 더 이상 사용하지 않는 const actions 를 제거한다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,7 @@
 import reducer from './reducer';
 
+import { updateTaskTitle } from './actions';
+
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
     it('returns new state with new task title ', () => {
@@ -7,14 +9,7 @@ describe('reducer', () => {
         taskTitle: '',
       };
 
-      const action = {
-        type: 'updateTaskTitle',
-        payload: {
-          taskTitle: 'New Title',
-        },
-      };
-
-      const state = reducer(previousState, action);
+      const state = reducer(previousState, updateTaskTitle('New Title'));
 
       expect(state.taskTitle).toBe('New Title');
     });
