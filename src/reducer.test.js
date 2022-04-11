9e5dda0a33becd7625820bb1d57a0e2e16b21e44
hdpyo <hudongpyo.dev@gmail.com>
2022-04-11 18:22:29 +0900

    refactor: addTask 테스트 코드 리팩토링
    - 공통으로 사용하는 코드를 reduceAddTask 라는 함수로 분리했다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -16,15 +16,19 @@ describe('reducer', () => {
   });
 
   describe('addTask', () => {
+    function reduceAddTask(taskTitle) {
+      const previousState = {
+        newId: 100,
+        taskTitle,
+        tasks: [],
+      };
+
+      return reducer(previousState, addTask());
+    }
+
     context('with task title', () => {
       it('appends a new task into tasks', () => {
-        const previousState = {
-          newId: 100,
-          taskTitle: 'New Task',
-          tasks: [],
-        };
-
-        const state = reducer(previousState, addTask());
+        const state = reduceAddTask('New Task');
 
         expect(state.tasks).toHaveLength(1);
         expect(state.tasks[0].id).not.toBeUndefined();
@@ -32,26 +36,14 @@ describe('reducer', () => {
       });
 
       it('clears task title after appends a task into tasks', () => {
-        const previousState = {
-          newId: 100,
-          taskTitle: 'New Task',
-          tasks: [],
-        };
-
-        const state = reducer(previousState, addTask());
+        const state = reduceAddTask('New Task');
 
         expect(state.taskTitle).toBe('');
       });
 
       context('without task title', () => {
         it("doesn't work", () => {
-          const previousState = {
-            newId: 100,
-            taskTitle: '',
-            tasks: [],
-          };
-
-          const state = reducer(previousState, addTask());
+          const state = reduceAddTask();
 
           expect(state.tasks).toHaveLength(0);
         });
