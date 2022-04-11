19ea74ae992240389c30d0c33724e87ee461e00b
hdpyo <hudongpyo.dev@gmail.com>
2022-04-11 18:57:48 +0900

    test: ListContainer 컴포넌트에 대한 테스트 코드를 작성했다.
    - 화면에 호출하여 '아무것도 하지 않기 #1'이 존재하는지 확인한다.

---
+++ src/ListContainer.test.js
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import ListContainer from './ListContainer';
+
+jest.mock('react-redux');
+
+describe('ListContainer', () => {
+  it('shows tasks ', () => {
+    const tasks = [
+      { id: 1, title: '아무 것도 하지 않기 #1' },
+      { id: 2, title: '아무 것도 하지 않기 #2' },
+    ];
+
+    useSelector.mockImplementation((selector) => selector({
+      tasks,
+    }));
+
+    const { getByText } = render((
+      <ListContainer />
+    ));
+
+    expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+  });
+});
