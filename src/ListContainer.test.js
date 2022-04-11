67dbd9ecb7510fe65d001843ef48ba71b386e3db
hdpyo <hudongpyo.dev@gmail.com>
2022-04-12 00:19:11 +0900

    test: ListContainer 컴포넌트 테스트 코드를 추가했다.
    - tasks 가 빈 배열일 때 '할 일이 없어요!' 라는 메시지가 보이는지 테스트한다.
    - 공통으로 사용하는 코드들을 분리한다.

--- src/ListContainer.test.js
+++ src/ListContainer.test.js
@@ -7,20 +7,44 @@ import ListContainer from './ListContainer';
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
-  it('shows tasks ', () => {
-    const tasks = [
-      { id: 1, title: '아무 것도 하지 않기 #1' },
-      { id: 2, title: '아무 것도 하지 않기 #2' },
-    ];
-
-    useSelector.mockImplementation((selector) => selector({
-      tasks,
-    }));
-
-    const { getByText } = render((
+  function renderListContainer() {
+    return render((
       <ListContainer />
     ));
+  }
 
-    expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+  function returnUseSelector(tasks) {
+    return (
+      useSelector.mockImplementation((selector) => selector({
+        tasks,
+      }))
+    );
+  }
+
+  context('with tasks', () => {
+    it('shows existing tasks ', () => {
+      const tasks = [
+        { id: 1, title: '아무 것도 하지 않기 #1' },
+        { id: 2, title: '아무 것도 하지 않기 #2' },
+      ];
+
+      returnUseSelector(tasks);
+
+      const { getByText } = renderListContainer();
+
+      expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+    });
+  });
+
+  context('without tasks', () => {
+    it("shows message: '할 일이 없어요!'", () => {
+      const emptyTask = [];
+
+      returnUseSelector(emptyTask);
+
+      const { getByText } = renderListContainer();
+
+      expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+    });
   });
 });
