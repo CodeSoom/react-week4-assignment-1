2719f8e35b0f90d6ab314097a1abd809d48d88a0
free-ko <duddnr654@naver.com>
2022-04-12 21:44:43 +0900

    Add InputContainer.jsx

---
+++ src/InputContainer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import Input from './Input';
+
+import { updateTaskTitle, addTask } from './actions';
+
+export default function InputContainer() {
+  const { taskTitle } = useSelector((state) => ({
+    taskTitle: state.taskTitle,
+  }));
+
+  const dispatch = useDispatch();
+
+  function handleChangeTitle(event) {
+    dispatch(updateTaskTitle(event.target.value));
+  }
+
+  function handleClickAddTask() {
+    dispatch(addTask());
+  }
+
+  return (
+    <div>
+      <h1>To-do</h1>
+      <Input
+        value={taskTitle}
+        onChangeTitle={handleChangeTitle}
+        onclick={handleClickAddTask}
+      />
+    </div>
+  );
+}
