eac16caf75645515ffd05e2a2d55c89781118e29
이찬호 <0407chan@naver.com>
2022-04-12 17:01:18 +0900

    reducer, action 함수 추가

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,35 @@
 import { createStore } from 'redux';
 
-function reducer(state, action) {
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [],
+};
+
+function reducer(state = initialState, action) {
   if (action.type === 'updateTaskTitle') {
     return {
       ...state,
       taskTitle: action.payload.taskTitle,
+    };
+  }
+
+  if (action.type === 'addTask') {
+    const { newId, taskTitle, tasks } = state;
+
+    return {
+      ...state,
+      newId: newId + 1,
+      taskTitle: '',
+      tasks: [...tasks, { id: newId, taskTitle }],
+    };
+  }
+
+  if (action.type === 'deleteTask') {
+    const { tasks } = state;
+    return {
+      ...state,
+      tasks: tasks.filter((task) => task.id !== action.payload.id),
     };
   }
 
