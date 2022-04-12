8e3347d0611886b9273ef0e1539c76a192d6884a
free-ko <duddnr654@naver.com>
2022-04-12 21:03:14 +0900

    Add reducer.test.js to addTask test code

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,6 @@
 import reducer from './reducer';
 
-import { updateTaskTitle } from './actions';
+import { updateTaskTitle, addTask } from './actions';
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
@@ -10,6 +10,40 @@ describe('reducer', () => {
       }, updateTaskTitle('New Title'));
 
       expect(state.taskTitle).toBe('New Title');
+    });
+  });
+
+  describe('addTask', () => {
+    function reduceAddTask(taskTitle) {
+      return reducer({
+        newId: 100,
+        taskTitle,
+        tasks: [],
+      }, addTask());
+    }
+
+    context('with task title', () => {
+      it('appends a new task into tasks', () => {
+        const state = reduceAddTask('New Task');
+
+        expect(state.tasks).toHaveLength(1);
+        expect(state.tasks[0].id).not.toBeUndefined();
+        expect(state.tasks[0].title).toBe('New Task');
+      });
+
+      it('clears task title', () => {
+        const state = reduceAddTask('New Task');
+
+        expect(state.taskTitle).toBe('');
+      });
+    });
+
+    context('without task title', () => {
+      it("doesn't work", () => {
+        const state = reduceAddTask('');
+
+        expect(state.tasks).toHaveLength(0);
+      });
     });
   });
 });
