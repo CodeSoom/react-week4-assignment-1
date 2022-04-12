007c57cac1bd61104287cd8b7adb8f26277c8cfb
이찬호 <0407chan@naver.com>
2022-04-12 19:30:47 +0900

    test. app test추가

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,7 +1,19 @@
 import { fireEvent, render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  const tasks = [
+    // { id: 1, title: '안녕하세요 반가워요' },
+  ];
+
+  useDispatch.mockReturnValue(jest.fn());
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
   it('1. App 초기 빈배열 텍스트 출력', () => {
     const { container } = render(<App />);
     expect(container).toHaveTextContent('할 일이 없어요!');
@@ -13,12 +25,11 @@ describe('App', () => {
     const input = getByPlaceholderText('할 일을 입력해 주세요');
     fireEvent.change(
       input,
-      { target: { value: '할일이 없어' } },
+      { target: { value: '새 이벤트 추가' } },
     );
     fireEvent.click(getByText('추가'));
-    expect(input.value).toBe('');
 
-    expect(container).toHaveTextContent('할일이 없어');
+    expect(container).toHaveTextContent('새 이벤트 추가');
     expect(container).toHaveTextContent('완료');
   });
 
