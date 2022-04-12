158605a4ca7e8cb01ca5a60af75fd073120fb1e4
free-ko <duddnr654@naver.com>
2022-04-12 21:08:54 +0900

    Add deleteTask test code in reducer.test.js

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,6 @@
 import reducer from './reducer';
 
-import { updateTaskTitle, addTask } from './actions';
+import { updateTaskTitle, addTask, deleteTask } from './actions';
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
@@ -43,6 +43,32 @@ describe('reducer', () => {
         const state = reduceAddTask('');
 
         expect(state.tasks).toHaveLength(0);
+      });
+    });
+  });
+
+  describe('deleteTask', () => {
+    context('with existed task ID', () => {
+      it('remove the task from tasks', () => {
+        const state = reducer({
+          tasks: [
+            { id: 1, title: 'Task' },
+          ],
+        }, deleteTask(1));
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+
+    context('without existed task ID', () => {
+      it("doesn't work", () => {
+        const state = reducer({
+          tasks: [
+            { id: 1, title: 'Task' },
+          ],
+        }, deleteTask(100));
+
+        expect(state.tasks).toHaveLength(1);
       });
     });
   });
