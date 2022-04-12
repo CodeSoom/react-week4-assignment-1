7032f2e0d5ceefadcb3835ede7ff8c405d0acbf5
이찬호 <0407chan@naver.com>
2022-04-12 23:07:56 +0900

    Page 컴포넌트 제거, App에 Container들 적용

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
-import Page from './Page';
+import InputContainer from '../containers/InputContainer';
+import ListContainer from '../containers/ListContainer';
 
 export default function App() {
   return (
-    <Page />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
