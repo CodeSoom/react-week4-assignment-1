1d43f98b7819738dafa8e28b0e701721180d144c
이찬호 <0407chan@naver.com>
2022-04-13 16:37:43 +0900

    훔치기. with action.type, without action.type으로 context분리

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -2,77 +2,79 @@ import { addTask, deleteTask, updateTaskTitle } from './actions';
 import reducer from './reducer';
 
 describe('reducer', () => {
-  describe('updateTaskTitle', () => {
-    it('returns new state with a new task title', () => {
-      const state = reducer({
-        taskTitle: '',
-      }, updateTaskTitle('New Title'));
-
-      expect(state.taskTitle).toBe('New Title');
-    });
-  });
-
-  describe('addTask', () => {
-    function reduceAddTask({ taskTitle }) {
-      return reducer({
-        newId: 100,
-        taskTitle,
-        tasks: [],
-      }, addTask());
-    }
-
-    context('with taskTitle', () => {
-      it('tasks에 새 task 추가', () => {
-        const state = reduceAddTask({ taskTitle: 'New Task' });
-
-        expect(state.tasks).toHaveLength(1);
-        expect(state.tasks[0].id).not.toBeUndefined();
-        expect(state.tasks[0].title).toBe('New Task');
-      });
-
-      it('새 task추가 후 taskTitle 클리어', () => {
-        const state = reduceAddTask({ taskTitle: 'New Task' });
-
-        expect(state.taskTitle).toBe('');
-      });
-    });
-
-    context('with taskTitle', () => {
-      it('아무일도 일어나지 않는다.', () => {
-        const state = reduceAddTask({ taskTitle: '' });
-
-        expect(state.tasks).toHaveLength(0);
-      });
-    });
-  });
-
-  describe('deleteTask', () => {
-    context('with exist task ID', () => {
-      it('tasks에서 task 삭제', () => {
+  context('with action.type', () => {
+    describe('updateTaskTitle', () => {
+      it('returns new state with a new task title', () => {
         const state = reducer({
-          tasks: [{
-            id: 1, title: 'Task',
-          }],
-        }, deleteTask({ id: 1 }));
+          taskTitle: '',
+        }, updateTaskTitle('New Title'));
 
-        expect(state.tasks).toHaveLength(0);
+        expect(state.taskTitle).toBe('New Title');
       });
     });
 
-    context('without exist task ID', () => {
-      it("doesn't work", () => {
-        const state = reducer({
-          tasks: [{
-            id: 1, title: 'Task',
-          }],
-        }, deleteTask({ id: 100 }));
+    describe('addTask', () => {
+      function reduceAddTask({ taskTitle }) {
+        return reducer({
+          newId: 100,
+          taskTitle,
+          tasks: [],
+        }, addTask());
+      }
 
-        expect(state.tasks).toHaveLength(1);
+      context('with taskTitle', () => {
+        it('tasks에 새 task 추가', () => {
+          const state = reduceAddTask({ taskTitle: 'New Task' });
+
+          expect(state.tasks).toHaveLength(1);
+          expect(state.tasks[0].id).not.toBeUndefined();
+          expect(state.tasks[0].title).toBe('New Task');
+        });
+
+        it('새 task추가 후 taskTitle 클리어', () => {
+          const state = reduceAddTask({ taskTitle: 'New Task' });
+
+          expect(state.taskTitle).toBe('');
+        });
+      });
+
+      context('with taskTitle', () => {
+        it('아무일도 일어나지 않는다.', () => {
+          const state = reduceAddTask({ taskTitle: '' });
+
+          expect(state.tasks).toHaveLength(0);
+        });
+      });
+    });
+
+    describe('deleteTask', () => {
+      context('with exist task ID', () => {
+        it('tasks에서 task 삭제', () => {
+          const state = reducer({
+            tasks: [{
+              id: 1, title: 'Task',
+            }],
+          }, deleteTask({ id: 1 }));
+
+          expect(state.tasks).toHaveLength(0);
+        });
+      });
+
+      context('without exist task ID', () => {
+        it("doesn't work", () => {
+          const state = reducer({
+            tasks: [{
+              id: 1, title: 'Task',
+            }],
+          }, deleteTask({ id: 100 }));
+
+          expect(state.tasks).toHaveLength(1);
+        });
       });
     });
   });
 
-  describe('other action', () => {
+  context('without action.type', () => {
     function otherFunction({ id }) {
       return {
         type: 'otherFunction',
@@ -82,7 +84,7 @@ describe('reducer', () => {
       };
     }
 
-    it('아무 reducer실행', () => {
+    it('존재하지 않는 reducer실행', () => {
       const state = reducer(undefined, otherFunction({ id: 1 }));
 
       expect(state.tasks).toHaveLength(0);
