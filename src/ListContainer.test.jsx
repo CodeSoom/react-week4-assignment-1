5c80ebf9690144da947ac648d2ca4fc2af2ddadf
HJUN <guswns823@gmail.com>
2022-04-14 01:16:52 +0900

    ListContainer 테스트 개선 중

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
@@ -6,17 +6,51 @@ import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('ListContainer', () => {
+describe('ListContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+  });
+
+  function renderList() {
+    return render((
+      <ListContainer />
+    ));
+  }
+
   useSelector.mockImplementation((selector) => selector({
     tasks: [
-      { id: 1, title: '아무 것도 하지 않기 #1' },
-      { id: 2, title: '아무 것도 하지 않기 #2' },
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
     ],
   }));
 
-  const { getByText } = render((
-    <ListContainer />
-  ));
+  describe('with tasks', () => {
+    it('renders "완료" button to delete a task', () => {
+      const { getByText, getAllByText } = renderList();
 
-  expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+      expect(getByText(/Task-1/)).not.toBeNull();
+      expect(getByText(/Task-2/)).not.toBeNull();
+
+      const buttons = getAllByText('완료');
+
+      fireEvent.click(buttons[0]);
+
+      expect(dispatch).toBeCalledWith({
+        type: 'deleteTask',
+        payload: {
+          id: 1,
+        },
+      });
+    });
+  });
+
+  describe('without tasks', () => {
+    it('renders no task message', () => {
+      const { getByText } = renderList();
+
+      expect(getByText(/Task-1/)).not.toBeNull();
+    });
+  });
 });
