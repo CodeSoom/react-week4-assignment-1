f247d07d3468062d2d9488a40c46b91a55abe9e1
free-ko <duddnr654@naver.com>
2022-04-13 20:44:10 +0900

    Add renders tasks in ListContainer.test.jsx

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
-
 import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('ListContainer', () => {
+const dispatch = jest.fn();
+
+beforeEach(() => {
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     tasks: [
       { id: 1, title: '아무 것도 하지 않기 #1' },
       { id: 2, title: '아무 것도 하지 않기 #2' },
     ],
   }));
+});
 
-  const { getByText } = render(<ListContainer />);
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
-  expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+const renderListContainer = () => render((
+  <ListContainer />
+));
+
+describe('ListContainer', () => {
+  context('with tasks', () => {
+    it('renders tasks', () => {
+      const { queryByText } = renderListContainer();
+
+      expect(queryByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+      expect(queryByText(/아무 것도 하지 않기 #2/)).not.toBeNull();
+    });
+  });
 });
