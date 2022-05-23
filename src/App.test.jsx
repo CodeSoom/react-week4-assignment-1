3f2dbd3024d42d7160df9487b210d243a54071ca
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-05-24 00:16:39 +0900

    Test: redux mock 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,15 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
 import App from './App';
 
+// TODO: jest.mock에 대해서 공부해보자
+jest.mock('react-redux');
+
 test('App', () => {
+  const tasks = [{ id: 1, title: '아무 것도 하지 않기 #1' }, { id: 2, title: '아무 것도 하지 않기 #2' }];
+
+  useSelector.mockImplementation((selector) => selector({ tasks }));
   const { getByText } = render((
     <App />
   ));
