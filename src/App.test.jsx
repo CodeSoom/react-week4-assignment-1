3f16fe736ed265ae1dfa8863a5092fd1610aeb7a
Ga-Young Song <dolphin2440@naver.com>
2022-05-25 00:50:21 +0900

    Add container component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,21 +7,19 @@ import App from './App';
 jest.mock('react-redux');
 
 test('App', () => {
-  const tasks = [
-    { id: 1, title: '아무것도 하지 않기 #1' },
-    { id: 2, title: '아무것도 하지 않기 #2' },
-  ];
-
   useSelector.mockImplementation((selector) => selector({
-    tasks,
+    taskTitle: '',
+    tasks: [
+      { id: 1, title: 'Task-1' },
+      { id: 2, title: 'Task-2' },
+    ],
   }));
 
   const { getByText } = render((
     <App />
   ));
 
+  expect(getByText(/Task-1/)).not.toBeNull();
+  expect(getByText(/Task-2/)).not.toBeNull();
   expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
