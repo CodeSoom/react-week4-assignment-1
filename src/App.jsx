eb52f85b7143c8fa14ed552e8f5ef4b8aba34689
rukeni <rukeni@naver.com>
2022-05-24 13:45:23 +0900

    Refactor: InputContainer로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
-
 import Page from './Page';
 
-import { updateTaskTitle, addTask } from './actions';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  };
-}
-
 export default function App() {
-  const dispatch = useDispatch();
-
-  const { taskTitle } = useSelector(selector);
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <Page />
   );
 }
