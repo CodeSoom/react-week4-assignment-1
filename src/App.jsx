1793f968604a2f4b4fa64867e755d18bc0e19f7a
rukeni <rukeni@naver.com>
2022-05-24 13:23:25 +0900

    Refactor: 데이터처리 ListContainer로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Page from './Page';
 
-import { updateTaskTitle, addTask, deleteTask } from './actions';
+import { updateTaskTitle, addTask } from './actions';
 
 function selector(state) {
   return {
@@ -14,7 +14,7 @@ function selector(state) {
 export default function App() {
   const dispatch = useDispatch();
 
-  const { taskTitle, tasks } = useSelector(selector);
+  const { taskTitle } = useSelector(selector);
 
   function handleChangeTitle(event) {
     dispatch(updateTaskTitle(event.target.value));
@@ -24,17 +24,11 @@ export default function App() {
     dispatch(addTask());
   }
 
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
     <Page
       taskTitle={taskTitle}
       onChangeTitle={handleChangeTitle}
       onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
     />
   );
 }
