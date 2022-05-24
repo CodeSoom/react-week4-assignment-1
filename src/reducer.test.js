fa3c1d53135823be1026cc9df54c55110b6e6515
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-24 13:09:46 +0900

    addTask test code

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,15 +1,31 @@
 import reducer from './reducer';
 
-import {
-  updateTaskTitle,
-} from './actions';
+import { updateTaskTitle, addTask } from './actions';
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
-    it('returns new state with new task  title', () => {
+    it('returns new state with new task title', () => {
       const state = reducer({ taskTitle: '' }, updateTaskTitle('New Title'));
 
       expect(state.taskTitle).toBe('New Title');
     });
   });
 });
+
+describe('addTask', () => {
+  describe('updateTaskTitle', () => {
+    it('returns new state with a new task', () => {
+      const state = reducer({
+        taskTitle: 'New Task',
+        tasks: [],
+      },
+      addTask());
+
+      expect(state.tasks).toHaveLength(1);
+      expect(state.tasks[0].title).toBe('New Task');
+    });
+  });
+});
+
+// reducer 안에 있는 함수들을 호출을 해오려고 import를 해준다.
+//
