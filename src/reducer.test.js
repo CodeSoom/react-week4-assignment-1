9d9f3f6fb93891d640bc2df8d31782e7fe8575d8
jaeyooniverse <jaeyoon4819@naver.com>
2022-05-24 15:47:44 +0900

    리듀서 테스트 코드 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,70 +2,94 @@ import { updateTaskTitle, addTaskTitle, deleteTaskTitle } from './actions';
 import reducer from './reducer';
 
 describe('reducer', () => {
-  describe('updateTaskTitle', () => {
-    it('changes new task title', () => {
-      const state = reducer({ taskTitle: '' }, updateTaskTitle('New Title'));
+  context('with action type', () => {
+    describe('updateTaskTitle', () => {
+      it('changes new task title', () => {
+        const state = reducer({ taskTitle: '' }, updateTaskTitle('New Title'));
 
-      expect(state.taskTitle).toBe('New Title');
+        expect(state.taskTitle).toBe('New Title');
+      });
+    });
+
+    describe('addTaskTitle', () => {
+      function reduceAddTaskTitle(taskTitle) {
+        return reducer(
+          {
+            newId: 100,
+            taskTitle,
+            tasks: [],
+          },
+          addTaskTitle()
+        );
+      }
+
+      context('with task title', () => {
+        it('appends a new task into tasks', () => {
+          const state = reduceAddTaskTitle('New Task');
+
+          expect(state.tasks).toHaveLength(1);
+          expect(state.tasks[0].title).toBe('New Task');
+        });
+
+        it('clears task title', () => {
+          const state = reduceAddTaskTitle('New Task');
+
+          expect(state.taskTitle).toBe('');
+        });
+      });
+
+      context('without task title', () => {
+        it("doesn't work", () => {
+          const state = reduceAddTaskTitle('');
+
+          expect(state.tasks).toHaveLength(0);
+        });
+      });
+    });
+
+    describe('deleteTaskTitle', () => {
+      context('with existed task ID', () => {
+        it('remove the task from tasks', () => {
+          const state = reducer(
+            { tasks: [{ id: 1, title: 'Task' }] },
+            deleteTaskTitle(1)
+          );
+
+          expect(state.tasks).toHaveLength(0);
+        });
+      });
+
+      context('without existed task ID', () => {
+        it("doesn't work", () => {
+          const state = reducer(
+            { tasks: [{ id: 1, title: 'Task' }] },
+            deleteTaskTitle(100)
+          );
+
+          expect(state.tasks).toHaveLength(1);
+        });
+      });
     });
   });
 
-  describe('addTaskTitle', () => {
-    function reduceAddTaskTitle(taskTitle) {
-      return reducer(
-        {
-          newId: 100,
-          taskTitle,
-          tasks: [],
-        },
-        addTaskTitle(),
+  context('without action type', () => {
+    const anonymousFunction = jest.fn(({ id, taskTitle }) => ({
+      type: 'anonymousFunction',
+      payload: {
+        id,
+        taskTitle,
+      },
+    }));
+
+    it("doesn't work", () => {
+      const state = reducer(
+        undefined,
+        anonymousFunction({ id: 101, taskTitle: 'New Title' })
       );
-    }
-    context('with task title', () => {
-      it('appends a new task into tasks', () => {
-        const state = reduceAddTaskTitle('New Task');
 
-        expect(state.tasks).toHaveLength(1);
-        expect(state.tasks[0].title).toBe('New Task');
-      });
-
-      it('clears task title', () => {
-        const state = reduceAddTaskTitle('New Task');
-
-        expect(state.taskTitle).toBe('');
-      });
-    });
-
-    context('without task title', () => {
-      it("doesn't work", () => {
-        const state = reduceAddTaskTitle('');
-
-        expect(state.tasks).toHaveLength(0);
-      });
-    });
-  });
-
-  describe('deleteTaskTitle', () => {
-    context('with existed task ID', () => {
-      it('remove the task from tasks', () => {
-        const state = reducer(
-          { tasks: [{ id: 1, title: 'Task' }] },
-          deleteTaskTitle(1),
-        );
-
-        expect(state.tasks).toHaveLength(0);
-      });
-    });
-
-    context('without existed task ID', () => {
-      it("doesn't work", () => {
-        const state = reducer(
-          { tasks: [{ id: 1, title: 'Task' }] },
-          deleteTaskTitle(100),
-        );
-
-        expect(state.tasks).toHaveLength(1);
-      });
+      expect(state.newId).toBe(100);
+      expect(state.taskTitle).toBe('');
+      expect(state.tasks).toHaveLength(0);
     });
   });
 });
