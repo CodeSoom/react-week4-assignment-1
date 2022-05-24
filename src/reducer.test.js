3ad6a1710272a4c1c91ef6b5253f0d8e66655bad
rukeni <rukeni@naver.com>
2022-05-24 12:56:20 +0900

    Test: deleteTask 테스트 코드 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,5 @@
 import { reducer } from './reducer';
-import { updateTaskTitle, addTask } from './actions';
+import { updateTaskTitle, addTask, deleteTask } from './actions';
 
 describe('reducer', () => {
   describe('updateTaskTitle', () => {
@@ -41,6 +41,30 @@ describe('reducer', () => {
         const state = reduceAddTask('');
 
         expect(state.tasks).toHaveLength(0);
+      });
+    });
+  });
+  describe('deleteTask', () => {
+    context('with existed task ID', () => {
+      it('remove the task from tasks', () => {
+        const state = reducer({
+          tasks: [
+            { id: 1, title: 'Task' },
+          ],
+        }, deleteTask(1));
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+    context('without existed task ID', () => {
+      it("doesn't work", () => {
+        const state = reducer({
+          tasks: [
+            { id: 1, title: 'Task' },
+          ],
+        }, deleteTask(100));
+
+        expect(state.tasks).toHaveLength(1);
       });
     });
   });
