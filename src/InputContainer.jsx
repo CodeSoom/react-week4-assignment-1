bdde897912d17626f63b2b16b74a8b1ddc2047ed
rukeni <rukeni@naver.com>
2022-05-24 13:40:05 +0900

    Add: InputContainer 추가

---
+++ src/InputContainer.jsx
@@ -0,0 +1,11 @@
+import Input from './Input';
+
+export default function InputContainer({ taskTitle, onChangeTitle, onClickAddTask }) {
+  return (
+    <Input
+      value={taskTitle}
+      onChange={onChangeTitle}
+      onClick={onClickAddTask}
+    />
+  );
+}
