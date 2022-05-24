d87a1752f304506fb830e16b352893cd85c4a50d
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-24 10:47:43 +0900

    updateTaskTitle TestCode

---
+++ src/reducer.test.js
@@ -0,0 +1,15 @@
+import reducer from './reducer';
+
+import {
+  updateTaskTitle,
+} from './actions';
+
+describe('reducer', () => {
+  describe('updateTaskTitle', () => {
+    it('returns new state with new task  title', () => {
+      const state = reducer({ taskTitle: '' }, updateTaskTitle('New Title'));
+
+      expect(state.taskTitle).toBe('New Title');
+    });
+  });
+});
