34b20ff3b65d0bc06f00e0f64faa9e7298559353
Ga-Young Song <dolphin2440@naver.com>
2022-05-24 23:57:15 +0900

    Add reducer test case

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Page from './Page';
 
-export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
+import {
+  addTask,
+  deleteTask,
+  updateTaskTitle,
+} from './actions';
 
-  const { newId, taskTitle, tasks } = state;
+function selector(state) {
+  return {
+    taskTitle: state.taskTitle,
+    tasks: state.tasks,
+  };
+}
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  const { taskTitle, tasks } = useSelector(selector);
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+    dispatch(updateTaskTitle(event.target.value));
   }
 
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
+    dispatch(addTask());
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
+    dispatch(deleteTask(id));
   }
 
   return (
