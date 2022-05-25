bcb9c40a0d5f06f1248c76bb5b410f1c0b470a42
jodheeee <soso99700@gmail.com>
2022-05-25 11:16:22 +0900

    영상 강의 1강 내용

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,20 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
+  useSelector.mockImplementation((selector) => selector({
+    tasks: [
+      { id: 1, title: '아무 것도 하지 않기 #1' },
+      { id: 2, title: '아무 것도 하지 않기 #2' },
+    ],
+  }));
+
+  const { getByText } = render(<App />);
 
   expect(getByText(/추가/)).not.toBeNull();
 
