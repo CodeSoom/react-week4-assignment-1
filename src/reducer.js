b997cbcee71d010e3d9368aaf3c66ff722e36fac
Ga-Young Song <dolphin2440@naver.com>
2022-05-25 20:28:21 +0900

    Change if clause to look up table

--- src/reducer.js
+++ src/reducer.js
@@ -4,36 +4,26 @@ const initialState = {
   tasks: [],
 };
 
-export default function reducer(state = initialState, action) {
-  if (action.type === 'updateTaskTitle') {
+const actions = {
+  updateTaskTitle: (state, action) => ({ ...state, taskTitle: action.payload.taskTitle }),
+  addTask: (state) => {
+    if (!state.taskTitle) return state;
+
     return {
       ...state,
-      taskTitle: action.payload.taskTitle,
-    };
-  }
-
-  if (action.type === 'addTask') {
-    const { newId, taskTitle, tasks } = state;
-
-    if (!taskTitle) {
-      return state;
-    }
-    return {
-      ...state,
-      newId: newId + 1,
+      newId: state.newId + 1,
       taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
+      tasks: [...state.tasks, { id: state.newId, title: state.taskTitle }],
     };
-  }
+  },
+  deleteTask: (state, action) => ({
+    ...state,
+    tasks: state.tasks.filter((task) => task.id !== action.payload.id),
+  }),
+};
 
-  if (action.type === 'deleteTask') {
-    const { tasks } = state;
+const defaultAction = (state) => state;
 
-    return {
-      ...state,
-      tasks: tasks.filter((task) => task.id !== action.payload.id),
-    };
-  }
-
-  return state;
+export default function reducer(state = initialState, action) {
+  return (actions[action.type] || defaultAction)(state, action);
 }
