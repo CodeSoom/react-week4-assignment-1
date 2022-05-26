5428a82abba82a36c6b9daded5bc0a759ec878aa
Mika <solinepik@gmail.com>
2022-05-27 02:46:38 +0900

    refactor: App 컴포넌트 통합 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,7 +7,7 @@ import App from './App';
 
 jest.mock('react-redux');
 
-test('App', () => {
+describe('App', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
@@ -19,15 +19,56 @@ test('App', () => {
     ],
   }));
 
-  const { getByText } = render((
-    <App />
-  ));
+  test('앱이 실행되면  화면에 추가 버튼과 코드숨 과제하기1 task가 있다', () => {
+    const { getByText } = render((
+      <App />
+    ));
 
-  expect(getByText(/추가/)).not.toBeNull();
+    expect(getByText(/추가/)).not.toBeNull();
 
-  expect(getByText(/코드숨 과제하기1/)).not.toBeNull();
+    expect(getByText(/코드숨 과제하기1/)).not.toBeNull();
+  });
 
-  fireEvent.click(getByText('추가'));
+  test('input에 값을 입력하면 updateTaskTitle action이 호출된다', () => {
+    const { getByPlaceholderText } = render((
+      <App />
+    ));
+    const input = getByPlaceholderText(/할 일을 입력해 주세요/);
 
-  expect(dispatch).toBeCalled();
+    fireEvent.change(input, { target: { value: '잠자기' } });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'updateTaskTitle',
+      payload: {
+        taskTitle: '잠자기',
+      },
+    });
+  });
+
+  test('추가 버튼을 누르면 addTask action이 호출된다', () => {
+    const { getByText } = render((
+      <App />
+    ));
+
+    fireEvent.click(getByText(/추가/));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'addTask',
+    });
+  });
+
+  test('완료 버튼을 누르면 deleteTask action이 호출된다', () => {
+    const { getAllByText } = render((
+      <App />
+    ));
+
+    fireEvent.click(getAllByText('완료')[0]);
+
+    expect(dispatch).toBeCalledWith({
+      type: 'deleteTask',
+      payload: {
+        id: 1,
+      },
+    });
+  });
 });
