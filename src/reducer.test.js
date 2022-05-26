55d95571b853ec035bcb2bea17b222fe2c36abd2
Mika <solinepik@gmail.com>
2022-05-27 01:45:01 +0900

    refactor: reducer test에 addTask state 중복코드 추출

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -14,25 +14,23 @@ describe('reducer', () => {
   });
 
   describe('addTask', () => {
+    const reduceAddTask = (taskTitle) => reducer({
+      newId: 2,
+      taskTitle,
+      tasks: [],
+    }, addTask());
+
     context('taskTitle의 값이 있으면', () => {
       it('state의 tasks 값이 추가된다.', () => {
-        const state = reducer({
-          newId: 2,
-          taskTitle: '할 일2',
-          tasks: [{ id: 1, title: '할 일1' }],
-        }, addTask());
+        const state = reduceAddTask('할 일2');
 
-        expect(state.tasks).toHaveLength(2);
-        expect(state.tasks[1].id).not.toBeUndefined();
-        expect(state.tasks[1].title).toBe('할 일2');
+        expect(state.tasks).toHaveLength(1);
+        expect(state.tasks[0].id).not.toBeUndefined();
+        expect(state.tasks[0].title).toBe('할 일2');
       });
 
       it('taskTitle의 값이 빈값이 된다.', () => {
-        const state = reducer({
-          newId: 2,
-          taskTitle: '할 일2',
-          tasks: [],
-        }, addTask());
+        const state = reduceAddTask('할 일2');
 
         expect(state.taskTitle).toBe('');
       });
@@ -40,11 +38,7 @@ describe('reducer', () => {
 
     context('taskTitle의 값이 없으면', () => {
       it('task 값이 추가되지 않는다.', () => {
-        const state = reducer({
-          newId: 1,
-          taskTitle: '',
-          tasks: [],
-        }, addTask());
+        const state = reduceAddTask('');
 
         expect(state.tasks).toHaveLength(0);
       });
