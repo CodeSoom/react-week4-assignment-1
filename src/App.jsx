6d1d1be23a68f0112801aa8987fabe792ac85968
Mika <solinepik@gmail.com>
2022-05-27 02:31:50 +0900

    refactor: InputContainer 적용 및 Post 컴포넌트 제거
    App test 코드 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import { updateTaskTitle, addTask } from './actions';
-
-import Page from './Page';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const { taskTitle } = useSelector((state) => ({
-    taskTitle: state.taskTitle,
-  }));
-
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
+import { useDispatch } from '../__mocks__/react-redux';
 
 import App from './App';
 
 jest.mock('react-redux');
 
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     tasks: [
       { id: 1, title: '코드숨 과제하기1' },
@@ -20,6 +25,9 @@ test('App', () => {
 
   expect(getByText(/추가/)).not.toBeNull();
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  expect(getByText(/코드숨 과제하기1/)).not.toBeNull();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(dispatch).toBeCalled();
 });
