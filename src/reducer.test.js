f77207eda27149e480ae3da3b9325cecccd131c2
2scent <ad7549@gmail.com>
2022-06-27 06:44:31 +0900

    Add addTask action tests
    - tasks에 newId를 가진 새로운 task가 추가되는지 테스트
    - taskTitle이 빈 문자열이 되는지 테스트
    - newId이 newId + 1로 업데이트 되는지 테스트

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,9 @@
 import reducer from './reducer';
 
 describe('reducer', () => {
+  const newId = 101;
+  const taskTitle = '새로운 할 일';
+
   describe('updateTaskTitle', () => {
     it('changes task title', () => {
       const previousState = {
@@ -10,13 +13,48 @@ describe('reducer', () => {
       const action = {
         type: 'updateTaskTitle',
         payload: {
-          taskTitle: '새로운 할 일',
+          taskTitle,
         },
       };
 
       const state = reducer(previousState, action);
 
-      expect(state.taskTitle).toBe('새로운 할 일');
+      expect(state.taskTitle).toBe(taskTitle);
+    });
+  });
+
+  describe('addTask', () => {
+    const previousState = {
+      newId,
+      taskTitle,
+      tasks: [],
+    };
+
+    const action = {
+      type: 'addTask',
+      payload: {
+        taskTitle,
+      },
+    };
+
+    it('appends a new task with new id into tasks', () => {
+      const state = reducer(previousState, action);
+
+      expect(state.tasks).toHaveLength(1);
+      expect(state.tasks[0].id).toBe(newId);
+      expect(state.tasks[0].title).toBe(taskTitle);
+    });
+
+    it('adds 1 to newId', () => {
+      const state = reducer(previousState, action);
+
+      expect(state.newId).toBe(newId + 1);
+    });
+
+    it('makes taskTitle blank', () => {
+      const state = reducer(previousState, action);
+
+      expect(state.taskTitle).toBe('');
     });
   });
 });
