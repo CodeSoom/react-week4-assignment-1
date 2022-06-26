0d734b0fc89bbd3ccbfcdba9718a02cddf1799a5
2scent <ad7549@gmail.com>
2022-06-27 06:06:21 +0900

    Implement updateTaskTitle action

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,12 @@
-const reducer = () => ({});
+const reducer = (state, action) => {
+  if (action.type === 'updateTaskTitle') {
+    return {
+      ...state,
+      taskTitle: action.payload.taskTitle,
+    };
+  }
+
+  return state;
+};
 
 export default reducer;
