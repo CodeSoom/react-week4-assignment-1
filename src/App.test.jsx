a03f4b6c0150a57cba629aa594f24c9d524676b2
unknown <alstjr0183@naver.com>
2022-06-28 09:06:56 +0900

    feat: 과제 기본설정작업중 강의 과제 기본설정중입니다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,21 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  // TODO: useSelector 조작
+  const tasks = [
+    { id: 1, title: '아무 것도 하지않기 #1' },
+    { id: 2, title: '아무 것도 하지않기 #2' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
   const { getByText } = render((
     <App />
   ));
