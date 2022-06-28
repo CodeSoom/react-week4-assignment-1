9e2ae2d5e3e49baf8b92b84cb32361bd52ea9447
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-28 20:13:51 +0900

    App.jsx 테스트 코드를 mock function을 이용하여 테스트를 하도록 수정했습니다
    기존 Provider로만 감쌌던 App 테스트코드를  초기값을 useSelector mock function에 세팅해주는 방식으로 수정해주었습니다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,24 @@
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
+
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const tasks = [
+    { id: 1, title: '아무 것도 하지 않기 #1' },
+    { id: 2, title: '아무 것도 하지 않기 #2' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
   const { getByText } = render((
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   ));
 
   expect(getByText(/추가/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
