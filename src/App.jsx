78e1a559822d3e307362db6e33b90b366c5b6934
2scent <ad7549@gmail.com>
2022-06-28 20:54:40 +0900

    Refactor App to render containers directly
    App 컴포넌트에서 InputContainer, ListContainer를 직접 렌더하도록 수정
    필요 없어진 Page 컴포넌트 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import Page from './Page';
-
-import { addTask, updateTaskTitle } from './actions';
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  const taskTitle = useSelector((state) => state.taskTitle);
-
-  const handleChangeTitle = (event) => dispatch(updateTaskTitle(event.target.value));
-
-  const handleClickAddTask = () => dispatch(addTask());
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
