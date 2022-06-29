f8092c59ca4d08d542b86659b456d97425ccc8d8
minsuk <alstjr0183@naver.com>
2022-06-29 20:39:09 +0900

    fix: app test useSelector이용해 작성 app test에 useSelector 를 이용해 테스트를 했습니다

--- __mocks__/react-redux.js
+++ __mocks__/react-redux.js
@@ -1,5 +1,3 @@
 export const useDispatch = jest.fn();
 
-export const useSelector = jest.fn((selector) => selector({
-  tasks: [{ id: 1, title: '할일 1' }, { id: 1, title: '할일 2' }],
-}));
+export const useSelector = jest.fn((selector) => selector());
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,20 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
 jest.mock('react-redux');
 
 test('App', () => {
+  const tasks = [
+    { id: 1, title: '할일 1' }, { id: 2, title: '할일 2' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
   const { getByText } = render((
     <App />
   ));
