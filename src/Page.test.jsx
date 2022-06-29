c14873ccfbee143832ee43aa22bda9d9299f338e
minsuk <alstjr0183@naver.com>
2022-06-29 23:23:20 +0900

    fix: page test 오류 list를 분리해주며 나온 테스트 오류를 useseletor를 이용하여 수정해주었습니다

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,24 +1,30 @@
 import { render, fireEvent } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import Page from './Page';
+
+jest.mock('react-redux');
 
 test('Page', () => {
   const handleChangeTitle = jest.fn();
   const handleClickAddTask = jest.fn();
-  const handleClickDeleteTask = jest.fn();
 
   const tasks = [
     { id: 1, title: 'Task-1' },
     { id: 2, title: 'Task-2' },
   ];
 
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
   const { getByText } = render((
     <Page
       taskTitle=""
       onChangeTitle={handleChangeTitle}
       onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
+
     />
   ));
 
