8c34471eb981256bd06c05faac171dd659fb531d
2scent <ad7549@gmail.com>
2022-06-29 18:46:13 +0900

    Add App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,21 +1,78 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { addTask, deleteTask, updateTaskTitle } from './actions';
+
 import App from './App';
 
 jest.mock('react-redux');
 
-test('App', () => {
+describe('App', () => {
+  const taskTitle = '새로 할 일';
+
+  const tasks = [
+    { id: 1, title: '할 일 1' },
+    { id: 2, title: '할 일 2' },
+  ];
+
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((state) => state({
-    tasks: [],
+    taskTitle,
+    tasks,
   }));
 
-  const { getByText } = render((
-    <App />
-  ));
+  const renderApp = () => render(<App />);
 
-  expect(getByText(/추가/)).not.toBeNull();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  it('renders the title', () => {
+    const { container } = renderApp();
+
+    expect(container).toHaveTextContent('To-do');
+  });
+
+  it('renders the task title', () => {
+    const { getByLabelText } = renderApp();
+
+    expect(getByLabelText('할 일')).toHaveDisplayValue(taskTitle);
+  });
+
+  it('dispatches a updateTaskTitle action with input value when the input is changed', () => {
+    const { getByLabelText } = renderApp();
+
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '그냥 놀기' } });
+
+    expect(dispatch).toBeCalledWith(updateTaskTitle('그냥 놀기'));
+  });
+
+  it('dispatches a addTask action when the button is clicked', () => {
+    const { getByRole } = renderApp();
+
+    fireEvent.click(getByRole('button', { name: '추가' }));
+
+    expect(dispatch).toBeCalledWith(addTask(taskTitle));
+  });
+
+  it('renders titles of tasks', () => {
+    const { container } = renderApp();
+
+    tasks.map((task) => task.title).forEach((title) => {
+      expect(container).toHaveTextContent(title);
+    });
+  });
+
+  it('dispatches a deleteTask action with the task id when complete button is clicked', () => {
+    const { getAllByRole } = renderApp();
+    const completeButtons = getAllByRole('button', { name: '완료' });
+
+    fireEvent.click(completeButtons[1]);
+
+    expect(dispatch).toBeCalledWith(deleteTask(tasks[1].id));
+  });
 });
