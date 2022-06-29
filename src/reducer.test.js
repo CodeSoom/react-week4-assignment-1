e4777f3ed97bb3b4d14b270e171107c14b049756
kyh196201 <kyh196201@gmail.com>
2022-06-29 20:07:36 +0900

    UpdateTaskTitle action에 대한 테스트 추가

---
+++ src/reducer.test.js
@@ -0,0 +1,20 @@
+import reducer from './reducer';
+
+describe('reducer', () => {
+  describe('updateTaskTitle', () => {
+    it('taskTitle이 변경된다.', () => {
+      const action = {
+        type: 'updateTaskTitle',
+        payload: {
+          taskTitle: 'New Task',
+        },
+      };
+
+      const newState = reducer({
+        taskTitle: '',
+      }, action);
+
+      expect(newState.taskTitle).toBe('New Task');
+    });
+  });
+});
