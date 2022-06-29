47eb9857627b587bf3335beb967855d5cc4d14bc
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-29 20:32:24 +0900

    reducer 함수의 addTask type에 예외케이스 추가와 반환 문 수정
    type이 addTask일 경우 타이틀이 빈 값으로 반환되게끔 수정.
    taskTitle 입력값이 없을 경우 예외 케이스 추가

--- src/reducer.js
+++ src/reducer.js
@@ -11,15 +11,22 @@ export default function reducer(state = initialState, action) {
       taskTitle: action.payload.taskTitle,
     };
   }
+
   if (action.type === 'addTask') {
     const { newId, taskTitle, tasks } = state;
+
+    if (!taskTitle) {
+      return state;
+    }
+
     return {
       ...state,
       newId: newId + 1,
-      taskTitle,
+      taskTitle: '',
       tasks: [...tasks, { id: newId, title: taskTitle }],
     };
   }
+
   if (action.type === 'deleteTask') {
     return {
       ...state,
