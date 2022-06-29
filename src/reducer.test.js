3fefdbcdc6e846cb60b321b49a43759af2bfe68d
kyh196201 <kyh196201@gmail.com>
2022-06-29 20:23:34 +0900

    할 일을 추가하는 액션에 대한 테스트 작성
    - 입력 중인 할 일이 있을 경우에 대한 테스트만 추가했습니다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -17,4 +17,22 @@ describe('reducer', () => {
       expect(newState).toHaveProperty('taskTitle', 'New Task');
     });
   });
+
+  describe('addTask', () => {
+    context('현재 입력중인 할 일이 있으면', () => {
+      it('할 일이 추가된다.', () => {
+        const action = {
+          type: 'addTask',
+        };
+
+        const state = reducer({
+          newId: 100,
+          taskTitle: 'New Task',
+          tasks: [],
+        }, action);
+
+        expect(state.tasks).toHaveLength(1);
+      });
+    });
+  });
 });
