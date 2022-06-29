cbfbc11518a2b96483385002dc40e6cc75918da6
minsuk <alstjr0183@naver.com>
2022-06-30 00:33:46 +0900

    refactor: 통일감을 위한 list test 수정 영어로 되어있던 설명을 통일감있게 수정해주었습니다

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -28,20 +28,20 @@ describe('List', () => {
     ));
   }
 
-  context('with tasks', () => {
+  context('tasks가 있을경우', () => {
     const tasks = [
       { id: 1, title: 'Task-1' },
       { id: 2, title: 'Task-2' },
     ];
 
-    it('renders tasks', () => {
+    it('tasks-title 을 렌더링한다', () => {
       const { getByText } = renderList(tasks);
 
       expect(getByText(/Task-1/)).not.toBeNull();
       expect(getByText(/Task-2/)).not.toBeNull();
     });
 
-    it('renders “완료” button to delete a task', () => {
+    it('click 이벤트를 listen한다', () => {
       const { getAllByText } = renderList(tasks);
 
       const buttons = getAllByText('완료');
@@ -52,7 +52,7 @@ describe('List', () => {
     });
   });
 
-  context('without tasks', () => {
+  context('tasks가 없는경우', () => {
     it('renders no task message', () => {
       const tasks = [];
 
