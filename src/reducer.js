369af0692da0b7af00cd81a6d1c6fcc70431f4f4
seungwoo Kim <kyh196201@gmail.com>
2022-06-30 00:03:39 +0900

    할 일을 추가하는 액션 수정
    - taskTitle이 없을 경우 기존 state를 리턴하도록 수정했습니다.

--- src/reducer.js
+++ src/reducer.js
@@ -15,6 +15,10 @@ const reducer = (state = initialState, action) => {
   if (action.type === 'addTask') {
     const { newId, taskTitle, tasks } = state;
 
+    if (!taskTitle) {
+      return state;
+    }
+
     return {
       ...state,
       newId: newId + 1,
