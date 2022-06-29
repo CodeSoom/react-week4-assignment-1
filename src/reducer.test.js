80ab4c73b1cefb74cf6cd1298f25647447015a3f
minsuk <alstjr0183@naver.com>
2022-06-29 21:06:53 +0900

    feat: reducer addTask 테스트 케이스 추가 reducer test에 addTask가 작동하는 테스트 케이스를 추가했습니다

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,4 +1,4 @@
-import { updateTaskTitle } from './actions';
+import { addTask, updateTaskTitle } from './actions';
 
 import reducer from './reducer';
 
@@ -8,6 +8,15 @@ describe('reducer', () => {
       const state = reducer({ taskTitle: '' }, updateTaskTitle('새로운 할일이다!'));
 
       expect(state.taskTitle).toBe('새로운 할일이다!');
+    });
+  });
+
+  describe('tasks의 할일추가를 시도한다 ', () => {
+    it('tasks에 할일이 추가되었다', () => {
+      const state = reducer({ taskTitle: '할일을 추가해봅시다!', tasks: [] }, addTask());
+
+      expect(state.tasks).toHaveLength(1);
+      expect(state.tasks[0].title).toBe('할일을 추가해봅시다!');
     });
   });
 });
