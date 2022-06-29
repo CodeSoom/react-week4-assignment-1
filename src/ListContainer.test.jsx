3ab11c042e1dbbbdafb82d644c93b7c6bcfdc1de
unknown <alstjr0183@naver.com>
2022-06-30 08:57:49 +0900

    feat: listContainer test 케이스 추가 listContainer의 완료버튼 눌렀을때를 테스트 하는 코드를 추가했습니다

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,12 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteTask } from './actions';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   const tasks = [
     { id: 1, title: '할일 1' }, { id: 2, title: '할일 2' },
   ];
@@ -23,5 +28,15 @@ describe('ListContainer', () => {
     const { getByText } = renderListContainer();
 
     expect(getByText(/할일 1/)).not.toBeNull();
+  });
+
+  it('click 이벤트를 listen한다', () => {
+    const { getAllByText } = renderListContainer();
+
+    const buttons = getAllByText('완료');
+
+    fireEvent.click(buttons[0]);
+
+    expect(dispatch).toBeCalledWith(deleteTask(1));
   });
 });
