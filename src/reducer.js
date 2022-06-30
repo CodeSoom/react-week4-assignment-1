fe4df66e86ca13f84828079ad69a21be9173f8c6
minsuk <alstjr0183@naver.com>
2022-06-30 20:57:32 +0900

    fix: reducer if문 Object Literals 으로 변경 가독성이 안좋은 if문을 대신하여 object Literals를 사용했습니다

--- src/reducer.js
+++ src/reducer.js
@@ -4,37 +4,38 @@ const initialState = {
   tasks: [],
 };
 
-export default function reducer(state = initialState, action) {
-  if (action.type === 'updateTaskTitle') {
-    return {
-      ...state,
-      taskTitle: action.payload.taskTitle,
-    };
-  }
+const reducerObject = {
+  updateTaskTitle: (state, action) => ({
+    ...state,
+    taskTitle: action.payload.taskTitle,
+  }),
 
-  if (action.type === 'addTask') {
+  addTask: (state) => {
     const { newId, taskTitle, tasks } = state;
 
     if (!taskTitle) {
       return state;
     }
-
     return {
       ...state,
       newId: newId + 1,
       taskTitle: '',
       tasks: [...tasks, { id: newId, title: taskTitle }],
     };
-  }
+  },
 
-  if (action.type === 'deleteTask') {
+  deleteTask: (state, action) => {
     const { tasks } = state;
 
     return {
       ...state,
       tasks: tasks.filter((task) => task.id !== action.payload.id),
     };
-  }
+  },
+};
+
+export default function reducer(state = initialState, action) {
+  if (reducerObject[action.type]) { return reducerObject[action.type]?.(state, action); }
 
   return state;
 }
