ec85e87dd23037a66956ced8504a1a505346ddd9
kyh196201 <kyh196201@gmail.com>
2022-06-30 20:34:56 +0900

    App 컴포넌트 소스 리팩터링
    - App 컴포넌트 내부의 상태 관리 로직을 모두 제거했습니다.
    - Page 컴포넌트가 불필요하므로 Page 컴포넌트 내부의 컴포넌트를 App으로 옮겼습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,14 @@
-import { useSelector, useDispatch } from 'react-redux';
-
-import Page from './Page';
-
-import {
-  updateTaskTitle,
-  addTask,
-  deleteTask,
-} from './actions';
-
-const selector = (state) => ({
-  newId: state.newId,
-  taskTitle: state.taskTitle,
-  tasks: state.tasks,
-});
+import ListContainer from './ListContainer';
+import InputContainer from './InputContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  const { taskTitle, tasks } = useSelector(selector);
-
-  function handleChangeTitle(newTitle) {
-    dispatch(updateTaskTitle(newTitle));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <div>
+      <h1>To-do</h1>
+
+      <InputContainer />
+
+      <ListContainer />
+    </div>
   );
 }
