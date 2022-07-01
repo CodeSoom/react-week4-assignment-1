a53a900e851e64b30d0408c62c84358c2690d126
bruceoh-rtm <brucegwanhooh@rtm.ai>
2022-07-01 16:57:12 +0900

    Feat: adding listcontainer test

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,25 +1,63 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteTask } from './actions';
 
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 
-test('ListContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    tasks: [
-      { id: 1, title: '아무 것도 하지 않기 #1' },
-      { id: 2, title: '아무 것도 하지 않기 #2' },
-    ],
-  }));
+describe('ListContainer', () => {
+  context('with tasks', () => {
+    it('renders tasks', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [
+          { id: 1, title: '아무 것도 하지 않기 #1' },
+          { id: 2, title: '아무 것도 하지 않기 #2' },
+        ],
+      }));
 
-  const { getByText } = render((
-    <ListContainer />
-  ));
+      const { getByText } = render((
+        <ListContainer />
+      ));
 
-  expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+      expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();
+      expect(getByText(/아무 것도 하지 않기 #2/)).not.toBeNull();
+    });
+  });
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  context('without tasks', () => {
+    it('renders no task message', () => {
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [],
+      }));
+
+      const { getByText } = render((
+        <ListContainer />
+      ));
+
+      expect(getByText(/할 일이 없어요/)).not.toBeNull();
+    });
+  });
+
+  context('button click', () => {
+    it('delete task', () => {
+      const dispatch = jest.fn();
+
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        tasks: [
+          { id: 1, title: '아무 것도 하지 않기 #1' },
+        ],
+      }));
+
+      const { getByText } = render((
+        <ListContainer />
+      ));
+
+      fireEvent.click(getByText('완료'));
+
+      expect(dispatch).toBeCalledWith(deleteTask(1));
+    });
+  });
 });
