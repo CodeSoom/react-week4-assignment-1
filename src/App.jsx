b16304f406acd740c7ac08cbd621e5c153fec804
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-02 20:56:47 +0900

    App 컨테이너 분리
    InputContainer와 ListContainer로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import { addTask, deleteTask, updateTaskTitle } from './actions';
-
-import Page from './Page';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  };
-}
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const { taskTitle, tasks } = useSelector(selector);
-
-  const dispatch = useDispatch();
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={(event) => dispatch(updateTaskTitle(event.target.value))}
-      onClickAddTask={() => dispatch(addTask())}
-      tasks={tasks}
-      onClickDeleteTask={(id) => dispatch(deleteTask(id))}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
