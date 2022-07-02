063ccf834337cb973a2c7623c5e0fb95cb71ae6d
seungwoo Kim <kyh196201@gmail.com>
2022-07-02 17:17:59 +0900

    reducer 테스트 코드 리팩터링
    - 정의되어 있지 않은 액션을 테스트하는 코드를 describe에서 context로 변경했습니다.
    
    - state가 있을 경우에 대한 context를 추가했습니다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -15,70 +15,74 @@ describe('reducer', () => {
     });
   });
 
-  describe('정의되지 않은 액션', () => {
-    it('현재 상태를 그대로 반환한다.', () => {
-      const newState = reducer(initialState, {
-        type: undefined,
-      });
+  context('state가 있으면', () => {
+    context('정의되지 있지 않은 액션일 경우', () => {
+      it('현재 상태를 그대로 반환한다.', () => {
+        const newState = reducer(initialState, {
+          type: undefined,
+        });
 
-      expect(newState).toEqual(initialState);
-    });
-  });
-
-  describe('updateTaskTitle', () => {
-    it('taskTitle이 변경된다.', () => {
-      const newState = reducer({
-        taskTitle: '',
-      }, updateTaskTitle('New Task'));
-
-      expect(newState.taskTitle).toBe('New Task');
-    });
-  });
-
-  describe('addTask', () => {
-    const reduceAddTask = (taskTitle = '') => reducer({
-      newId: 100,
-      taskTitle,
-      tasks: [],
-    }, addTask());
-
-    context('현재 입력중인 할 일이 있으면', () => {
-      it('할 일이 추가된다.', () => {
-        const newState = reduceAddTask('New Task');
-
-        expect(newState.tasks).toHaveLength(1);
-        expect(newState.tasks[0].title).toBe('New Task');
-        expect(newState.tasks[0].id).toBe(100);
-      });
-
-      it('입력중인 할 일이 비워진다.', () => {
-        const newState = reduceAddTask('New Task');
-
-        expect(newState.taskTitle).toBe('');
+        expect(newState).toEqual(initialState);
       });
     });
 
-    context('현재 입력중인 할 일이 없으면', () => {
-      it('할 일이 추가되지 않는다.', () => {
-        const newState = reduceAddTask('');
+    context('정의되어 있는 액션일 경우', () => {
+      describe('updateTaskTitle', () => {
+        it('taskTitle이 변경된다.', () => {
+          const newState = reducer({
+            taskTitle: '',
+          }, updateTaskTitle('New Task'));
 
-        expect(newState.tasks).toHaveLength(0);
+          expect(newState.taskTitle).toBe('New Task');
+        });
       });
-    });
-  });
 
-  describe('deleteTask', () => {
-    it('할 일이 삭제된다.', () => {
-      const newState = reducer({
-        tasks: [
-          {
-            id: 1,
-            title: '아무것도 하지 않기',
-          },
-        ],
-      }, deleteTask(1));
+      describe('addTask', () => {
+        const reduceAddTask = (taskTitle = '') => reducer({
+          newId: 100,
+          taskTitle,
+          tasks: [],
+        }, addTask());
 
-      expect(newState.tasks).toHaveLength(0);
+        context('현재 입력중인 할 일이 있으면', () => {
+          it('할 일이 추가된다.', () => {
+            const newState = reduceAddTask('New Task');
+
+            expect(newState.tasks).toHaveLength(1);
+            expect(newState.tasks[0].title).toBe('New Task');
+            expect(newState.tasks[0].id).toBe(100);
+          });
+
+          it('입력중인 할 일이 비워진다.', () => {
+            const newState = reduceAddTask('New Task');
+
+            expect(newState.taskTitle).toBe('');
+          });
+        });
+
+        context('현재 입력중인 할 일이 없으면', () => {
+          it('할 일이 추가되지 않는다.', () => {
+            const newState = reduceAddTask('');
+
+            expect(newState.tasks).toHaveLength(0);
+          });
+        });
+      });
+
+      describe('deleteTask', () => {
+        it('할 일이 삭제된다.', () => {
+          const newState = reducer({
+            tasks: [
+              {
+                id: 1,
+                title: '아무것도 하지 않기',
+              },
+            ],
+          }, deleteTask(1));
+
+          expect(newState.tasks).toHaveLength(0);
+        });
+      });
     });
   });
 });
