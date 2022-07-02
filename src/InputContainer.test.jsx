cdf92f0d8fafbadfa3c1aee20e8cea56901e8309
seungwoo Kim <kyh196201@gmail.com>
2022-07-02 17:31:25 +0900

    InputContainer 컴포넌트 리팩터링
    - given2를 이용해서 중복되는 useSelector.mockImplementation을 제거했습니다.

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -2,6 +2,8 @@ import { render, fireEvent } from '@testing-library/react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
+import given from 'given2';
+
 import InputContainer from './InputContainer';
 
 import { addTask, updateTaskTitle } from './actions';
@@ -9,19 +11,21 @@ import { addTask, updateTaskTitle } from './actions';
 jest.mock('react-redux');
 
 describe('InputContainer', () => {
+  given('taskTitle', () => '');
+
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    taskTitle: given.taskTitle,
+  }));
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('Input이 보인다.', () => {
-    useSelector.mockImplementation((selector) => selector({
-      taskTitle: '',
-    }));
-
     const { getByLabelText, getByText } = render((
       <InputContainer />
     ));
@@ -32,10 +36,6 @@ describe('InputContainer', () => {
 
   describe('할 일 입력', () => {
     it('할 일이 입력된다.', () => {
-      useSelector.mockImplementation((selector) => selector({
-        taskTitle: '',
-      }));
-
       const { getByLabelText } = render((
         <InputContainer />
       ));
@@ -55,9 +55,7 @@ describe('InputContainer', () => {
   describe('추가 버튼 클릭', () => {
     context('입력 중인 할 일이 있을 경우', () => {
       it('할 일이 추가된다.', () => {
-        useSelector.mockImplementation((selector) => selector({
-          taskTitle: 'New Task',
-        }));
+        given('taskTitle', () => 'New Task');
 
         const { getByText } = render((
           <InputContainer />
