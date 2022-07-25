e74b79d6677fe41ea7d1c6eea271df942cd59879
Heejae Chang <heejae0704@gmail.com>
2022-07-26 00:54:33 +0900

    테스트 커버리지 100% 달성 및 user interaction 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import App from './App';
 
@@ -19,15 +19,32 @@ test('App', () => {
   ];
 
   useSelector.mockImplementation((selector) => selector({
+    taskTitle: 'new title',
     tasks,
   }));
 
-  const { getByText } = render((
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  const { getByText, getAllByText, getByLabelText } = render((
     <App />
   ));
 
   expect(getByText(/아무 것도 하지 않기/)).not.toBeNull();
   expect(getByText(/그래도 뭐라도 하기/)).not.toBeNull();
+
+  fireEvent.click(getAllByText(/완료/)[0]);
+
+  expect(dispatch).toBeCalledWith({ type: 'deleteTask', payload: { id: 1 } });
+
+  fireEvent.click(getByText(/추가/));
+
+  expect(dispatch).toBeCalledWith({ type: 'addTask' });
+
+  fireEvent.change(getByLabelText(/할 일/), { target: { value: 'a' } });
+
+  expect(dispatch).toBeCalledWith({ type: 'updateTaskTitle', payload: { taskTitle: 'a' } });
 
   // TODO: 통합 테스트 코드 작성
   // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
--- src/simpleIf.js
+++
@@ -1,6 +0,0 @@
-export default function simpleIf(value) {
-  if (value % 2 === 0) {
-    return 'even';
-  }
-  return 'odd';
-}
