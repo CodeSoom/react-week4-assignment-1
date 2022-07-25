301cb4d221cd5d5903588155cfade06cb002f246
Heejae Chang <heejae0704@gmail.com>
2022-07-25 21:00:02 +0900

    App 컴포넌트가 리덕스의 store 기반으로 할 일 목록을 보여주는 기능에 대한 테스트
    App.jsx에 대한 테스트 코드를 리덕스 기반의 테스트가 되도록 수정하여 Red가 되었습니다.
    Green이 되려면 리덕스의 useSelector mock 함수에 넣어주는 Task list가 화면에
    랜더링되어야 합니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,34 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  const tasks = [
+    {
+      id: 1,
+      taskTitle: '아무 것도 하지 않기',
+    },
+    {
+      id: 2,
+      taskTitle: '더더욱 아무 것도 하지 않기',
+    },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({
+    tasks,
+  }));
+
   const { getByText } = render((
     <App />
   ));
 
-  expect(getByText(/추가/)).not.toBeNull();
+  expect(getByText(/아무 것도 하지 않기/)).not.toBeNull();
+  expect(getByText(/더더욱 아무 것도 하지 않기/)).not.toBeNull();
+
 
   // TODO: 통합 테스트 코드 작성
   // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
