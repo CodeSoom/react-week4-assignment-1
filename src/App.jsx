601606e84c622990b16c9ff93fc5f06f01e8cc41
hanna244 <shhn0509@gmail.com>
2022-07-27 02:18:10 +0900

    구현: redux, react-redux 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { updateTaskTitle, addTask, deleteTask } from './actions';
 
 import Page from './Page';
 
-const initialState = {
-  newId: 100,
-  taskTitle: '',
-  tasks: [
-    { id: 1, title: '아무 것도 하지 않기 #1' },
-    { id: 2, title: '아무 것도 하지 않기 #2' },
-  ],
-};
-
-function updateTaskTitle(state, taskTitle) {
+function selector(state) {
   return {
-    ...state,
-    taskTitle,
-  };
-}
-
-function addTask(state) {
-  const { newId, tasks, taskTitle } = state;
-  return {
-    ...state,
-    newId: newId + 1,
-    taskTitle: '',
-    tasks: [...tasks, { id: newId, title: taskTitle }],
-  };
-}
-
-function deleteTask(state, id) {
-  const { tasks } = state;
-
-  return {
-    ...state,
-    tasks: tasks.filter((task) => task.id !== id),
+    taskTitle: state.taskTitle,
+    tasks: state.tasks,
   };
 }
 
 export default function App() {
-  const [state, setState] = useState(initialState);
+  const dispatch = useDispatch();
 
-  const { taskTitle, tasks } = state;
+  // store에서 상태를 가져옴
+  const { taskTitle, tasks } = useSelector(selector);
 
   function handleChangeTitle(event) {
-    setState(updateTaskTitle(state, event.target.value));
+    dispatch(updateTaskTitle(event.target.value));
   }
 
   function handleClickAddTask() {
-    setState(addTask(state));
+    dispatch(addTask());
   }
 
-  function handleClickDeleteTask() {
-    setState(deleteTask(state));
+  function handleClickDeleteTask(id) {
+    dispatch(deleteTask(id));
   }
 
   return (
