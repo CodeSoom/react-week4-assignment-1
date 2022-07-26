be45d1837016ba76ac22f4f60df9eb51112a95e5
hanna244 <shhn0509@gmail.com>
2022-07-27 03:00:46 +0900

    테스트: 기본적인 리덕스 테스트

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,22 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+// mocks 디렉토리 내부의 react-redux파일을 찾는다.
+jest.mock('react-redux');
+
 test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
+  // TODO: useSelector 조작
+  const tasks = [
+    { id: 1, title: '아무 것도 하지 않기 #1' },
+    { id: 2, title: '아무 것도 하지 않기 #2' },
+  ];
+
+  useSelector.mockImplementation((selector) => selector({ tasks }));
+
+  const { getByText } = render(<App />);
 
   expect(getByText(/추가/)).not.toBeNull();
 
