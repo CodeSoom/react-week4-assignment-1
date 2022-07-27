55c684b14d3a18b1efaa35016603426e5421168a
clapho <clapp301@gmail.com>
2022-07-28 00:36:48 +0900

    Redux 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
-import { useState } from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
 import Page from './Page';
+import { updateTaskTitle, addTask, deleteTask } from './actions';
 
 export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
+  const dispatch = useDispatch();
 
-  const { newId, taskTitle, tasks } = state;
+  const { taskTitle, tasks } = useSelector((state) => ({
+    taskTitle: state.taskTitle,
+    tasks: state.tasks,
+  }));
 
   function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+    dispatch(updateTaskTitle(event.target.value));
   }
 
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
+    dispatch(addTask());
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
+    dispatch(deleteTask(id));
   }
 
   return (
