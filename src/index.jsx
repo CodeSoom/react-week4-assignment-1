16468f4eef3f434b839e7f9c36b28a1a36084532
daadaadaah <60481383+daadaadaah@users.noreply.github.com>
2022-08-08 15:51:49 +0900

    Revert "[Edit] Refactor a TodoList with Redux and TDD"

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-
 import App from './App';
 
-import store from './store';
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('app'),
 );
