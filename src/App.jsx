1509bb280ee1dbab0bce265bd54dfa591030734c
0-Chan <kyc4421194@gmail.com>
2022-08-29 00:00:15 +0900

    :sparkles: Add InputContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import Page from './Page';
-
-import {
-  updateTaskTitle,
-  addTask,
-} from './actions';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-  };
-}
+import InputContainer from './InputContainer';
+import ListContainer from './ListContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  const { taskTitle } = useSelector(selector);
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,15 +5,14 @@ import { useSelector } from 'react-redux';
 import App from './App';
 
 jest.mock('react-redux');
-test('App', () => {
-  // todo: useSelector 조작
-  const tasks = [
-    { id: 1, title: '아무 것도 안함 #1' },
-    { id: 2, title: '아무 것도 안함 #2' },
-  ];
 
+test('App', () => {
   useSelector.mockImplementation((selector) => selector({
-    tasks,
+    taskTitle: '',
+    tasks: [
+      { id: 1, title: '아무 것도 안함 #1' },
+      { id: 2, title: '아무 것도 안함 #2' },
+    ],
   }));
 
   const { getByText } = render((
@@ -22,7 +21,4 @@ test('App', () => {
 
   expect(getByText(/추가/)).not.toBeNull();
   expect(getByText(/아무 것도 안함 #1/)).not.toBeNull();
-
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
