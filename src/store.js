72d5082d8fda033ea3b6f660d4cf71fba2fb4424
kang hye-won <gnpsi@fnf.co.kr>
2022-10-07 09:34:22 +0900

    4-2강 reducer.test 작성#

--- src/store.js
+++ src/store.js
@@ -1,43 +1,6 @@
 import { createStore } from 'redux';
 
-// Redux action
-// - type (string)
-// - payload => object => { taskTitle }
-
-const initialState = {
-  newId: 100,
-  taskTitle: '',
-  tasks: [
-    { id: 1, title: '아무 것도 하지 않기 #1' },
-    { id: 2, title: '아무 것도 하지 않기 #2' },
-  ],
-};
-
-function reducer(state = initialState, action){
-	if(action.type === 'updateTaskTitle') {
-		return {
-			...state,
-			taskTitle: action.payload.taskTitle,
-		}
-	}
-	if(action.type === 'addTask') {
-		const { newId, taskTitle, tasks } = state;
-		return {
-			newId: newId + 1,
-			taskTitle: '',
-			tasks: [...tasks, { id: newId, title: taskTitle }],
-		}
-	}
-	if(action.type === 'deleteTask') {
-		const { tasks } = state;
-		return {
-		...state,
-    tasks: tasks.filter((task) => task.id !== action.payload.id),
-		}
-	}
-
-	return state;
-}
+import reducer from './reducer';
 
 const store = createStore(reducer);
 
