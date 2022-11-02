1a38a93a706cb1c1ea8f4c551c7c7051a9179d2e
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-02 20:24:46 +0900

    App 컴포넌트 내에서 관심사 분리를 하였습니다.
    상태를 관리하는 함수 updateTaskTitle, addTask, deleteTask를 생성하여 App 컴포넌트 외부로 분리시켰습니다.
    ₩

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,68 @@ import { useState } from 'react';
 
 import Page from './Page';
 
-export default function App() {
-  const [state, setState] = useState({
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  });
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [
+    {
+      id: 1,
+      title: '아무것도 하지 않기 #1'
+    },
+    {
+      id: 2,
+      title: '아무것도 하지 않기 #2'
+    },
+  ],
+}
 
-  const { newId, taskTitle, tasks } = state;
-
-  function handleChangeTitle(event) {
-    setState({
-      ...state,
-      taskTitle: event.target.value,
-    });
+function updateTaskTitle(state, taskTitle) {
+  return {
+    ...state,
+    taskTitle,
   }
 
+}
+
+function addTask(state) {
+  const { newId, tasks, taskTitle } = state;
+
+  return {
+    ...state,
+    newId: newId + 1,
+    taskTitle: '',
+    tasks: [...tasks, { id: newId, title: taskTitle }],
+  }
+}
+
+function deleteTask(id, state) {
+  const { tasks } = state;
+
+  return {
+    ...state,
+    tasks: tasks.filter((task) => task.id !== id),
+  }
+
+}
+
+
+
+export default function App() {
+  const [state, setState] = useState(initialState);
+
+  const { taskTitle, tasks } = state;
+
+  function handleChangeTitle(event) {
+    setState(updateTaskTitle(state, event.target.value))
+  };
+  
+
   function handleClickAddTask() {
-    setState({
-      ...state,
-      newId: newId + 1,
-      taskTitle: '',
-      tasks: [...tasks, { id: newId, title: taskTitle }],
-    });
+    setState(addTask(state));
   }
 
   function handleClickDeleteTask(id) {
-    setState({
-      ...state,
-      tasks: tasks.filter((task) => task.id !== id),
-    });
+    setState(deleteTask(id, state));
   }
 
   return (
