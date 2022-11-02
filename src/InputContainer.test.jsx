b885ffec8398374dbf86a4bacee052c655751729
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-03 02:02:41 +0900

    InputContainer.test 테스트 케이스를 추가하였습니다.
    InputContainer의 랜더링, 입력한 할일의 랜더링, 추가 버튼을 누르면 addTask 호출을 확인할 수 있는 케이스를 추가하였습니다.

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -1,12 +1,14 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
 import InputContainer from './InputContainer';
 
+import { updateTaskTitle } from './actions';
+
 jest.mock('react-redux');
 
-test('InputContainer', () => {
+describe('InputContainer', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
@@ -15,15 +17,37 @@ test('InputContainer', () => {
     taskTitle: '누워있기',
   }));
 
-  const { getByText, getByDisplayValue } = render((
+  const renderInputContainer = () => render((
     <InputContainer />
   ));
 
-  expect(getByText(/추가/)).not.toBeNull();
-  expect(getByDisplayValue(/누워있기/)).not.toBeNull();
+  it('InputContainer가 랜더링 된다', () => {
+    renderInputContainer();
 
-  fireEvent.click(getByText(/추가/));
-  expect(dispatch).toBeCalledWith({
-    type: 'addTask',
+    expect(screen.getByLabelText(/할 일/)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: '추가' })).toBeInTheDocument();
+  });
+
+  it('입력한 할일이 랜더링 된다', () => {
+    renderInputContainer();
+
+    const input = screen.getByLabelText('할 일');
+
+    fireEvent.change(input, {
+      target: {
+        value: '잠자기',
+      },
+    });
+
+    expect(dispatch).toBeCalledWith(updateTaskTitle('잠자기'));
+  });
+
+  it('추가 버튼을 누르면 addTask가 호출된다', () => {
+    renderInputContainer();
+
+    fireEvent.click(screen.getByText(/추가/));
+    expect(dispatch).toBeCalledWith({
+      type: 'addTask',
+    });
   });
 });
