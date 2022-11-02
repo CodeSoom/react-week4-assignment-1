de230fc407e6b323193cd9cc8069700df84b69ae
sejineeee <sejinee0918@gmail.com>
2022-11-02 21:16:41 +0900

    reducer test 파일 생성
    reducer test 파일 생성
    context 구문 사용하기 위해서 context import
    describe-context-it 적용하여 할 일이 있을 경우 없을 경우 나누어 테스트

---
+++ src/reducer.test.js
@@ -0,0 +1,84 @@
+import context from 'jest-plugin-context';
+import reducer from './reducer';
+
+import { changeTaskTitle, addTask, deleteTask } from './actions';
+
+describe('reducer', () => {
+  describe('changeTaskTitle', () => {
+    it('할 일을 변경한다.', () => {
+      const state = reducer(
+        {
+          taskTitle: '',
+        },
+        changeTaskTitle('New Title'),
+      );
+
+      expect(state.taskTitle).toBe('New Title');
+    });
+
+    describe('addTask', () => {
+      function reduceAddTask(taskTitle) {
+        return reducer(
+          {
+            newId: 100,
+            taskTitle,
+            tasks: [],
+          },
+          addTask(),
+        );
+      }
+
+      context('할 일이 있을 경우', () => {
+        it('할 일을 포함하여 새 목록을 만든다.', () => {
+          const state = reduceAddTask('New Task');
+
+          expect(state.tasks).toHaveLength(1);
+          expect(state.tasks[0].id).not.toBeUndefined();
+          expect(state.tasks[0].title).toBe('New Task');
+        });
+
+        it('task Title을 빈 값으로 만든다.', () => {
+          const state = reduceAddTask('New Task');
+
+          expect(state.taskTitle).toBe('');
+        });
+      });
+
+      context('할 일이 없을 경우', () => {
+        it('동작하지 않는다.', () => {
+          const state = reduceAddTask('');
+
+          expect(state.tasks).toHaveLength(0);
+        });
+      });
+    });
+  });
+
+  describe('deleteTask', () => {
+    context('task의 id가 존재할 경우', () => {
+      it('목록에서 할 일을 지운다.', () => {
+        const state = reducer(
+          {
+            tasks: [{ id: 1, title: 'Task' }],
+          },
+          deleteTask(1),
+        );
+
+        expect(state.tasks).toHaveLength(0);
+      });
+    });
+
+    context('task의 id가 존재하지 않을 경우', () => {
+      it('목록에서 할 일을 지우지 않는다.', () => {
+        const state = reducer(
+          {
+            tasks: [{ id: 1, title: 'Task' }],
+          },
+          deleteTask(100),
+        );
+
+        expect(state.tasks).toHaveLength(1);
+      });
+    });
+  });
+});
