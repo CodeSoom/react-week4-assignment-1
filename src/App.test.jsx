5043238fdf5ee7b5bfa666ffd081a8995a2da840
sejineeee <sejinee0918@gmail.com>
2022-11-03 20:56:16 +0900

    review: useSelector 추가, BDD 적용
    useSelector 추가
    describe - it 구문 사용하여 BDD 적용하여 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,19 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
+import tasks from '../fixtures/tasks';
+
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
+jest.mock('react-redux');
 
-  expect(getByText(/추가/)).not.toBeNull();
+describe('App', () => {
+  useSelector.mockImplementation((selector) => selector({ tasks }));
 
-  // TODO: 통합 테스트 코드 작성
-  // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
+  it('App을 렌더링한다.', () => {
+    const { getByText } = render((<App />));
+
+    expect(getByText('추가')).not.toBeNull();
+  });
 });
