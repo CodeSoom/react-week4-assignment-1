c69e0302497e98f6afeeb7f1208d264f3f46f28d
sejineeee <sejinee0918@gmail.com>
2022-11-03 20:52:28 +0900

    review: 삭제했을 때 테스트 추가
    테스트용 데이터를 fixtures로 분리하여 호출해서 사용하는 것으로 변경
    dispatch에 대한 mock 함수 추가
    it 테스트 name 구체적으로 작성하여 수정
    할 일을 삭제하면 해당 할 일이 삭제되는 테스트 추가

--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -1,24 +1,36 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListContainer from './ListContainer';
+
+import tasks from '../fixtures/tasks';
 
 jest.mock('react-redux');
 
 describe('ListContainer', () => {
-  it('List', () => {
-    const tasks = [
-      { id: 1, title: '리덕스 공부하기' },
-      { id: 2, title: 'TDD 공부하기' },
-    ];
+  const dispatch = jest.fn();
 
-    useSelector.mockImplementation((selector) => selector({
-      tasks,
-    }));
+  useDispatch.mockImplementation(() => dispatch);
 
+  useSelector.mockImplementation((selector) => selector({ tasks }));
+
+  it('ListContainer를 렌더링한다.', () => {
     const { getByText } = render(<ListContainer />);
 
     expect(getByText('리덕스 공부하기')).not.toBeNull();
+  });
+
+  it('할 일을 삭제하면 해당 할 일이 삭제된다.', () => {
+    const { getAllByText } = render(<ListContainer />);
+
+    fireEvent.click(getAllByText('완료')[0]);
+
+    expect(dispatch).toBeCalledWith({
+      type: 'deleteTask',
+      payload: {
+        id: 1,
+      },
+    });
   });
 });
