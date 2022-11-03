2b820b9c4a875225e306b3020299ee4cb2e01391
sejineeee <sejinee0918@gmail.com>
2022-11-03 20:49:48 +0900

    review: 입력했을 때, 추가했을 때에 대한 test 추가
    입력했을 때, handleChange 작동에 대한 test 추가
    렌더링 테스트에 있던 추가 버튼에 대한 것을 추가 버튼 test로 분리

--- src/InputContainer.test.jsx
+++ src/InputContainer.test.jsx
@@ -9,15 +9,36 @@ jest.mock('react-redux');
 describe('InpurContainer', () => {
   const dispatch = jest.fn();
 
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({ taskTitle: 'New Title' }));
+
   it('InputContainer를 렌더링한다.', () => {
-    useDispatch.mockImplementation(() => dispatch);
-
-    useSelector.mockImplementation((selector) => selector({ taskTitle: 'New Title' }));
-
     const { getByText, getByDisplayValue } = render(<InputContainer />);
 
     expect(getByText('추가')).not.toBeNull();
     expect(getByDisplayValue('New Title')).not.toBeNull();
+  });
+
+  it('할 일을 입력하면 handleChange가 실행되어 value가 변경된다.', () => {
+    const { getByLabelText } = render(<InputContainer />);
+
+    fireEvent.change(getByLabelText('할 일'), {
+      target: {
+        value: '무엇인가 하기',
+      },
+    });
+
+    expect(dispatch).toBeCalledWith({
+      type: 'changeTaskTitle',
+      payload: {
+        taskTitle: '무엇인가 하기',
+      },
+    });
+  });
+
+  it('추가 버튼을 누르면 할 일이 추가된다.', () => {
+    const { getByText } = render(<InputContainer />);
 
     fireEvent.click(getByText('추가'));
 
