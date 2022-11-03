664a58d6c3f4d2491a300719955c6b900d4c8f14
sejineeee <sejinee0918@gmail.com>
2022-11-03 15:54:13 +0900

    review: useSelector mock 함수 수정
    강의를 통해서 useSelector mock 함수 수정

--- __mocks__/react-redux.js
+++ __mocks__/react-redux.js
@@ -1,9 +1,3 @@
 export const useDispatch = jest.fn();
 
-export const useSelector = jest.fn((selector) => selector({
-    tasks: [
-      { id: 1, title: '리덕스 공부하기' },
-      { id: 2, title: 'TDD 공부하기' },
-    ],
-  }),
-);
+export const useSelector = jest.fn();
