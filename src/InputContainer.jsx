715da7858762ebb8f4ab65618e2fac6a927fa87c
Ubermensch0608 <melancholyee28@gmail.com>
2023-01-01 23:12:16 +0900

    Update: redux 상태관리 세팅
    관심사의 분리를 위해 App에서 진행되던 모든 state관리를 나누었습니다.
    ListContainer에서는 tasks를 InputContainer에서는 새로운 할일을 만드는 데 필요한 비즈니스 로직이 모여있습니다.

---
+++ src/InputContainer.jsx
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import Input from './Input';
+
+import { addTask, updateTaskTitle } from './actions';
+
+export default function InputContainer() {
+  const { taskTitle } = useSelector((state) => ({
+    taskTitle: state.taskTitle,
+  }));
+
+  const dispatch = useDispatch();
+
+  const handleChangeTitle = (event) => {
+    dispatch(updateTaskTitle(event.target.value));
+  };
+
+  const handleClickAddTask = () => {
+    dispatch(addTask());
+  };
+
+  return (
+    <Input
+      value={taskTitle}
+      onChange={handleChangeTitle}
+      onClick={handleClickAddTask}
+    />
+
+  );
+}
---
+++ src/reducer.js
@@ -0,0 +1,39 @@
+const initialState = {
+  newId: 100,
+  taskTitle: '',
+  tasks: [
+    { id: 1, title: '할 일 #1' },
+    { id: 2, title: '할 일 #2' },
+  ],
+};
+
+function reducer(state = initialState, action) {
+  const { newId, taskTitle, tasks } = state;
+
+  if (action.type === 'updateTaskTitle') {
+    return {
+      ...state,
+      taskTitle: action.payload.taskTitle,
+    };
+  }
+
+  if (action.type === 'addTask') {
+    return {
+      ...state,
+      newId: newId + 1,
+      taskTitle: '',
+      tasks: [...tasks, { id: newId, title: taskTitle }],
+    };
+  }
+
+  if (action.type === 'deleteTask') {
+    return {
+      ...state,
+      tasks: tasks.filter((task) => task.id !== action.payload.id),
+    };
+  }
+
+  return state;
+}
+
+export default reducer;
